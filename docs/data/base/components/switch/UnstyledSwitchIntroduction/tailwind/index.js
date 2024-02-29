cb57671b0f05840e69702f9dc3a9afa5629cce61
Michał Dudak <michal@mui.com>
2024-02-29 11:35:16 +0100

    Update the System intro demo

--- docs/data/base/components/switch/UnstyledSwitchIntroduction/tailwind/index.js
+++ docs/data/base/components/switch/UnstyledSwitchIntroduction/tailwind/index.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { Switch as BaseSwitch } from '@mui/base/Switch';
 import { useTheme } from '@mui/system';
@@ -104,16 +103,3 @@ const Switch = React.forwardRef((props, ref) => {
     />
   );
 });
-
-Switch.propTypes = {
-  /**
-   * The props used for each slot inside the Switch.
-   * @default {}
-   */
-  slotProps: PropTypes.shape({
-    input: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
-    root: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
-    thumb: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
-    track: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
-  }),
-};
