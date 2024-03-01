b81b741ee03a6df87fd67026e61c63452eb35682
Michał Dudak <michal@mui.com>
2024-03-01 09:32:11 +0100

    Rewrite the Switch on Badge demos

--- docs/data/base/components/badge/BadgeVisibility.js
+++ docs/data/base/components/badge/BadgeVisibility.js
@@ -3,7 +3,7 @@ import { Badge as BaseBadge, badgeClasses } from '@mui/base/Badge';
 // Auxiliary demo components
 import { styled, Stack } from '@mui/system';
 import { Button, buttonClasses } from '@mui/base/Button';
-import { Switch, switchClasses } from '@mui/base/Switch';
+import { Switch as BaseSwitch } from '@mui/base/Switch';
 import Divider from '@mui/material/Divider';
 // Icons
 import AddIcon from '@mui/icons-material/Add';
@@ -13,6 +13,7 @@ import MailIcon from '@mui/icons-material/Mail';
 const blue = {
   200: '#99CCF3',
   500: '#007FFF',
+  700: '#0059B2',
 };
 
 const grey = {
@@ -95,66 +96,73 @@ const StyledButton = styled(Button)(
   `,
 );
 
-const Root = styled('span')(
+const Switch = styled(BaseSwitch)(
   ({ theme }) => `
-  position: relative;
+  width: 38px;
+  height: 24px;
+  margin: 10px;
+  padding: 0;
+  box-sizing: border-box;
+  background: ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
+  border: 1px solid ${theme.palette.mode === 'dark' ? grey[800] : grey[200]};
+  border-radius: 24px;
   display: inline-block;
-  width: 32px;
-  height: 20px;
-  cursor: pointer;
+  transition-property: all;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  transition-duration: 120ms;
+  box-shadow: inset 0px 1px 1px ${
+    theme.palette.mode === 'dark' ? 'rgba(0, 0, 0, 0.5)' : 'rgba(0, 0, 0, 0.05)'
+  };
 
-
-  & .${switchClasses.track} {
-    background: ${theme.palette.mode === 'dark' ? grey[600] : grey[400]};
-    border-radius: 16px;
-    display: block;
-    height: 100%;
-    width: 100%;
-    position: absolute;
+  &[data-disabled] {
+    opacity: 0.4;
+    cursor: not-allowed;
   }
 
-  & .${switchClasses.thumb} {
-    position: relative;
+  &:hover:not([data-disabled]) {
+    background: ${theme.palette.mode === 'dark' ? grey[800] : grey[100]};
+    border-color: ${theme.palette.mode === 'dark' ? grey[600] : grey[300]};
+  }
+
+  &:focus-visible {
+    box-shadow: 0 0 0 3px ${theme.palette.mode === 'dark' ? blue[700] : blue[200]};
+  }
+
+  &[data-state="checked"] {
+    border: none;
+    background: ${blue[500]};
+  }
+
+  &[data-state="checked"]:not([data-disabled]):hover {
+    background: ${blue[700]};
+  }
+  `,
+);
+
+const Thumb = styled(BaseSwitch.Thumb)(
+  ({ theme }) => `
+    box-sizing: border-box;
+    border: 1px solid ${theme.palette.mode === 'dark' ? grey[800] : grey[200]};
     display: block;
-    width: 14px;
-    height: 14px;
-    top: 3px;
-    left: 3px;
+    width: 16px;
+    height: 16px;
+    left: 4px;
     border-radius: 16px;
-    background-color: #fff;
+    background-color: #FFF;
+    position: relative;
     transition-property: all;
     transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
     transition-duration: 120ms;
-  }
+    box-shadow: 0px 1px 2px ${
+      theme.palette.mode === 'dark' ? 'rgba(0, 0, 0, 0.25)' : 'rgba(0, 0, 0, 0.1)'
+    };
 
-  &.${switchClasses.focusVisible} .${switchClasses.track} {
-    box-shadow: 0 0 0 3px ${theme.palette.mode === 'dark' ? grey[700] : blue[200]};
-  }
-
-  &.${switchClasses.checked} {
-    .${switchClasses.thumb} {
-      left: 15px;
-      top: 3px;
+    &[data-state="checked"] {
+      left: 18px;
       background-color: #fff;
+      box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.3);
     }
-
-    .${switchClasses.track} {
-      background: ${blue[500]};
-    }
-  }
-
-  & .${switchClasses.input} {
-    cursor: inherit;
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    opacity: 0;
-    z-index: 1;
-    margin: 0;
-  }
-  `,
+`,
 );
 
 const StyledLabel = styled('label')(
@@ -205,13 +213,9 @@ export default function BadgeVisibility() {
         <Divider orientation="vertical" />
         <Stack direction="row" spacing={1} useFlexGap>
           <StyledLabel>Show badge</StyledLabel>
-          <Switch
-            slots={{
-              root: Root,
-            }}
-            checked={!invisible}
-            onChange={handleBadgeVisibility}
-          />
+          <Switch checked={!invisible} onChange={handleBadgeVisibility}>
+            <Thumb />
+          </Switch>
         </Stack>
       </Stack>
     </Stack>
