cc74c08c4022bacc4c07140600e30b561923f4df
Albert Yu <albert@albertyu.co>
2024-03-14 18:18:28 +0800

    Update docs

--- packages/mui-base/src/Textarea/useTextareaStyleHooks.ts
+++ packages/mui-base/src/Textarea/useTextareaStyleHooks.ts
@@ -1,7 +1,12 @@
 import * as React from 'react';
 import { TextareaOwnerState } from './Textarea.types';
 import { getStyleHookProps } from './utils';
-
+/**
+ *
+ * API:
+ *
+ * - [useTextareaStyleHooks API](https://mui.com/base-ui/api/use-textarea-style-hooks/)
+ */
 export function useTextareaStyleHooks(ownerState: TextareaOwnerState) {
   return React.useMemo(() => {
     return getStyleHookProps(ownerState, {
