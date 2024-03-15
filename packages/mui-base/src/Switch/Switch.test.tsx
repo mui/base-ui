eeb9d3cf4e8936cb146dbe40e7c42544c46c56d4
Michał Dudak <michal@mui.com>
2024-03-15 15:12:13 +0100

    Update the tests

--- packages/mui-base/src/Switch/Switch.test.tsx
+++ packages/mui-base/src/Switch/Switch.test.tsx
@@ -3,71 +3,21 @@ import { expect } from 'chai';
 import { spy } from 'sinon';
 import { createRenderer, act } from '@mui/internal-test-utils';
 import { Switch } from '@mui/base/Switch';
+import { describeConformance } from '../../test/describeConformance';
 
 describe('<Switch />', () => {
   const { render } = createRenderer();
 
-  // TODO: move to the conformance test suite
-
-  describe('prop: className', () => {
-    it('should apply the className when passed as a string', () => {
-      const { container } = render(<Switch className="test-class" />);
-      expect(container.firstElementChild?.className).to.contain('test-class');
-    });
-  });
+  describeConformance(<Switch />, () => ({
+    inheritComponent: 'button',
+    refInstanceof: window.HTMLButtonElement,
+    render,
+  }));
 
   describe('extra props', () => {
-    it('should spread extra props', () => {
-      const { container } = render(<Switch data-extra-prop="Lorem ipsum" />);
-      expect(container.firstElementChild as HTMLElement).to.have.attribute(
-        'data-extra-prop',
-        'Lorem ipsum',
-      );
-    });
-
     it('should not override the built-in attributes', () => {
       const { container } = render(<Switch data-state="checked" role="checkbox" />);
       expect(container.firstElementChild as HTMLElement).to.have.attribute('role', 'switch');
-    });
-  });
-
-  describe('prop: render', () => {
-    const Wrapper = React.forwardRef<HTMLButtonElement, { children?: React.ReactNode }>(
-      function Wrapper(props, forwardedRef) {
-        return (
-          <div data-testid="wrapper">
-            <button ref={forwardedRef} {...props} />
-          </div>
-        );
-      },
-    );
-
-    it('should render the custom component', () => {
-      const { container, getByTestId } = render(
-        <Switch render={(props) => <Wrapper {...props} />} data-testid="wrapped" />,
-      );
-
-      expect(container.firstElementChild).to.equal(getByTestId('wrapper'));
-    });
-
-    it('should pass the ref to the custom component', () => {
-      let instanceFromRef = null;
-
-      function Test() {
-        return (
-          <Switch
-            ref={(el) => {
-              instanceFromRef = el;
-            }}
-            render={(props) => <Wrapper {...props} />}
-            data-testid="wrapped"
-          />
-        );
-      }
-
-      render(<Test />);
-      expect(instanceFromRef!.tagName).to.equal('BUTTON');
-      expect(instanceFromRef!).to.have.attribute('data-testid', 'wrapped');
     });
   });
 
---
+++ packages/mui-base/src/Switch/SwitchThumb.test.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { createRenderer } from '@mui/internal-test-utils';
+import { Switch } from '@mui/base/Switch';
+import { describeConformance } from '../../test/describeConformance';
+import { SwitchContext } from './SwitchContext';
+
+const testContext = {
+  checked: false,
+  disabled: false,
+  readOnly: false,
+  required: false,
+};
+
+describe('<Switch.Thumb />', () => {
+  const { render } = createRenderer();
+
+  describeConformance(<Switch.Thumb />, () => ({
+    inheritComponent: 'span',
+    refInstanceof: window.HTMLSpanElement,
+    render: (node) => {
+      return render(<SwitchContext.Provider value={testContext}>{node}</SwitchContext.Provider>);
+    },
+    skip: ['reactTestRenderer'],
+  }));
+});
