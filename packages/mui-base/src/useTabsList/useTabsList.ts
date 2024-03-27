868e1bde0da00e4fae321c87c934cbf8c226f61b
Michał Dudak <michal@mui.com>
2024-03-27 14:01:12 +0100

    Break import cycle

--- packages/mui-base/src/useTabsList/useTabsList.ts
+++ packages/mui-base/src/useTabsList/useTabsList.ts
@@ -1,6 +1,6 @@
 'use client';
 import * as React from 'react';
-import { useTabsContext } from '../Tabs';
+import { useTabsContext } from '../Tabs/TabsContext';
 import {
   TabsListActionTypes,
   UseTabsListParameters,
