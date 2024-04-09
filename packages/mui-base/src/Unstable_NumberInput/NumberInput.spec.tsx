6fe4968da826432528b061991fbd57a018bcb7b8
atomiks <cc.glows@gmail.com>
2024-04-09 22:07:47 +1000

    Remove NumberInput

--- packages/mui-base/src/Unstable_NumberInput/NumberInput.spec.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { Unstable_NumberInput as NumberInput } from '@base_ui/react/Unstable_NumberInput';
-
-/* @ts-expect-error - inputId shouldn't be available on NumberInput */
-<NumberInput inputId="" />;
-
-/* @ts-expect-error - inputRef shouldn't be available on NumberInput */
-<NumberInput inputRef={{}} />;
