4397adb85ee08d49e36a68217a2d8f1a0425d5e4
Michał Dudak <michal@mui.com>
2024-05-22 15:21:55 +0200

    Use `useComponentRenderer` in Tabs

--- packages/mui-base/src/Tabs/TabPanel/useTabPanelStyleHooks.ts
+++
@@ -1,9 +0,0 @@
-import { TabPanelOwnerState } from './TabPanel.types';
-import { useTabsRootStyleHooks } from '../Root/useTabsRootStyleHooks';
-
-/**
- * @ignore - internal hook.
- */
-export function useTabPanelStyleHooks(ownerState: TabPanelOwnerState) {
-  return useTabsRootStyleHooks(ownerState);
-}
