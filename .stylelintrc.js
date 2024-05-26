ab93734c939f663224b3a105758d779d00aa2d7d
Olivier Tassinari <olivier.tassinari@gmail.com>
2024-05-26 22:34:40 +0200

    fix ci

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,18 @@
 const baseline = require('@mui/monorepo/.stylelintrc');
 
+// TODO remove. In theory, deleting these lines should just work.
+delete baseline.rules['declaration-colon-newline-after'];
+delete baseline.rules['function-parentheses-newline-inside'];
+delete baseline.rules['no-missing-end-of-source-newline'];
+delete baseline.rules['value-list-comma-newline-after'];
+
 module.exports = {
   ...baseline,
+  // TODO remove. In theory, deleting these lines should just work.
+  ignoreFiles: [
+    '**/*.d.ts',
+    'docs/.next/**/*',
+    'docs/export/**/*',
+    '**/node_modules/**/*',
+  ],
 };
