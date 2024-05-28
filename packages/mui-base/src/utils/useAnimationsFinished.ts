6b45ff0e744e78dd7a43082905735225619ff9dd
atomiks <cc.glows@gmail.com>
2024-05-28 21:41:55 +1000

    Refactor out double rAF

--- packages/mui-base/src/utils/useAnimationsFinished.ts
+++ packages/mui-base/src/utils/useAnimationsFinished.ts
@@ -7,12 +7,10 @@ import { useEventCallback } from './useEventCallback';
  * @ignore - internal hook.
  */
 export function useAnimationsFinished(getElement: () => Element | null | undefined) {
-  const frame1Ref = React.useRef(-1);
-  const frame2Ref = React.useRef(-1);
+  const frameRef = React.useRef(-1);
 
   const cancelFrames = useEventCallback(() => {
-    cancelAnimationFrame(frame1Ref.current);
-    cancelAnimationFrame(frame2Ref.current);
+    cancelAnimationFrame(frameRef.current);
   });
 
   React.useEffect(() => cancelFrames, [cancelFrames]);
@@ -29,14 +27,9 @@ export function useAnimationsFinished(getElement: () => Element | null | undefin
     // Wait for the CSS styles to be applied to determine if the animation has been removed in the
     // [data-instant] state. This allows the close animation to play if the `delay` instantType is
     // applying to the same element.
-    // Notes:
-    // - A single `requestAnimationFrame` is sometimes unreliable.
-    // - `queueMicrotask` does not work.
-    frame1Ref.current = requestAnimationFrame(() => {
-      frame2Ref.current = requestAnimationFrame(async () => {
-        await Promise.allSettled(element.getAnimations().map((animation) => animation.finished));
-        fnToExecute();
-      });
+    frameRef.current = requestAnimationFrame(async () => {
+      await Promise.allSettled(element.getAnimations().map((animation) => animation.finished));
+      fnToExecute();
     });
   });
 }
