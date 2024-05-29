694612fe35033ed54ea032288f82b82aca605d3c
Michał Dudak <michal@mui.com>
2024-05-29 13:55:31 +0200

    Move the directive from index files to components

--- packages/mui-base/src/Checkbox/Root/useCheckboxRoot.ts
+++ packages/mui-base/src/Checkbox/Root/useCheckboxRoot.ts
@@ -1,3 +1,4 @@
+'use client';
 import * as React from 'react';
 import type { UseCheckboxRootParameters, UseCheckboxRootReturnValue } from './CheckboxRoot.types';
 import { useControlled } from '../../utils/useControlled';
--- packages/mui-base/src/Checkbox/index.barrel.ts
+++ packages/mui-base/src/Checkbox/index.barrel.ts
@@ -1,4 +1,3 @@
-'use client';
 export { CheckboxRoot } from './Root/CheckboxRoot';
 export { useCheckboxRoot } from './Root/useCheckboxRoot';
 export { CheckboxContext } from './Root/CheckboxContext';
