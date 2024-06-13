2abbb6a98e2f6881a89c2757d642e4ed935ac713
Michał Dudak <michal@mui.com>
2024-06-13 21:19:56 +0200

    Fix proptypes generator

--- scripts/coreTypeScriptProjects.js
+++ scripts/coreTypeScriptProjects.js
@@ -4,6 +4,7 @@ export default {
   base: {
     rootPath: path.join(process.cwd(), 'packages/mui-base'),
     entryPointPath: 'src/index.ts',
+    tsConfigPath: 'tsconfig.json',
   },
   docs: {
     rootPath: path.join(process.cwd(), 'docs'),
