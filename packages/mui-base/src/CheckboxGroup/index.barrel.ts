44d40b04586af5307bf51b1f0c331eed92863e54
atomiks <cc.glows@gmail.com>
2024-06-19 16:49:22 +1000

    Remove hook exports

--- packages/mui-base/src/CheckboxGroup/index.barrel.ts
+++ packages/mui-base/src/CheckboxGroup/index.barrel.ts
@@ -1,23 +1,11 @@
 export * from './Root/CheckboxGroupRoot';
-export * from './Root/useCheckboxGroupRoot';
 export * from './Label/CheckboxGroupLabel';
-export * from './Label/useCheckboxGroupLabel';
-export * from './Parent/useCheckboxGroupParent';
 
 export type {
   CheckboxGroupRootProps,
   CheckboxGroupRootOwnerState,
 } from './Root/CheckboxGroupRoot.types';
-export type { UseCheckboxGroupRootReturnValue } from './Root/useCheckboxGroupRoot.types';
 export type {
   CheckboxGroupLabelProps,
   CheckboxGroupLabelOwnerState,
 } from './Label/CheckboxGroupLabel.types';
-export type {
-  UseCheckboxGroupLabelParameters,
-  UseCheckboxGroupLabelReturnValue,
-} from './Label/useCheckboxGroupLabel.types';
-export type {
-  UseCheckboxGroupParentParameters,
-  UseCheckboxGroupParentReturnValue,
-} from './Parent/useCheckboxGroupParent.types';
--- packages/mui-base/src/CheckboxGroup/index.ts
+++ packages/mui-base/src/CheckboxGroup/index.ts
@@ -1,8 +1,5 @@
 export { CheckboxGroupRoot as Root } from './Root/CheckboxGroupRoot';
 export { CheckboxGroupLabel as Label } from './Label/CheckboxGroupLabel';
-export { useCheckboxGroupRoot as useRoot } from './Root/useCheckboxGroupRoot';
-export { useCheckboxGroupLabel as useLabel } from './Label/useCheckboxGroupLabel';
-export { useCheckboxGroupParent } from './Parent/useCheckboxGroupParent';
 
 export type {
   CheckboxGroupRootProps as RootProps,
@@ -12,12 +9,3 @@ export type {
   CheckboxGroupLabelProps as LabelProps,
   CheckboxGroupLabelOwnerState as LabelOwnerState,
 } from './Label/CheckboxGroupLabel.types';
-export type { UseCheckboxGroupRootReturnValue as UseRootReturnValue } from './Root/useCheckboxGroupRoot.types';
-export type {
-  UseCheckboxGroupLabelParameters as UseLabelParameters,
-  UseCheckboxGroupLabelReturnValue as UseLabelReturnValue,
-} from './Label/useCheckboxGroupLabel.types';
-export type {
-  UseCheckboxGroupParentParameters as UseParentParameters,
-  UseCheckboxGroupParentReturnValue as UseParentReturnValue,
-} from './Parent/useCheckboxGroupParent.types';
