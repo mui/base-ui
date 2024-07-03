070b3ed15bdc24fcb4463363c4eb531203446eb4
Michał Dudak <michal@mui.com>
2024-07-03 11:19:05 +0200

    Remove enzyme

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -122,7 +122,7 @@ module.exports = async function setKarmaConfig(config) {
           'process.env.TEST_GATE': JSON.stringify(process.env.TEST_GATE),
         }),
         new webpack.ProvidePlugin({
-          // required by enzyme > cheerio > parse5 > util
+          // required by code accessing `process.env` in the browser
           process: 'process/browser.js',
         }),
       ],
@@ -156,10 +156,8 @@ module.exports = async function setKarmaConfig(config) {
           // needed by sourcemap
           fs: false,
           path: false,
-          // needed by enzyme > cheerio
+          // Exclude polyfill and treat 'stream' as an empty module since it is not required. next -> gzip-size relies on it.
           stream: false,
-          // required by enzyme > cheerio > parse5
-          util: require.resolve('util/'),
           vm: false,
         },
       },
