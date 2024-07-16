19a5219945bc2a5a511d57033df8a26f7bc7f4b0
atomiks <cc.glows@gmail.com>
2024-07-16 13:46:46 +1000

    Restore Name field

--- docs/data/base/components/field/UnstyledFieldIntroduction/system/index.tsx
+++ docs/data/base/components/field/UnstyledFieldIntroduction/system/index.tsx
@@ -1,20 +1,13 @@
 import * as React from 'react';
 import { styled } from '@mui/system';
 import * as Field from '@base_ui/react/Field';
-import * as NumberField from '@base_ui/react/NumberField';
 
 export default function UnstyledSwitchIntroduction() {
   return (
     <Field.Root style={{ width: 250 }}>
       <div style={{ display: 'flex', gap: 8 }}>
         <Field.Label>Name</Field.Label>
-        <NumberField.Root>
-          <NumberField.Group>
-            <NumberField.Increment>+</NumberField.Increment>
-            <NumberField.Decrement>-</NumberField.Decrement>
-            <NumberField.Input required />
-          </NumberField.Group>
-        </NumberField.Root>
+        <Field.Control required pattern="[a-zA-Z0-9]+" />
       </div>
       <Field.Validity>
         {(validity, value) => {
@@ -31,7 +24,6 @@ export default function UnstyledSwitchIntroduction() {
           return null;
         }}
       </Field.Validity>
-      <FieldMessage data-error show="rangeUnderflow" />
       <FieldMessage data-error show="valueMissing" />
       <FieldMessage data-error show={(value) => value === 'admin'}>
         Name not allowed.
--- packages/mui-base/src/Field/Control/FieldControl.types.ts
+++ packages/mui-base/src/Field/Control/FieldControl.types.ts
@@ -1,15 +1,7 @@
 import { BaseUIComponentProps } from '@base_ui/react/utils/types';
 
-export type FieldControlElement =
-  | HTMLInputElement
-  | HTMLTextAreaElement
-  | HTMLSelectElement
-  | HTMLButtonElement;
+export type FieldControlElement = HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement;
 
 export type FieldControlOwnerState = {};
 
-export interface FieldControlProps
-  extends BaseUIComponentProps<
-    'input' | 'select' | 'textarea' | 'button',
-    FieldControlOwnerState
-  > {}
+export interface FieldControlProps extends BaseUIComponentProps<'input', FieldControlOwnerState> {}
