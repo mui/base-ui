a3bea555875c454f623217e2066c1c9578aa43ec
atomiks <cc.glows@gmail.com>
2024-07-25 12:00:28 +1000

    Test

--- packages/mui-base/src/PreviewCard/Root/PreviewCardRoot.test.tsx
+++ packages/mui-base/src/PreviewCard/Root/PreviewCardRoot.test.tsx
@@ -43,5 +43,217 @@ describe('<PreviewCard.Root />', () => {
 
       expect(screen.getByText('Content')).not.to.equal(null);
     });
+
+    it('should close when the trigger is unhovered', async () => {
+      render(
+        <Root>
+          <Trigger />
+          <PreviewCard.Positioner>
+            <PreviewCard.Popup>Content</PreviewCard.Popup>
+          </PreviewCard.Positioner>
+        </Root>,
+      );
+
+      const trigger = screen.getByRole('link');
+
+      fireEvent.pointerDown(trigger, { pointerType: 'mouse' });
+      fireEvent.mouseEnter(trigger);
+      fireEvent.mouseMove(trigger);
+
+      clock.tick(OPEN_DELAY);
+
+      await waitForPosition();
+
+      fireEvent.mouseLeave(trigger);
+
+      clock.tick(CLOSE_DELAY);
+
+      expect(screen.queryByText('Content')).to.equal(null);
+    });
+
+    it('should open when the trigger is focused', async () => {
+      if (!/jsdom/.test(window.navigator.userAgent)) {
+        // Ignore due to `:focus-visible` being required in the browser.
+        return;
+      }
+
+      render(
+        <Root>
+          <Trigger />
+          <PreviewCard.Positioner>
+            <PreviewCard.Popup>Content</PreviewCard.Popup>
+          </PreviewCard.Positioner>
+        </Root>,
+      );
+
+      const trigger = screen.getByRole('link');
+
+      act(() => trigger.focus());
+
+      clock.tick(OPEN_DELAY);
+
+      await waitForPosition();
+
+      expect(screen.getByText('Content')).not.to.equal(null);
+    });
+
+    it('should close when the trigger is blurred', async () => {
+      render(
+        <Root>
+          <Trigger />
+          <PreviewCard.Positioner>
+            <PreviewCard.Popup>Content</PreviewCard.Popup>
+          </PreviewCard.Positioner>
+        </Root>,
+      );
+
+      const trigger = screen.getByRole('link');
+
+      act(() => trigger.focus());
+
+      clock.tick(OPEN_DELAY);
+
+      await waitForPosition();
+
+      act(() => trigger.blur());
+
+      clock.tick(OPEN_DELAY);
+
+      expect(screen.queryByText('Content')).to.equal(null);
+    });
+  });
+
+  describe('controlled open', () => {
+    clock.withFakeTimers();
+
+    it('should open when controlled open is true', async () => {
+      render(
+        <Root open>
+          <PreviewCard.Positioner>
+            <PreviewCard.Popup>Content</PreviewCard.Popup>
+          </PreviewCard.Positioner>
+        </Root>,
+      );
+
+      expect(screen.getByText('Content')).not.to.equal(null);
+    });
+
+    it('should close when controlled open is false', async () => {
+      render(
+        <Root open={false}>
+          <PreviewCard.Positioner>
+            <PreviewCard.Popup>Content</PreviewCard.Popup>
+          </PreviewCard.Positioner>
+        </Root>,
+      );
+
+      expect(screen.queryByText('Content')).to.equal(null);
+    });
+
+    it('should call onOpenChange when the open state changes', async () => {
+      const handleChange = spy();
+
+      function App() {
+        const [open, setOpen] = React.useState(false);
+
+        return (
+          <Root
+            open={open}
+            onOpenChange={(nextOpen) => {
+              handleChange(open);
+              setOpen(nextOpen);
+            }}
+          >
+            <Trigger />
+            <PreviewCard.Positioner>
+              <PreviewCard.Popup>Content</PreviewCard.Popup>
+            </PreviewCard.Positioner>
+          </Root>
+        );
+      }
+
+      render(<App />);
+
+      expect(screen.queryByText('Content')).to.equal(null);
+
+      const trigger = screen.getByRole('link');
+
+      fireEvent.mouseEnter(trigger);
+      fireEvent.mouseMove(trigger);
+
+      clock.tick(OPEN_DELAY);
+
+      await waitForPosition();
+
+      expect(screen.getByText('Content')).not.to.equal(null);
+
+      fireEvent.mouseLeave(trigger);
+
+      clock.tick(CLOSE_DELAY);
+
+      expect(screen.queryByText('Content')).to.equal(null);
+      expect(handleChange.callCount).to.equal(2);
+      expect(handleChange.firstCall.args[0]).to.equal(false);
+      expect(handleChange.secondCall.args[0]).to.equal(true);
+    });
+
+    it('should not call onChange when the open state does not change', async () => {
+      const handleChange = spy();
+
+      function App() {
+        const [open, setOpen] = React.useState(false);
+
+        return (
+          <Root
+            open={open}
+            onOpenChange={(nextOpen) => {
+              handleChange(open);
+              setOpen(nextOpen);
+            }}
+          >
+            <Trigger />
+            <PreviewCard.Positioner>
+              <PreviewCard.Popup>Content</PreviewCard.Popup>
+            </PreviewCard.Positioner>
+          </Root>
+        );
+      }
+
+      render(<App />);
+
+      expect(screen.queryByText('Content')).to.equal(null);
+
+      const trigger = screen.getByRole('link');
+
+      fireEvent.mouseEnter(trigger);
+      fireEvent.mouseMove(trigger);
+
+      clock.tick(OPEN_DELAY);
+
+      await waitForPosition();
+
+      expect(screen.getByText('Content')).not.to.equal(null);
+      expect(handleChange.callCount).to.equal(1);
+      expect(handleChange.firstCall.args[0]).to.equal(false);
+    });
+  });
+
+  describe('prop: defaultOpen', () => {
+    clock.withFakeTimers();
+
+    it('should open when the component is rendered', async () => {
+      render(
+        <Root defaultOpen>
+          <Trigger />
+          <PreviewCard.Positioner>
+            <PreviewCard.Popup>Content</PreviewCard.Popup>
+          </PreviewCard.Positioner>
+        </Root>,
+      );
+
+      await waitForPosition();
+
+      expect(screen.getByText('Content')).not.to.equal(null);
+    });
   });
 });
