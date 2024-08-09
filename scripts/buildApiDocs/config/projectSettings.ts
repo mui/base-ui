4fb1dd1803e23080c08410a0e9a0c84da019de16
Michał Dudak <michal@mui.com>
2024-08-09 11:13:02 +0200

    Make docs:api pass

--- scripts/buildApiDocs/config/projectSettings.ts
+++ scripts/buildApiDocs/config/projectSettings.ts
@@ -3,13 +3,12 @@ import { ProjectSettings } from '@mui-internal/api-docs-builder';
 import findApiPages from '@mui-internal/api-docs-builder/utils/findApiPages';
 import { getBaseUiComponentInfo } from './getBaseUiComponentInfo';
 import { getBaseUiHookInfo } from './getBaseUiHookInfo';
-import { generateBaseUIApiPages } from './generateBaseUiApiPages';
-import { generateApiLinks } from './generateApiLinks';
 import { getComponentImports } from './getComponentImports';
 
 export const projectSettings: ProjectSettings = {
   output: {
     apiManifestPath: path.join(process.cwd(), 'docs/data/base/pagesApi.js'),
+    writeApiManifest: false,
   },
   typeScriptProjects: [
     {
@@ -26,17 +25,6 @@ export const projectSettings: ProjectSettings = {
   getHookImports: getComponentImports,
   translationLanguages: ['en'],
   skipComponent: () => false,
-  onCompleted: async () => {
-    await generateBaseUIApiPages();
-  },
-  onWritingManifestFile(builds, source) {
-    const apiLinks = generateApiLinks(builds);
-    if (apiLinks.length > 0) {
-      return `module.exports = ${JSON.stringify(apiLinks)}`;
-    }
-
-    return source;
-  },
   skipAnnotatingComponentDefinition: true,
   skipSlotsAndClasses: true,
   generateJsonFileOnly: true,
