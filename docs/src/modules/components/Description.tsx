74f60bf6e827e830ea895a1bf9e0392561379f03
colmtuite <colmtuite@gmail.com>
2024-08-13 16:04:35 +0100

    refactor description component

--- docs/src/modules/components/Description.tsx
+++ docs/src/modules/components/Description.tsx
@@ -3,5 +3,10 @@ import clsx from 'clsx';
 import classes from './Description.module.css';
 
 export function Description(props: React.ComponentProps<'p'>) {
-  return <p {...props} className={clsx(classes.root, props.className)} />;
+  return (
+    <p
+      {...props}
+      className={clsx('Text size-5 weight-1 color-gray', classes.root, props.className)}
+    />
+  );
 }
