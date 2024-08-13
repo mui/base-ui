55c59f96f4bd2b6522a86d561ae9edf90c77fd32
colmtuite <colmtuite@gmail.com>
2024-08-13 15:48:24 +0100

    basic ass homepage

--- docs/app/page.tsx
+++ docs/app/page.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
-      <h1>Base UI</h1>
+      <h1 className="Text size-8">
+        Accessible, composable, unstyled UI components for building high-quality React apps,
+        websites, and design systems.
+      </h1>
+      <Link href="/base-ui-react/getting-started/overview">Documentation</Link>
     </div>
   );
 }
