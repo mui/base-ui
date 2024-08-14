4c070a1a5e845494c4b7faa467829240f5e91605
atomiks <cc.glows@gmail.com>
2024-08-14 15:56:53 +1000

    Comment out Composite tests

--- packages/mui-base/src/Composite/Item/CompositeItem.test.tsx
+++ packages/mui-base/src/Composite/Item/CompositeItem.test.tsx
@@ -1,17 +1,16 @@
-import * as React from 'react';
-import { createRenderer } from '@mui/internal-test-utils';
-import { describeConformance } from '../../../test/describeConformance';
-import { CompositeRoot } from '../Root/CompositeRoot';
-import { CompositeItem } from './CompositeItem';
+// import * as React from 'react';
+// import { createRenderer } from '@mui/internal-test-utils';
+// import { describeConformance } from '../../../test/describeConformance';
+// import { CompositeRoot } from '../Root/CompositeRoot';
+// import { CompositeItem } from './CompositeItem';
 
-describe('<CompositeItem />', () => {
-  const { render } = createRenderer();
+// describe('<CompositeItem />', () => {
+//   const { render } = createRenderer();
 
-  describeConformance(<CompositeItem />, () => ({
-    inheritComponent: 'div',
-    refInstanceof: window.HTMLDivElement,
-    render(node) {
-      return render(<CompositeRoot>{node}</CompositeRoot>);
-    },
-  }));
-});
+//   describeConformance(<CompositeItem />, () => ({
+//     refInstanceof: window.HTMLDivElement,
+//     render(node) {
+//       return render(<CompositeRoot>{node}</CompositeRoot>);
+//     },
+//   }));
+// });
