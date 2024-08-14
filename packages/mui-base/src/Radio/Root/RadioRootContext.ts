4dd8ea895648f4cde3e54b727a291bdcf964a7ac
atomiks <cc.glows@gmail.com>
2024-08-14 15:48:42 +1000

    Fix error throw

--- packages/mui-base/src/Radio/Root/RadioRootContext.ts
+++ packages/mui-base/src/Radio/Root/RadioRootContext.ts
@@ -5,7 +5,7 @@ export const RadioRootContext = React.createContext<RadioRootContext.Value | nul
 export function useRadioRootContext() {
   const value = React.useContext(RadioRootContext);
   if (value === null) {
-    throw new Error('RadioGroupIndicator component must be used within <RadioGroup.Item>');
+    throw new Error('Base UI: <Radio.Indicator> must be used within <Radio.Root>');
   }
   return value;
 }
