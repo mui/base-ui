9e8efad21c7f46f7e616bf904e02de38724dfa79
Michał Dudak <michal@mui.com>
2024-08-15 17:00:26 +0200

    Remove redirection

--- docs/app/(content)/[...slug]/page.tsx
+++ docs/app/(content)/[...slug]/page.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { redirect } from 'next/navigation';
 import { components } from 'docs-base/src/modules/common/MDXComponents';
 import { getMarkdownPage } from 'docs-base/src/utils/getMarkdownPage';
 import { TableOfContents } from 'docs-base/src/modules/common/TableOfContents';
@@ -18,12 +17,6 @@ export default async function DocsPage(props: Props) {
   const {
     params: { slug },
   } = props;
-
-  if (slug.length === 1) {
-    // such as /getting-started
-    const subpages = getSlugs(`/${slug[0]}`);
-    redirect(`/${slug[0]}/${subpages[0]}`);
-  }
 
   const { MDXContent, tableOfContents } = await getMarkdownPage(slug[0], slug[1]);
 
