24f698d94048c20199a89c3e0a59ffc5d8a008e2
sai6855 <fayazceg@gmail.com>
2024-08-26 15:21:23 +0530

    update openOnHover description

--- packages/mui-base/src/Menu/Root/MenuRoot.tsx
+++ packages/mui-base/src/Menu/Root/MenuRoot.tsx
@@ -139,6 +139,7 @@ namespace MenuRoot {
     delay?: number;
     /**
      * Whether the menu popup opens when the trigger is hovered after the provided `delay`.
+     * By default, openOnHover is set to `true` for nested menus.
      */
     openOnHover?: boolean;
   }
@@ -209,6 +210,7 @@ MenuRoot.propTypes /* remove-proptypes */ = {
   open: PropTypes.bool,
   /**
    * Whether the menu popup opens when the trigger is hovered after the provided `delay`.
+   * By default, openOnHover is set to `true` for nested menus.
    */
   openOnHover: PropTypes.bool,
   /**
