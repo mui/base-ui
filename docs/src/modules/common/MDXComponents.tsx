9e1b65463d6b78679d98cdfe34db75e78c6d66ab
Michał Dudak <michal@mui.com>
2024-08-27 12:32:06 +0200

    Fix the build even more

--- docs/src/modules/common/MDXComponents.tsx
+++ docs/src/modules/common/MDXComponents.tsx
@@ -1,15 +1,16 @@
 import * as React from 'react';
-import * as DocsComponents from 'docs-base/src/modules/components';
-import { Callout, type CalloutProps } from './Callout';
+import { ComponentLinkHeader, InstallationInstructions } from 'docs-base/src/modules/components';
+import { Callout } from 'docs-base/src/modules/common/Callout';
 import { PackageManagerSnippet, Npm, Pnpm, Yarn } from './PackageManagerSnippet';
 
 export const components = {
-  Callout: (props: CalloutProps) => <Callout {...props} />,
-  ComponentLinkHeader: () => <DocsComponents.ComponentLinkHeader />,
+  Callout,
+  ComponentLinkHeader,
   PackageManagerSnippet: (props: { children: React.ReactNode }) => (
     <PackageManagerSnippet {...props} />
   ),
   Npm: (props: React.PropsWithChildren<{}>) => <Npm>{props.children}</Npm>,
   Pnpm: (props: React.PropsWithChildren<{}>) => <Pnpm>{props.children}</Pnpm>,
   Yarn: (props: React.PropsWithChildren<{}>) => <Yarn>{props.children}</Yarn>,
+  InstallationInstructions,
 };
