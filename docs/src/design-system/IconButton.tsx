cf623290cb4bdf9d9dc313b5ec2d5b27290a0cd1
Michał Dudak <michal@mui.com>
2024-08-27 20:00:23 +0200

    Fix prop spreading

--- docs/src/design-system/IconButton.tsx
+++ docs/src/design-system/IconButton.tsx
@@ -6,12 +6,12 @@ import classes from './IconButton.module.css';
 import { Tooltip } from './Tooltip';
 
 export function IconButton(props: IconButton.Props) {
-  const { size = 1, label, withTooltip } = props;
+  const { size = 1, label, withTooltip, ...other } = props;
   const button = (
     <button
       type="button"
       aria-label={label}
-      {...props}
+      {...other}
       className={clsx(classes.root, classes[`size-${size}`], props.className)}
     />
   );
@@ -24,18 +24,18 @@ export function IconButton(props: IconButton.Props) {
 }
 
 export function IconLinkButton(props: IconLinkButton.Props) {
-  const { size = 1, useNextLink, label, withTooltip } = props;
+  const { size = 1, useNextLink, label, withTooltip, ...other } = props;
   const link = useNextLink ? (
     <Link
       aria-label={label}
-      {...props}
+      {...(other as React.ComponentPropsWithoutRef<typeof Link>)}
       className={clsx(classes.root, classes[`size-${size}`], props.className)}
     />
   ) : (
     // eslint-disable-next-line jsx-a11y/anchor-has-content
     <a
       aria-label={label}
-      {...props}
+      {...(other as React.ComponentPropsWithoutRef<'a'>)}
       className={clsx(classes.root, classes[`size-${size}`], props.className)}
     />
   );
