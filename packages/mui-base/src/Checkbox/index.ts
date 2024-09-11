f7f61ace7ed83f18ba169dc08ddb3ea07c146549
atomiks <cc.glows@gmail.com>
2024-09-11 11:44:05 +1000

    Update barrel files

--- packages/mui-base/src/Checkbox/index.ts
+++ packages/mui-base/src/Checkbox/index.ts
@@ -1,14 +1,2 @@
-'use client';
 export { CheckboxRoot as Root } from './Root/CheckboxRoot';
-export { useCheckboxRoot } from './Root/useCheckboxRoot';
-export { CheckboxContext } from './Root/CheckboxContext';
-export type {
-  CheckboxRootProps as RootProps,
-  CheckboxRootOwnerState as OwnerState,
-  CheckboxContextValue,
-  UseCheckboxRootParameters,
-  UseCheckboxRootReturnValue,
-} from './Root/CheckboxRoot.types';
-
 export { CheckboxIndicator as Indicator } from './Indicator/CheckboxIndicator';
-export type { CheckboxIndicatorProps as IndicatorProps } from './Indicator/CheckboxIndicator.types';
