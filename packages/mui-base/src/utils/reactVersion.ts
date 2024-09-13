a3468adb9d46eaa28048af8f6f603cc95ca3df26
Michał Dudak <michal@mui.com>
2024-09-13 09:52:49 +0200

    Use React.version

--- packages/mui-base/src/utils/reactVersion.ts
+++ packages/mui-base/src/utils/reactVersion.ts
@@ -1,23 +1,9 @@
 import * as React from 'react';
 
-let version = 17;
-
-if ('useId' in React) {
-  version = 18;
-}
-
-if ('use' in React) {
-  version = 19;
-}
-
-export const reactVersion = version;
+const majorVersion = parseInt(React.version.split('.')[0], 10);
 
 type SupportedVersions = 17 | 18 | 19;
 
-export function isReactVersion(reactVersionToCheck: SupportedVersions): boolean {
-  return reactVersion === reactVersionToCheck;
-}
-
 export function isReactVersionAtLeast(reactVersionToCheck: SupportedVersions): boolean {
-  return reactVersion >= reactVersionToCheck;
+  return majorVersion >= reactVersionToCheck;
 }
