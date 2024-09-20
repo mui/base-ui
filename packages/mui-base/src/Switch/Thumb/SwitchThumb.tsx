a7fa0a934149d4b002bdac48a6d772df4be57af0
Michał Dudak <michal@mui.com>
2024-09-20 12:05:01 +0200

    Create Thumb.OwnerState

--- packages/mui-base/src/Switch/Thumb/SwitchThumb.tsx
+++ packages/mui-base/src/Switch/Thumb/SwitchThumb.tsx
@@ -41,7 +41,9 @@ const SwitchThumb = React.forwardRef(function SwitchThumb(
 });
 
 namespace SwitchThumb {
-  export interface Props extends BaseUIComponentProps<'span', SwitchRoot.OwnerState> {}
+  export interface Props extends BaseUIComponentProps<'span', OwnerState> {}
+
+  export interface OwnerState extends SwitchRoot.OwnerState {}
 }
 
 SwitchThumb.propTypes /* remove-proptypes */ = {
