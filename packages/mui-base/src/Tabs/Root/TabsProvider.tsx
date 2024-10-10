bde120f25450dd75adb42c526c88caf20cc245fc
Michał Dudak <michal@mui.com>
2024-10-10 09:46:50 +0200

    [core] Add explicit type annotations to components (#705)

--- packages/mui-base/src/Tabs/Root/TabsProvider.tsx
+++ packages/mui-base/src/Tabs/Root/TabsProvider.tsx
@@ -22,7 +22,7 @@ export interface TabsProviderProps {
  *
  * @ignore - do not document.
  */
-function TabsProvider(props: TabsProviderProps) {
+const TabsProvider: React.FC<TabsProviderProps> = function TabsProvider(props) {
   const { value: valueProp, children } = props;
   const {
     direction,
@@ -76,7 +76,7 @@ function TabsProvider(props: TabsProviderProps) {
       <TabsRootContext.Provider value={tabsContextValue}>{children}</TabsRootContext.Provider>
     </CompoundComponentContext.Provider>
   );
-}
+};
 
 TabsProvider.propTypes /* remove-proptypes */ = {
   // ┌────────────────────────────── Warning ──────────────────────────────┐
