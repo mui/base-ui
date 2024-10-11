9abb78a39aade1484775d7e464461c3953ba97dc
Michał Dudak <michal@mui.com>
2024-10-11 11:53:31 +0200

    Fix types

--- packages/mui-base/test/createRenderer.ts
+++ packages/mui-base/test/createRenderer.ts
@@ -10,16 +10,20 @@ import {
 
 type BaseUITestRenderer = Omit<Renderer, 'render'> & {
   render: (
-    element: React.ReactElement<unknown>,
+    element: React.ReactElement<DataAttributes>,
     options?: RenderOptions,
   ) => Promise<MuiRenderResult>;
 };
+
+interface DataAttributes {
+  [key: `data-${string}`]: string;
+}
 
 export function createRenderer(globalOptions?: CreateRendererOptions): BaseUITestRenderer {
   const createRendererResult = sharedCreateRenderer(globalOptions);
   const { render: originalRender } = createRendererResult;
 
-  const render = async (element: React.ReactElement<unknown>, options?: RenderOptions) =>
+  const render = async (element: React.ReactElement<DataAttributes>, options?: RenderOptions) =>
     act(async () => {
       const result = await originalRender(element, options);
       await flushMicrotasks();
