006f8fdaac12d3879d358c35d97363cf7381c0db
Michał Dudak <michal@mui.com>
2024-10-14 10:33:05 +0200

    [core] Restore development playground (#701)

--- docs/src/blocks/GoogleTagManager.tsx
+++ docs/src/blocks/GoogleTagManager.tsx
@@ -8,29 +8,10 @@ interface GoogleTagManagerProps {
 export function GoogleTagManager(props: React.PropsWithChildren<GoogleTagManagerProps>) {
   const { id } = props;
   return (
-    <React.Fragment>
-      <script
-        // eslint-disable-next-line react/no-danger
-        dangerouslySetInnerHTML={{
-          __html: `
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-window.gtag = gtag;
-gtag('js', new Date());
-gtag('config', '${id}', {
-  send_page_view: false,
-});
-`,
-        }}
-      />
-      {/**
-       * A better alternative to <script async>, to delay its execution
-       * https://developer.chrome.com/blog/script-component/
-       */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${id}`}
-      />
-    </React.Fragment>
+    /**
+     * A better alternative to <script async>, to delay its execution
+     * https://developer.chrome.com/blog/script-component/
+     */
+    <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${id}`} />
   );
 }
