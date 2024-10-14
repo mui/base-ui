0898af8d3698f1f24dbc1287d27dcc8785476aa4
atomiks <cc.glows@gmail.com>
2024-10-14 20:12:54 +1100

    Migrate to new exports

--- packages/mui-base/src/ScrollArea/index.ts
+++ packages/mui-base/src/ScrollArea/index.ts
@@ -1,5 +1,1 @@
-export { ScrollAreaRoot as Root } from './Root/ScrollAreaRoot';
-export { ScrollAreaViewport as Viewport } from './Viewport/ScrollAreaViewport';
-export { ScrollAreaScrollbar as Scrollbar } from './Scrollbar/ScrollAreaScrollbar';
-export { ScrollAreaThumb as Thumb } from './Thumb/ScrollAreaThumb';
-export { ScrollAreaCorner as Corner } from './Corner/ScrollAreaCorner';
+export * as ScrollArea from './index.parts';
