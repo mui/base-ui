de962c97ec07558388ec586abed65d12b8494778
atomiks <cc.glows@gmail.com>
2024-10-15 21:03:05 +1100

    queryByRole

--- packages/mui-base/src/Select/Root/SelectRoot.test.tsx
+++ packages/mui-base/src/Select/Root/SelectRoot.test.tsx
@@ -140,7 +140,7 @@ describe('<Select.Root />', () => {
 
       await flushMicrotasks();
 
-      await waitFor(() => screen.getByRole('option', { name: 'b', hidden: false }));
+      await waitFor(() => screen.queryByRole('option', { name: 'b', hidden: false }));
 
       await user.click(screen.getByRole('option', { name: 'b', hidden: false }));
 
