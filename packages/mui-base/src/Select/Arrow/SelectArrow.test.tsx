d4144dc7d1ad0eb40f58a75378da11b5f09bc3a2
atomiks <cc.glows@gmail.com>
2024-10-21 17:50:40 +1100

    Debug tests

--- packages/mui-base/src/Select/Arrow/SelectArrow.test.tsx
+++ packages/mui-base/src/Select/Arrow/SelectArrow.test.tsx
@@ -1,18 +1,18 @@
-// import * as React from 'react';
-// import { Select } from '@base_ui/react/Select';
-// import { createRenderer, describeConformance } from '#test-utils';
+import * as React from 'react';
+import { Select } from '@base_ui/react/Select';
+import { createRenderer, describeConformance } from '#test-utils';
 
-// describe('<Select.Arrow />', () => {
-//   const { render } = createRenderer();
+describe('<Select.Arrow />', () => {
+  const { render } = createRenderer();
 
-//   describeConformance(<Select.Arrow />, () => ({
-//     refInstanceof: window.HTMLDivElement,
-//     render(node) {
-//       return render(
-//         <Select.Root open animated={false} alignOptionToTrigger={false}>
-//           <Select.Positioner>{node}</Select.Positioner>
-//         </Select.Root>,
-//       );
-//     },
-//   }));
-// });
+  describeConformance(<Select.Arrow />, () => ({
+    refInstanceof: window.HTMLDivElement,
+    render(node) {
+      return render(
+        <Select.Root open animated={false} alignOptionToTrigger={false}>
+          <Select.Positioner>{node}</Select.Positioner>
+        </Select.Root>,
+      );
+    },
+  }));
+});
--- packages/mui-base/src/Select/Icon/SelectIcon.test.tsx
+++ packages/mui-base/src/Select/Icon/SelectIcon.test.tsx
@@ -1,18 +1,18 @@
-// import * as React from 'react';
-// import { Select } from '@base_ui/react/Select';
-// import { createRenderer, describeConformance } from '#test-utils';
+import * as React from 'react';
+import { Select } from '@base_ui/react/Select';
+import { createRenderer, describeConformance } from '#test-utils';
 
-// describe('<Select.Icon />', () => {
-//   const { render } = createRenderer();
+describe('<Select.Icon />', () => {
+  const { render } = createRenderer();
 
-//   describeConformance(<Select.Icon />, () => ({
-//     refInstanceof: window.HTMLSpanElement,
-//     render(node) {
-//       return render(
-//         <Select.Root open animated={false}>
-//           {node}
-//         </Select.Root>,
-//       );
-//     },
-//   }));
-// });
+  describeConformance(<Select.Icon />, () => ({
+    refInstanceof: window.HTMLSpanElement,
+    render(node) {
+      return render(
+        <Select.Root open animated={false}>
+          {node}
+        </Select.Root>,
+      );
+    },
+  }));
+});
