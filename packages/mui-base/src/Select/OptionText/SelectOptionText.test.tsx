7107ab05abe19f0f7df1e0a1b883ef2107ab4eed
atomiks <cc.glows@gmail.com>
2024-10-21 17:25:59 +1100

    Debug tests

--- packages/mui-base/src/Select/OptionText/SelectOptionText.test.tsx
+++ packages/mui-base/src/Select/OptionText/SelectOptionText.test.tsx
@@ -1,20 +1,20 @@
-import * as React from 'react';
-import { Select } from '@base_ui/react/Select';
-import { createRenderer, describeConformance } from '#test-utils';
+// import * as React from 'react';
+// import { Select } from '@base_ui/react/Select';
+// import { createRenderer, describeConformance } from '#test-utils';
 
-describe('<Select.OptionText />', () => {
-  const { render } = createRenderer();
+// describe('<Select.OptionText />', () => {
+//   const { render } = createRenderer();
 
-  describeConformance(<Select.OptionText />, () => ({
-    refInstanceof: window.HTMLDivElement,
-    render(node) {
-      return render(
-        <Select.Root open animated={false}>
-          <Select.Positioner>
-            <Select.Option value="">{node}</Select.Option>
-          </Select.Positioner>
-        </Select.Root>,
-      );
-    },
-  }));
-});
+//   describeConformance(<Select.OptionText />, () => ({
+//     refInstanceof: window.HTMLDivElement,
+//     render(node) {
+//       return render(
+//         <Select.Root open animated={false}>
+//           <Select.Positioner>
+//             <Select.Option value="">{node}</Select.Option>
+//           </Select.Positioner>
+//         </Select.Root>,
+//       );
+//     },
+//   }));
+// });
