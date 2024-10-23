f9b4ab6eaea44b22d77909eabf6d90a78252e0de
Vlad Moroz <hi@vladmoroz.com>
2024-10-23 21:17:48 +0200

    [docs] Set up fonts and other base styles (#759)

---
+++ docs/src/app/new/layout.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import 'docs/src/styles.css';
+
+export default function Layout({ children }: React.PropsWithChildren) {
+  return children;
+}
--- docs/src/app/new/page.tsx
+++ docs/src/app/new/page.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import './styles.css';
 
 export default function Page() {
   return (
