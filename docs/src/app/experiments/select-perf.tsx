474bbb7af01680aeb59e219159c1ace351e0f5b8
atomiks <cc.glows@gmail.com>
2024-11-06 20:56:55 +1100

    Same perf demo

--- docs/src/app/experiments/select-perf.tsx
+++ docs/src/app/experiments/select-perf.tsx
@@ -5,23 +5,61 @@ import * as Select from '@radix-ui/react-select';
 
 const options = [...Array(1000)].map((_, i) => `Item ${i + 1}`);
 
+const arrowStyles: React.CSSProperties = {
+  width: 'calc(100% - 2px)',
+  margin: '0 auto',
+  textAlign: 'center',
+  background: 'white',
+  fontSize: 12,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginInline: 1,
+};
+
 function BaseSelectExample() {
   return (
     <BaseSelect.Root>
       <BaseSelect.Trigger
         aria-label="Select"
-        style={{ fontSize: 16, border: 'none' }}
+        style={{ fontSize: 16, border: 'none', lineHeight: 1, fontFamily: 'Arial' }}
       >
         <BaseSelect.Value placeholder="Select..." />
       </BaseSelect.Trigger>
       <BaseSelect.Positioner style={{ margin: '10px 0' }}>
-        <BaseSelect.Popup>
+        <BaseSelect.ScrollUpArrow
+          style={{
+            ...arrowStyles,
+            top: 0,
+            marginTop: 1,
+          }}
+        />
+        <BaseSelect.Popup
+          style={{
+            padding: '1rem 0.5rem',
+            border: '1px solid black',
+            background: 'white',
+            fontSize: 16,
+            fontFamily: 'Arial',
+          }}
+        >
           {options.map((item) => (
-            <BaseSelect.Option key={item} value={item}>
+            <BaseSelect.Option
+              key={item}
+              value={item}
+              style={{ scrollMargin: 15, lineHeight: 1 }}
+            >
               <BaseSelect.OptionText>{item}</BaseSelect.OptionText>
             </BaseSelect.Option>
           ))}
         </BaseSelect.Popup>
+        <BaseSelect.ScrollDownArrow
+          style={{
+            ...arrowStyles,
+            bottom: 0,
+            marginBottom: 1,
+          }}
+        />
       </BaseSelect.Positioner>
     </BaseSelect.Root>
   );
@@ -64,7 +102,7 @@ const SelectItem = React.forwardRef(
 export default function SelectPerf() {
   return (
     <React.Fragment>
-      <h2>Base UI Select</h2>
+      <h2>Base UI Select 2</h2>
       <BaseSelectExample />
       <h2>Radix UI Select</h2>
       <RadixSelectExample />
