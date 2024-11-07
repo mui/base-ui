f5b07ee37f84e6805a89669755c62bb2d3b9dd4e
Vlad Moroz <hi@vladmoroz.com>
2024-11-07 14:14:03 +0100

    [docs] Quick nav and base content styles (#804)

--- docs/src/app/new/(content)/layout.tsx
+++ docs/src/app/new/(content)/layout.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 
 export default function Layout({ children }: React.PropsWithChildren) {
   return (
-    <div className="px-8 pb-24 pt-8">
-      <div className="mx-auto max-w-[768px]">{children}</div>
+    <div className="px-8 pt-12 pb-20">
+      <div className="relative mx-auto max-w-[768px]">{children}</div>
     </div>
   );
 }
