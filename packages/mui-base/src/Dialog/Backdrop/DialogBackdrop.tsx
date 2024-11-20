432d9313fc3983208e2ff0158b511fa97139143a
Michał Dudak <michal@mui.com>
2024-11-20 11:31:42 +0100

    docs

--- packages/mui-base/src/Dialog/Backdrop/DialogBackdrop.tsx
+++ packages/mui-base/src/Dialog/Backdrop/DialogBackdrop.tsx
@@ -101,6 +101,32 @@ DialogBackdrop.propTypes /* remove-proptypes */ = {
    */
   className: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
   /**
+   * The container element to which the backdrop is appended to.
+   * @default false
+   */
+  container: PropTypes.oneOfType([
+    (props, propName) => {
+      if (props[propName] == null) {
+        return new Error(`Prop '${propName}' is required but wasn't specified`);
+      }
+      if (typeof props[propName] !== 'object' || props[propName].nodeType !== 1) {
+        return new Error(`Expected prop '${propName}' to be of type Element`);
+      }
+      return null;
+    },
+    PropTypes.shape({
+      current: (props, propName) => {
+        if (props[propName] == null) {
+          return null;
+        }
+        if (typeof props[propName] !== 'object' || props[propName].nodeType !== 1) {
+          return new Error(`Expected prop '${propName}' to be of type Element`);
+        }
+        return null;
+      },
+    }),
+  ]),
+  /**
    * If `true`, the backdrop element is kept in the DOM when closed.
    *
    * @default false
