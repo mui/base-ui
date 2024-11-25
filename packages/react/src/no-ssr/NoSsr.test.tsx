83d68c8ec1662a8b839a7d531bcf3a84d47c2937
mnajdova <mnajdova@gmail.com>
2024-11-25 17:54:35 +0100

    no ssr

--- packages/react/src/no-ssr/NoSsr.test.tsx
+++ packages/react/src/no-ssr/NoSsr.test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { expect } from 'chai';
 import { createRenderer } from '@mui/internal-test-utils';
-import { NoSsr } from '@base-ui-components/react/NoSsr';
+import { NoSsr } from '@base-ui-components/react/no-ssr';
 
 describe('<NoSsr />', () => {
   const { render, renderToString } = createRenderer();
