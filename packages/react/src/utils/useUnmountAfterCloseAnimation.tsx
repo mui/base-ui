9de3d155c73e60d0fe2541edf96845f728421c96
Michał Dudak <michal@mui.com>
2024-11-27 21:12:02 +0100

    Tests

--- packages/react/src/utils/useUnmountAfterCloseAnimation.tsx
+++ packages/react/src/utils/useUnmountAfterCloseAnimation.tsx
@@ -3,6 +3,9 @@ import { useAnimationsFinished } from './useAnimationsFinished';
 import { useEnhancedEffect } from './useEnhancedEffect';
 import { useLatestRef } from './useLatestRef';
 
+/**
+ * Unmounts a component (by calling the provided `setMounted` function) when its CSS animation or transition completes.
+ */
 export function useUnmountAfterExitAnimation(parameters: useUnmountAfterExitAnimation.Parameters) {
   const {
     open,
@@ -41,10 +44,29 @@ export function useUnmountAfterExitAnimation(parameters: useUnmountAfterExitAnim
 
 export namespace useUnmountAfterExitAnimation {
   export interface Parameters {
+    /**
+     * Determines if the component is open.
+     * The logic runs when the component goes from open to closed.
+     */
     open: boolean;
+    /**
+     * Determines if the animations are enabled.
+     * If set to `false`, `setMounted(false)` is set immediately after `open` turns `false` (in the next layout effect).
+     */
     animated: boolean;
+    /**
+     * Ref to the element being closed.
+     */
     animatedElementRef: React.RefObject<HTMLElement | null>;
+    /**
+     * Function to call when the element should be unmounted.
+     * It is always called with a single parameter: `false`.
+     */
     setMounted: (mounted: boolean) => void;
+    /**
+     * If `true`, forces React to flush all updates immediately after calling `setMounted`
+     * by using `flushSync` function.
+     */
     flushUpdatesSynchronously?: boolean;
   }
 }
