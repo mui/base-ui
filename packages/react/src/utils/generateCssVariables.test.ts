44cc34e6f2d6c5a6d8673cea404549b1f34fc005
mnajdova <mnajdova@gmail.com>
2024-11-27 09:51:52 +0100

    use deep equal

--- packages/react/src/utils/generateCssVariables.test.ts
+++ packages/react/src/utils/generateCssVariables.test.ts
@@ -3,10 +3,10 @@ import { generateCssVariables } from './generateCssVariables';
 
 describe('generateCssVariables', () => {
   it('converts the string to a CSS variables', () => {
-    expect(generateCssVariables(['width'])).to.equal({ width: '--width' });
+    expect(generateCssVariables(['width'])).to.deep.equal({ width: '--width' });
   });
 
   it('converts correctly multi-word strings to CSS variable', () => {
-    expect(generateCssVariables(['maxWidth'])).to.equal({ maxWidth: '--max-width' });
+    expect(generateCssVariables(['maxWidth'])).to.deep.equal({ maxWidth: '--max-width' });
   });
 });
