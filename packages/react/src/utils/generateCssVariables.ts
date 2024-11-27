c38de9750da3576e022b67e82d75664089cf0264
mnajdova <mnajdova@gmail.com>
2024-11-27 09:31:20 +0100

    fix

--- packages/react/src/utils/generateCssVariables.ts
+++ packages/react/src/utils/generateCssVariables.ts
@@ -2,7 +2,7 @@ export function generateCssVariables<T extends string>(cssVars: T[]): Record<T, 
   const result: Record<string, string> = {};
 
   cssVars.forEach((cssVar) => {
-    result[cssVar] = cssVar.replace(/([A-Z])/g, '-$1').toLowerCase();
+    result[cssVar] = `--${cssVar.replace(/([A-Z])/g, '-$1').toLowerCase()}`;
   });
 
   return result;
