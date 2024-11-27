c18afeefc4f89a3c479b5cf2cb0b8444b403085e
Vlad Moroz <hi@vladmoroz.com>
2024-11-27 10:22:01 +0100

    Feels good checkpoint

--- docs/src/components/Header.tsx
+++ docs/src/components/Header.tsx
@@ -1,16 +1,34 @@
 import * as React from 'react';
 import NextLink from 'next/link';
 import { GitHubIcon } from 'docs/src/icons/GitHub';
-import { HeaderHamburgerNav } from './HeaderHamburgerNav';
+import * as MobileNav from './MobileNav';
+import { nav } from '../nav';
 
 export function Header() {
   return (
     <div className="Header">
-      <NextLink href="/" className="HeaderLink text-base font-medium">
-        Base UI
+      <NextLink href="/" className="HeaderLink text-base font-bold tracking-wider">
+        BASE UI
       </NextLink>
       <div className="flex xs:hidden">
-        <HeaderHamburgerNav />
+        <MobileNav.Root>
+          <MobileNav.Trigger>Menu</MobileNav.Trigger>
+          <MobileNav.Backdrop />
+          <MobileNav.Popup>
+            {nav.map((section) => (
+              <MobileNav.Section key={section.label}>
+                <MobileNav.Heading>{section.label}</MobileNav.Heading>
+                <MobileNav.List>
+                  {section.links.map((link) => (
+                    <MobileNav.Item key={link.href} href={link.href}>
+                      {link.label}
+                    </MobileNav.Item>
+                  ))}
+                </MobileNav.List>
+              </MobileNav.Section>
+            ))}
+          </MobileNav.Popup>
+        </MobileNav.Root>
       </div>
       <div className="flex gap-6 max-xs:hidden">
         <span>v1.0.0-alpha.1</span>
---
+++ docs/src/components/MobileNav.tsx
@@ -0,0 +1,90 @@
+'use client';
+import * as React from 'react';
+import clsx from 'clsx';
+import NextLink from 'next/link';
+import { Dialog } from '@base-ui-components/react/dialog';
+
+type MobileNavState = [boolean, (open: boolean) => void];
+const MobileNavState = React.createContext<MobileNavState>([false, () => undefined]);
+
+export function Root(props: Dialog.Root.Props) {
+  const state = React.useState(false);
+  const [open, onOpenChange] = state;
+  return (
+    <MobileNavState.Provider value={state}>
+      <Dialog.Root open={open} onOpenChange={onOpenChange} {...props} />
+    </MobileNavState.Provider>
+  );
+}
+
+export const Trigger = Dialog.Trigger;
+
+export function Backdrop({ className, ...props }: Dialog.Backdrop.Props) {
+  return <Dialog.Backdrop className={clsx('MobileNavBackdrop', className)} {...props} />;
+}
+
+export function Popup({ children, className, ...props }: Dialog.Popup.Props) {
+  // const [open, setOpen] = React.useContext(MobileNavState);
+  return (
+    <Dialog.Popup className={clsx('MobileNavPopup', className)} {...props}>
+      <div className="MobileNavViewport">
+        <Dialog.Close className="MobileNavBackdropTapArea" tabIndex={-1} />
+        <nav aria-label="Main navigation" className="MobileNavPanel">
+          <div className="flex flex-col-reverse">
+            <div>{children}</div>
+            <div className="MobileNavCloseContainer">
+              <Dialog.Close aria-label="Close the navigation" className="MobileNavClose">
+                <svg
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M0.75 0.75L6 6M11.25 11.25L6 6M6 6L0.75 11.25M6 6L11.25 0.75"
+                    stroke="currentcolor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </Dialog.Close>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </Dialog.Popup>
+  );
+}
+
+export function Section({ className, ...props }: React.ComponentProps<'div'>) {
+  return <div className={clsx('MobileNavSection', className)} {...props} />;
+}
+
+export function Heading({ children, className, ...props }: React.ComponentProps<'div'>) {
+  return (
+    <div className={clsx('MobileNavHeading', className)} {...props}>
+      <div className="MobileNavHeadingInner">{children}</div>
+    </div>
+  );
+}
+
+export function List({ className, ...props }: React.ComponentProps<'ul'>) {
+  return <ul className={clsx('MobileNavList', className)} {...props} />;
+}
+
+interface ItemProps extends React.ComponentPropsWithoutRef<'li'> {
+  active?: boolean;
+  href: string;
+}
+
+export function Item({ children, className, href, ...props }: ItemProps) {
+  return (
+    <li className={clsx('MobileNavItem', className)} {...props}>
+      <NextLink className="MobileNavLink" href={href}>
+        {children}
+      </NextLink>
+    </li>
+  );
+}
