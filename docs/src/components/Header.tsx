41b840fea4b15d87a69cf281f73867813e24ccb9
Vlad Moroz <hi@vladmoroz.com>
2024-11-28 13:28:59 +0100

    Use larger font in the nav

--- docs/src/components/Header.tsx
+++ docs/src/components/Header.tsx
@@ -65,7 +65,7 @@ export function Header() {
                     <NpmIcon />
                     <span className="flex flex-grow-1 items-baseline justify-between">
                       npm package
-                      <span className="text-sm text-gray-600">{VERSION}</span>
+                      <span className="text-base text-gray-600">{VERSION}</span>
                     </span>
                   </MobileNav.Item>
                   <MobileNav.Item href="https://github.com/mui/base-ui" rel="noopener">
