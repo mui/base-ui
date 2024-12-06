de8e9ab72c10a437856c270b2a2463aa90b4583d
Vlad Moroz <hi@vladmoroz.com>
2024-12-06 17:33:22 +0100

    Slight changes

--- docs/src/app/new/(content)/components/slider/demos/hero/css-modules/index.tsx
+++ docs/src/app/new/(content)/components/slider/demos/hero/css-modules/index.tsx
@@ -5,7 +5,7 @@ import styles from './index.module.css';
 export default function ExampleSlider() {
   return (
     <Slider.Root defaultValue={25}>
-      <Slider.Control className={styles.Slider}>
+      <Slider.Control className={styles.Control}>
         <Slider.Track className={styles.Track}>
           <Slider.Indicator className={styles.Indicator} />
           <Slider.Thumb className={styles.Thumb} />
