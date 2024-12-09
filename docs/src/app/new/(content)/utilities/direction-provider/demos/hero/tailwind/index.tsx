845ff8bcf4af96c2739f183b77fe1908fe781da6
Vlad Moroz <hi@vladmoroz.com>
2024-12-09 16:15:20 +0100

    Remove temp slider styles

--- docs/src/app/new/(content)/utilities/direction-provider/demos/hero/tailwind/index.tsx
+++ docs/src/app/new/(content)/utilities/direction-provider/demos/hero/tailwind/index.tsx
@@ -8,8 +8,7 @@ export default function ExampleDirectionProvider() {
       <Slider.Root defaultValue={25}>
         <Slider.Control className="flex w-56 items-center py-3">
           <Slider.Track className="relative h-1 w-full rounded bg-gray-200 shadow-[inset_0_0_0_1px] shadow-gray-200">
-            {/* TODO remove relative when position absolute is removed */}
-            <Slider.Indicator className="!relative block rounded bg-gray-700" />
+            <Slider.Indicator className="rounded bg-gray-700" />
             <Slider.Thumb className="size-4 rounded-full bg-white outline outline-gray-300 focus-visible:outline-2 focus-visible:outline-blue-800" />
           </Slider.Track>
         </Slider.Control>
