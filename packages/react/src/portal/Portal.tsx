b28a528d6d2441c2c29c8caccb76dce2a06b6db8
atomiks <cc.glows@gmail.com>
2024-12-09 15:17:08 +1100

    Reorder types

--- packages/react/src/portal/Portal.tsx
+++ packages/react/src/portal/Portal.tsx
@@ -21,26 +21,6 @@ const Portal: Portal = function Portal(props: Portal.Props) {
   return <FloatingPortal root={container}>{children}</FloatingPortal>;
 };
 
-Portal.propTypes /* remove-proptypes */ = {
-  // ┌────────────────────────────── Warning ──────────────────────────────┐
-  // │ These PropTypes are generated from the TypeScript type definitions. │
-  // │ To update them, edit the TypeScript types and run `pnpm proptypes`. │
-  // └─────────────────────────────────────────────────────────────────────┘
-  /**
-   * @ignore
-   */
-  children: PropTypes.node,
-  /**
-   * The container to render the portal element into.
-   */
-  container: PropTypes /* @typescript-to-proptypes-ignore */.oneOfType([HTMLElementType, refType]),
-  /**
-   * Whether to keep the portal mounted in the DOM when the popup is closed.
-   * @default false
-   */
-  keepMounted: PropTypes.bool,
-} as any;
-
 namespace Portal {
   export interface Props {
     children?: React.ReactNode;
@@ -62,5 +42,25 @@ interface Portal {
   (props: Portal.Props): React.JSX.Element | null;
   propTypes?: any;
 }
+
+Portal.propTypes /* remove-proptypes */ = {
+  // ┌────────────────────────────── Warning ──────────────────────────────┐
+  // │ These PropTypes are generated from the TypeScript type definitions. │
+  // │ To update them, edit the TypeScript types and run `pnpm proptypes`. │
+  // └─────────────────────────────────────────────────────────────────────┘
+  /**
+   * @ignore
+   */
+  children: PropTypes.node,
+  /**
+   * The container to render the portal element into.
+   */
+  container: PropTypes /* @typescript-to-proptypes-ignore */.oneOfType([HTMLElementType, refType]),
+  /**
+   * Whether to keep the portal mounted in the DOM when the popup is closed.
+   * @default false
+   */
+  keepMounted: PropTypes.bool,
+} as any;
 
 export { Portal };
