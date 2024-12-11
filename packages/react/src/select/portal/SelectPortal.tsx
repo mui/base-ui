5eab0177f5b4633840b1712c6eea64dc3ad56136
atomiks <cc.glows@gmail.com>
2024-12-11 18:30:46 +1100

    Consistent comment

--- packages/react/src/select/portal/SelectPortal.tsx
+++ packages/react/src/select/portal/SelectPortal.tsx
@@ -5,7 +5,7 @@ import { Portal } from '../../portal/Portal';
 import { HTMLElementType, refType } from '../../utils/proptypes';
 
 /**
- * Renders a portal element into the DOM.
+ * Renders a portal element that moves its children to a different part of the DOM.
  *
  * Documentation: [Base UI Select](https://base-ui.com/react/components/select)
  */
