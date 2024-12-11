01aa994022c5782afa7dcdf4fe03948919365ffc
Marija Najdova <mnajdova@gmail.com>
2024-12-11 19:07:52 +0100

    [wip][docs] Document data attributes on all components (part 1) (#1042)

---
+++ packages/react/src/checkbox/indicator/CheckboxIndicatorDataAttributes.ts
@@ -0,0 +1,46 @@
+export enum CheckboxIndicatorDataAttributes {
+  /**
+   * Present when the checkbox is checked.
+   */
+  checked = 'data-checked',
+  /**
+   * Present when the checkbox is not checked.
+   */
+  unchecked = 'data-unchecked',
+  /**
+   * Present when the checkbox is disabled.
+   */
+  disabled = 'data-disabled',
+  /**
+   * Present when the checkbox is readonly.
+   */
+  readonly = 'data-readonly',
+  /**
+   * Present when the checkbox is required.
+   */
+  required = 'data-required',
+  /**
+   * Present when the checkbox indicator is animating in.
+   */
+  startingStyle = 'data-starting-style',
+  /**
+   * Present when the checkbox indicator is animating out.
+   */
+  endingStyle = 'data-ending-style',
+  /**
+   * Present when the checkbox is in valid state (when wrapped in Field.Root).
+   */
+  valid = 'data-valid',
+  /**
+   * Present when the checkbox is in invalid state (when wrapped in Field.Root).
+   */
+  invalid = 'data-invalid',
+  /**
+   * Present when the checkbox has been thouched (when wrapped in Field.Root).
+   */
+  touched = 'data-touched',
+  /**
+   * Present when the checkbox's value has changed (when wrapped in Field.Root).
+   */
+  dirty = 'data-dirty',
+}
---
+++ packages/react/src/menu/positioner/MenuPositionerDataAttributes.ts
@@ -0,0 +1,14 @@
+export enum MenuPositionerDataAttributes {
+  /**
+   * Present when the menu popup is open.
+   */
+  open = 'data-open',
+  /**
+   * Present when the menu popup is closed.
+   */
+  closed = 'data-unchecked',
+  /**
+   * Present when the anchor is hidden.
+   */
+  anchorHidden = 'data-anchor-hidden',
+}
