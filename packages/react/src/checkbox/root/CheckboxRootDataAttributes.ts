88cb8aad0b8b86cecc1df64807fb1417f2395e65
mnajdova <mnajdova@gmail.com>
2024-12-11 10:29:28 +0100

    checkbox

---
+++ packages/react/src/checkbox/root/CheckboxRootDataAttributes.ts
@@ -0,0 +1,22 @@
+export enum CheckboxRootDataAttributes {
+  /**
+   * Present when the checkbox is checked.
+   */
+  checked = 'data-checked',
+  /**
+   * Present when the checkbox is not checked.
+   */
+  unchecked = 'data-unchecked',
+  /**
+   * Present when the checkbox is disabled.
+   */
+  disabled = 'data-disabled',
+  /**
+   * Present when the checkbox is readonly.
+   */
+  readonly = 'data-readonly',
+  /**
+   * Present when the checkbox is required.
+   */
+  required = 'data-required',
+}
