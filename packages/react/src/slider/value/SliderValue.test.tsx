47b81d1bd9b4cb07907f3dac7147c1f9f2c8860a
Albert Yu <albert@albertyu.co>
2024-12-11 10:28:40 +0800

    Fix TS error

--- packages/react/src/slider/value/SliderValue.test.tsx
+++ packages/react/src/slider/value/SliderValue.test.tsx
@@ -9,7 +9,6 @@ import { NOOP } from '../../utils/noop';
 const testRootContext: SliderRootContext = {
   active: -1,
   areValuesEqual: () => true,
-  axis: 'horizontal',
   changeValue: NOOP,
   direction: 'ltr',
   dragging: false,
