a284a3f3056e02d8848f27dadc044a10cde1a1b4
mnajdova <mnajdova@gmail.com>
2024-12-11 09:17:20 +0100

    Dialog

---
+++ packages/react/src/dialog/popup/DialogPopupDataAttributes.ts
@@ -0,0 +1,27 @@
+export enum DialogPopupDataAttributes {
+  /**
+   * Present when the dialog is open.
+   */
+  open = 'data-open',
+  /**
+   * Present when the dialog is closed.
+   */
+  closed = 'data-closed',
+  /**
+   * Present when the dialog is animating in.
+   */
+  startingStyle = 'data-starting-style',
+  /**
+   * Present when the dialog is animating out.
+   */
+  endingStyle = 'data-ending-style',
+  /**
+   * Present when the dialog is modal.
+   */
+  modal = 'data-modal',
+  /**
+   * Indicates how many dialogs are nested within.
+   * @type {number}
+   */
+  nestedDialogs = 'data-nested-dialogs',
+}
---
+++ packages/react/src/dialog/trigger/DialogTriggerDataAttributes.ts
@@ -0,0 +1,10 @@
+export enum DialogTriggerDataAttributes {
+  /**
+   * Present when the corresponding dialog is modal.
+   */
+  modal = 'data-modal',
+  /**
+   * Present when the corresponding dialog is open.
+   */
+  popupOpen = 'data-popup-open',
+}
