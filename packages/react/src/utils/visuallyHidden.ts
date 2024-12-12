7d35c1cfe65750469b2374b103f1e2e4350a880e
Vlad Moroz <hi@vladmoroz.com>
2024-12-12 22:38:30 +0100

    pretty

--- packages/react/src/utils/visuallyHidden.ts
+++ packages/react/src/utils/visuallyHidden.ts
@@ -1,15 +1,15 @@
 import * as React from 'react';
 
 export const visuallyHidden: React.CSSProperties = {
-  border: 0,
   clip: 'rect(0 0 0 0)',
-  height: '1px',
-  margin: '-1px',
   overflow: 'hidden',
-  padding: 0,
+  whiteSpace: 'nowrap',
   position: 'fixed',
   top: 0,
   left: 0,
-  whiteSpace: 'nowrap',
-  width: '1px',
+  border: 0,
+  padding: 0,
+  width: 1,
+  height: 1,
+  margin: -1,
 };
