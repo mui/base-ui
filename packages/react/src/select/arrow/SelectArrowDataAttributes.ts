c9373d2583113365c4e3228fce6188b0efe768b9
mnajdova <mnajdova@gmail.com>
2024-12-12 15:03:41 +0100

    select

---
+++ packages/react/src/select/arrow/SelectArrowDataAttributes.ts
@@ -0,0 +1,19 @@
+export enum SelectArrowDataAttributes {
+  /**
+   * Present when the select popup is open.
+   */
+  open = 'data-open',
+  /**
+   * Present when the select popup is closed.
+   */
+  closed = 'data-unchecked',
+  /**
+   * Present when the anchor is hidden.
+   */
+  anchorHidden = 'data-anchor-hidden',
+  /**
+   * Indicates which side the select is positioned relative to the trigger.
+   * @type {'none' | 'top' | 'bottom' | 'left' | 'right' | 'inline-end' | 'inline-start'}
+   */
+  side = 'data-side',
+}
