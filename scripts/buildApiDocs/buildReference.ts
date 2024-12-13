0aa5e73344cab5b44f8e426db521e99ec349ef4d
Albert Yu <albert@albertyu.co>
2024-12-13 20:51:38 +0800

    Do not delete everything when using docs:api --grep

--- scripts/buildApiDocs/buildReference.ts
+++ scripts/buildApiDocs/buildReference.ts
@@ -10,6 +10,7 @@ const TEMP_PROP_DEFS_GLOB = join(process.cwd(), 'docs/reference/temp/components/
 const TEMP_DESCRIPTIONS_GLOB = join(process.cwd(), 'docs/reference/temp/translations/**/*.json');
 const COMMON_OVERRIDES_JSON = join(process.cwd(), 'docs/reference/overrides/common.json');
 const OVERRIDES_GLOB = join(process.cwd(), 'docs/reference/overrides/*.json');
+const GENERATED_GLOB = join(process.cwd(), 'docs/reference/generated/*.json');
 const FINAL_DIR = join(process.cwd(), 'docs/reference/generated');
 
 interface CommonOverrides {
@@ -17,12 +18,26 @@ interface CommonOverrides {
   types: Record<string, string>;
 }
 
-export async function buildReference() {
-  if (existsSync(FINAL_DIR)) {
-    rmSync(FINAL_DIR, { recursive: true });
-  }
+export async function buildReference(grep: RegExp | null = null) {
+  if (grep == null) {
+    if (existsSync(FINAL_DIR)) {
+      rmSync(FINAL_DIR, { recursive: true });
+    }
 
-  mkdirSync(FINAL_DIR);
+    mkdirSync(FINAL_DIR);
+  } else {
+    if (!existsSync(FINAL_DIR)) {
+      mkdirSync(FINAL_DIR);
+    }
+    // the targeted files are all lowercased
+    const caseInsensitiveGrep = new RegExp(grep, 'i');
+
+    for (const pathname of globSync(GENERATED_GLOB)) {
+      if (caseInsensitiveGrep.test(pathname)) {
+        rmSync(pathname);
+      }
+    }
+  }
 
   const commonOverrides: CommonOverrides = JSON.parse(readFileSync(COMMON_OVERRIDES_JSON, 'utf-8'));
   const overrides: ComponentDef[] = globSync(OVERRIDES_GLOB).map((pathname) =>
