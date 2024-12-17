c2f353fc9a78c424ba7fad22462ec97b4d72d928
Albert Yu <albert@albertyu.co>
2024-12-17 16:11:06 +0800

    Use resolve.alias to docs

--- test/regressions/vite.config.js
+++ test/regressions/vite.config.js
@@ -1,15 +1,16 @@
-import { join } from 'path';
+import * as path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: join(process.cwd(), 'test/regressions'),
+  root: path.join(process.cwd(), 'test/regressions'),
   mode: process.env.NODE_ENV || 'development',
   plugins: [react()],
 
   resolve: {
     alias: {
+      docs: path.resolve(__dirname, '../../docs'),
       fs: null,
       stream: null,
       zlib: null,
