3249a2d7c62472873170b2830a9a3b2731d8e550
Vlad Moroz <hi@vladmoroz.com>
2024-12-17 16:23:47 +0100

    [docs] Nested dialog demo (#1146)

---
+++ docs/src/app/(public)/(content)/react/components/dialog/demos/nested/css-modules/index.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { Dialog } from '@base-ui-components/react/dialog';
+import styles from './index.module.css';
+
+export default function ExampleDialog() {
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger className={styles.Button}>View notifications</Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Backdrop className={styles.Backdrop} />
+        <Dialog.Popup className={styles.Popup}>
+          <Dialog.Title className={styles.Title}>Notifications</Dialog.Title>
+          <Dialog.Description className={styles.Description}>
+            You are all caught up. Good job!
+          </Dialog.Description>
+          <div className={styles.Actions}>
+            <div className={styles.ActionsLeft}>
+              <Dialog.Root>
+                <Dialog.Trigger className={styles.GhostButton}>
+                  Customize
+                </Dialog.Trigger>
+                <Dialog.Portal>
+                  <Dialog.Popup className={styles.Popup}>
+                    <Dialog.Title className={styles.Title}>
+                      Customize notifications
+                    </Dialog.Title>
+                    <Dialog.Description className={styles.Description}>
+                      You can customize your notifications here.
+                    </Dialog.Description>
+                    <div className={styles.Actions}>
+                      <Dialog.Close className={styles.Button}>Close</Dialog.Close>
+                    </div>
+                  </Dialog.Popup>
+                </Dialog.Portal>
+              </Dialog.Root>
+            </div>
+
+            <Dialog.Close className={styles.Button}>Close</Dialog.Close>
+          </div>
+        </Dialog.Popup>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+}
