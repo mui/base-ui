176dffafdef679ba6d382efd1eeeba3165f0ffb0
Marija Najdova <mnajdova@gmail.com>
2024-12-18 17:07:02 +0100

    Update packages/react/src/alert-dialog/root/AlertDialogRootContext.ts
    Signed-off-by: Marija Najdova <mnajdova@gmail.com>

--- packages/react/src/alert-dialog/root/AlertDialogRootContext.ts
+++ packages/react/src/alert-dialog/root/AlertDialogRootContext.ts
@@ -8,7 +8,7 @@ export function useAlertDialogRootContext() {
   const context = React.useContext(DialogContext);
   if (context === undefined) {
     throw new Error(
-      'Base UI: AlertDialogRootContext is missing. Alert dialogs parts must be placed within <AlertDialog.Root>.',
+      'Base UI: AlertDialogRootContext is missing. AlertDialog parts must be placed within <AlertDialog.Root>.',
     );
   }
 
