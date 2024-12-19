c686b2c7fc39c673fce5aca043c0d4217c49cf7e
Olivier Tassinari <olivier.tassinari@gmail.com>
2024-12-19 14:01:25 +0100

    prettier

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -2,5 +2,5 @@ import baseline from '@mui/monorepo/stylelint.config.mjs';
 
 export default {
   ...baseline,
-  defaultSeverity: 'warning' // TODO, remove
+  defaultSeverity: 'warning', // TODO, remove
 };
