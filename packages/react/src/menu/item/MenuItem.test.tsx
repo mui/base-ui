aadf78f60b768b7656bfe2ece73765cca9c32e3c
Albert Yu <albert@albertyu.co>
2024-12-31 22:42:28 +0800

    Fix useButton enter key preventDefault for links

--- packages/react/src/menu/item/MenuItem.test.tsx
+++ packages/react/src/menu/item/MenuItem.test.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { expect } from 'chai';
 import { spy } from 'sinon';
+import { MemoryRouter, Route, Routes, Link, useLocation } from 'react-router-dom';
 import { act, screen, waitFor } from '@mui/internal-test-utils';
 import { FloatingRootContext, FloatingTree } from '@floating-ui/react';
 import { Menu } from '@base-ui-components/react/menu';
@@ -189,6 +190,73 @@ describe('<Menu.Item />', () => {
       await user.click(item);
 
       expect(queryByRole('menu')).not.to.equal(null);
+    });
+  });
+
+  describe('rendering links', () => {
+    function One() {
+      return <div>page one</div>;
+    }
+    function Two() {
+      return <div>page two</div>;
+    }
+    function LocationDisplay() {
+      const location = useLocation();
+      return <div data-testid="location">{location.pathname}</div>;
+    }
+
+    it('react-router <Link>', async () => {
+      const { getAllByRole, getByTestId, user } = await render(
+        <MemoryRouter initialEntries={['/']}>
+          <Routes>
+            <Route path="/" element={<One />} />
+            <Route path="/two" element={<Two />} />
+          </Routes>
+
+          <LocationDisplay />
+
+          <Menu.Root open>
+            <Menu.Positioner>
+              <Menu.Popup>
+                <Menu.Item render={<Link to="/" />}>link 1</Menu.Item>
+                <Menu.Item render={<Link to="/two" />}>link 2</Menu.Item>
+              </Menu.Popup>
+            </Menu.Positioner>
+          </Menu.Root>
+        </MemoryRouter>,
+      );
+
+      const [link1, link2] = getAllByRole('menuitem');
+
+      const locationDisplay = getByTestId('location');
+
+      expect(screen.getByText(/page one/i)).not.to.equal(null);
+
+      expect(locationDisplay).to.have.text('/');
+
+      await act(async () => {
+        link2.focus();
+      });
+
+      await waitFor(() => {
+        expect(link2).toHaveFocus();
+      });
+
+      await user.keyboard('[Enter]');
+
+      expect(locationDisplay).to.have.text('/two');
+
+      expect(screen.getByText(/page two/i)).not.to.equal(null);
+
+      await act(async () => {
+        link1.focus();
+      });
+
+      await user.keyboard('[Enter]');
+
+      expect(screen.getByText(/page one/i)).not.to.equal(null);
+
+      expect(locationDisplay).to.have.text('/');
     });
   });
 });
