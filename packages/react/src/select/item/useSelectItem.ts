7de80748f30e0e88c8c6a417dfc4c5f28c2331b3
atomiks <cc.glows@gmail.com>
2025-01-03 19:01:02 +1100

    [Select] Fix highlight flash on Safari (#1233)

--- packages/react/src/select/item/useSelectItem.ts
+++ packages/react/src/select/item/useSelectItem.ts
@@ -64,9 +64,25 @@ export function useSelectItem(params: useSelectItem.Parameters): useSelectItem.R
               prevPopupHeightRef.current = popupRef.current.offsetHeight;
             }
           },
-          onMouseLeave() {
+          onMouseLeave(event) {
             const popup = popupRef.current;
             if (!popup || !open) {
+              return;
+            }
+
+            const targetRect = event.currentTarget.getBoundingClientRect();
+
+            // Safari randomly fires `mouseleave` incorrectly when the item is
+            // aligned to the trigger. This is a workaround to prevent the highlight
+            // from being removed while the cursor is still within the bounds of the item.
+            // https://github.com/mui/base-ui/issues/869
+            const isWithinBounds =
+              targetRect.top + 1 <= event.clientY &&
+              event.clientY <= targetRect.bottom - 1 &&
+              targetRect.left + 1 <= event.clientX &&
+              event.clientX <= targetRect.right - 1;
+
+            if (isWithinBounds) {
               return;
             }
 
