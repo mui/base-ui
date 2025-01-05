44f0557d341acf7f73bea8e668085dbac9d44e0d
onehanddev <hakimuddinhh@gmail.com>
2025-01-05 20:03:23 +0530

    fix: ts errors

--- packages/react/src/menu/item/MenuItem.test.tsx
+++ packages/react/src/menu/item/MenuItem.test.tsx
@@ -13,6 +13,7 @@ const testRootContext: MenuRootContext = {
   getPopupProps: (p) => ({ ...p }),
   getTriggerProps: (p) => ({ ...p }),
   getItemProps: (p) => ({ ...p }),
+  setActiveIndex: () => {},
   parentContext: undefined,
   nested: false,
   setTriggerElement: () => {},
