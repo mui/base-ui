31cea62c2c33b4c98e4534f7292fd32265eb2cb1
atomiks <cc.glows@gmail.com>
2025-01-08 17:47:21 +1100

    [Tooltip, PreviewCard] Use `FloatingPortalLite` (#1278)

---
+++ packages/react/src/utils/FloatingPortalLite.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { useFloatingPortalNode } from '@floating-ui/react';
+import * as ReactDOM from 'react-dom';
+
+/**
+ * `FloatingPortal` includes tabbable logic handling for focus management.
+ * For components that don't need tabbable logic, use `FloatingPortalLite`.
+ * @ignore - internal component.
+ */
+export function FloatingPortalLite(props: FloatingPortalLite.Props) {
+  const node = useFloatingPortalNode({ root: props.root });
+  return node && ReactDOM.createPortal(props.children, node);
+}
+
+namespace FloatingPortalLite {
+  export interface Props {
+    children?: React.ReactNode;
+    root?: HTMLElement | null | React.RefObject<HTMLElement | null>;
+  }
+}
