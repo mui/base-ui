7c25836d0db01199b8ff34addb1f3a8535002383
onehanddev <hakimuddinhh@gmail.com>
2025-01-16 19:19:10 +0530

    fix: added user and touch selection to none for all elements used in the slider component

--- docs/src/app/(public)/(content)/react/components/slider/demos/hero/tailwind/index.tsx
+++ docs/src/app/(public)/(content)/react/components/slider/demos/hero/tailwind/index.tsx
@@ -4,10 +4,10 @@ import { Slider } from '@base-ui-components/react/slider';
 export default function ExampleSlider() {
   return (
     <Slider.Root defaultValue={25}>
-      <Slider.Control className="flex w-56 items-center py-3">
+      <Slider.Control className="flex w-56 touch-none items-center py-3 select-none">
         <Slider.Track className="h-1 w-full touch-none rounded bg-gray-200 shadow-[inset_0_0_0_1px] shadow-gray-200 select-none">
-          <Slider.Indicator className="rounded bg-gray-700" />
-          <Slider.Thumb className="size-4 rounded-full bg-white outline outline-1 outline-gray-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-blue-800" />
+          <Slider.Indicator className="touch-none rounded bg-gray-700 select-none" />
+          <Slider.Thumb className="size-4 touch-none rounded-full bg-white outline outline-1 outline-gray-300 select-none focus-visible:outline focus-visible:outline-2 focus-visible:outline-blue-800" />
         </Slider.Track>
       </Slider.Control>
     </Slider.Root>
