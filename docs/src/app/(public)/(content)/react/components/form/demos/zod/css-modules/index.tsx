2c46a69255b99da28d455522d51e984b7107349b
atomiks <cc.glows@gmail.com>
2025-01-29 17:01:21 +1100

    Add example

---
+++ docs/src/app/(public)/(content)/react/components/form/demos/zod/css-modules/index.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { z } from 'zod';
+import { Field } from '@base-ui-components/react/field';
+import { Form } from '@base-ui-components/react/form';
+import styles from './index.module.css';
+
+const schema = z.object({
+  name: z.string().min(1),
+  age: z.coerce.number().positive(),
+});
+
+function handleRequest(event: React.FormEvent<HTMLFormElement>) {
+  const formData = new FormData(event.currentTarget);
+  const result = schema.safeParse(Object.fromEntries(formData as any));
+
+  if (!result.success) {
+    return result.error.flatten().fieldErrors;
+  }
+
+  return {};
+}
+
+export default function Page() {
+  const [errors, setErrors] = React.useState({});
+
+  return (
+    <Form
+      className={styles.Form}
+      errors={errors}
+      onClearErrors={setErrors}
+      onSubmit={(event) => {
+        event.preventDefault();
+        const reqErrors = handleRequest(event);
+        setErrors(reqErrors);
+      }}
+    >
+      <Field.Root name="name" className={styles.Field}>
+        <Field.Label className={styles.Label}>Name</Field.Label>
+        <Field.Control placeholder="Enter name" className={styles.Input} />
+        <Field.Error className={styles.Error} />
+      </Field.Root>
+      <Field.Root name="age" className={styles.Field}>
+        <Field.Label className={styles.Label}>Age</Field.Label>
+        <Field.Control placeholder="Enter age" className={styles.Input} />
+        <Field.Error className={styles.Error} />
+      </Field.Root>
+      <button type="submit" className={styles.Button}>
+        Submit
+      </button>
+    </Form>
+  );
+}
---
+++ docs/src/app/(public)/(content)/react/components/form/demos/zod/tailwind/index.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import { z } from 'zod';
+import { Field } from '@base-ui-components/react/field';
+import { Form } from '@base-ui-components/react/form';
+
+const schema = z.object({
+  name: z.string().min(1),
+  age: z.coerce.number().positive(),
+});
+
+function handleRequest(event: React.FormEvent<HTMLFormElement>) {
+  const formData = new FormData(event.currentTarget);
+  const result = schema.safeParse(Object.fromEntries(formData as any));
+
+  if (!result.success) {
+    return result.error.flatten().fieldErrors;
+  }
+
+  return {};
+}
+
+export default function Page() {
+  const [errors, setErrors] = React.useState({});
+
+  return (
+    <Form
+      className="flex w-full max-w-64 flex-col gap-4"
+      errors={errors}
+      onClearErrors={setErrors}
+      onSubmit={(event) => {
+        event.preventDefault();
+        const reqErrors = handleRequest(event);
+        setErrors(reqErrors);
+      }}
+    >
+      <Field.Root name="name" className="flex flex-col items-start gap-1">
+        <Field.Label className="text-sm font-medium text-gray-900">Name</Field.Label>
+        <Field.Control
+          placeholder="Enter name"
+          className="h-10 w-full rounded-md border border-gray-200 pl-3.5 text-base text-gray-900 focus:outline focus:outline-2 focus:-outline-offset-1 focus:outline-blue-800"
+        />
+        <Field.Error className="text-sm text-red-800" />
+      </Field.Root>
+      <Field.Root name="age" className="flex flex-col items-start gap-1">
+        <Field.Label className="text-sm font-medium text-gray-900">Age</Field.Label>
+        <Field.Control
+          placeholder="Enter age"
+          className="h-10 w-full rounded-md border border-gray-200 pl-3.5 text-base text-gray-900 focus:outline focus:outline-2 focus:-outline-offset-1 focus:outline-blue-800"
+        />
+        <Field.Error className="text-sm text-red-800" />
+      </Field.Root>
+      <button
+        type="submit"
+        className="flex h-10 items-center justify-center rounded-md border border-gray-200 bg-gray-50 px-3.5 text-base font-medium text-gray-900 select-none hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:-outline-offset-1 focus-visible:outline-blue-800 active:bg-gray-100 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400"
+      >
+        Submit
+      </button>
+    </Form>
+  );
+}
