6f07894c160a4726b1a77bb6c464cc97e9304efa
Michał Dudak <michal@mui.com>
2025-02-03 11:58:38 +0100

    Elasting indicator option

--- docs/src/app/(private)/experiments/tabs.tsx
+++ docs/src/app/(private)/experiments/tabs.tsx
@@ -1,20 +1,29 @@
 'use client';
 /* eslint-disable react/no-danger */
 import * as React from 'react';
+import clsx from 'clsx';
 import { Tabs } from '@base-ui-components/react/tabs';
 import classes from './tabs.module.css';
 import { SettingsPanel } from './infra/SettingsPanel';
 
 interface Settings {
-  vertical: boolean;
+  orientation: 'horizontal' | 'vertical';
   activateOnFocus: boolean;
+  elasticIndicator: boolean;
 }
+
+const settingsMetadata = {
+  orientation: {
+    options: ['horizontal', 'vertical'],
+  },
+};
 
 export default function TabsExperiment() {
   const [value, setValue] = React.useState<string | number | null>(0);
   const [settings, setSettings] = React.useState<Settings>({
-    vertical: false,
+    orientation: 'horizontal',
     activateOnFocus: true,
+    elasticIndicator: false,
   });
 
   return (
@@ -24,7 +33,7 @@ export default function TabsExperiment() {
         className={classes.tabs}
         value={value}
         onValueChange={(val) => setValue(val)}
-        orientation={settings.vertical ? 'vertical' : 'horizontal'}
+        orientation={settings.orientation}
       >
         <Tabs.List
           className={classes.list}
@@ -45,7 +54,12 @@ export default function TabsExperiment() {
           <Tabs.Tab className={classes.tab} value={4}>
             Actions
           </Tabs.Tab>
-          <Tabs.Indicator className={classes.indicator} />
+          <Tabs.Indicator
+            className={clsx(
+              classes.indicator,
+              settings.elasticIndicator && classes.elastic,
+            )}
+          />
         </Tabs.List>
         <Tabs.Panel className={classes.panel} value={0} keepMounted>
           Code panel
@@ -64,125 +78,11 @@ export default function TabsExperiment() {
         </Tabs.Panel>
       </Tabs.Root>
 
-      <SettingsPanel settings={settings} onChange={setSettings} />
+      <SettingsPanel
+        settings={settings}
+        settingsMetadata={settingsMetadata}
+        onChange={setSettings}
+      />
     </div>
   );
 }
-
-/*
-
-const TabsList = styled(BaseTabs.List)(
-  ({ theme }) => css`
-    background-color: ${blue[500]};
-    border-radius: 12px;
-    display: flex;
-    flex-wrap: nowrap;
-    align-items: center;
-    justify-content: space-evenly;
-    box-shadow: 0 4px 30px ${theme.palette.mode === 'dark' ? grey[900] : grey[200]};
-    position: relative;
-
-    &[data-orientation='vertical'] {
-      flex-direction: column;
-    }
-  `,
-);
-
-const Indicator = styled(BaseTabs.Indicator)`
-  position: absolute;
-  inset: var(--active-tab-top) var(--active-tab-right) var(--active-tab-bottom)
-    var(--active-tab-left);
-  background: ${blue[800]};
-  border-radius: 8px;
-  z-index: 0;
-  box-shadow: 0 0 0 0 ${blue[200]};
-  outline-width: 0;
-
-  &[data-orientation='vertical'] {
-    inset: var(--active-tab-top) 6px var(--active-tab-bottom) 6px;
-  }
-
-  &[data-activation-direction='right'] {
-    transition:
-      left 0.6s 0.1s,
-      right 0.3s,
-      top 0.3s,
-      bottom 0.3s,
-      box-shadow 0.2s;
-  }
-
-  &[data-activation-direction='down'] {
-    transition:
-      top 0.6s 0.1s,
-      bottom 0.3s;
-    left: 0;
-    right: 0;
-    margin: 0 6px;
-  }
-
-  &[data-activation-direction='left'] {
-    transition:
-      left 0.3s,
-      right 0.6s 0.1s,
-      top 0.3s,
-      bottom 0.3s,
-      box-shadow 0.2s;
-  }
-
-  &[data-activation-direction='up'] {
-    transition:
-      top 0.3s,
-      bottom 0.6s 0.1s;
-    left: 0;
-    right: 0;
-    margin: 0 6px;
-  }
-
-  *:has(:focus-visible) > & {
-    box-shadow: 0 0 0 2px ${blue[200]};
-  }
-`;
-
-const Tab = styled(BaseTabs.Tab)`
-  font-family: 'IBM Plex Sans', sans-serif;
-  color: #fff;
-  cursor: pointer;
-  font-size: 0.875rem;
-  font-weight: 600;
-  background-color: transparent;
-  white-space: nowrap;
-  flex: 1 1 auto;
-  padding: 10px 12px;
-  margin: 6px;
-  border: none;
-  border-radius: 7px;
-  display: flex;
-  justify-content: center;
-  position: relative;
-  z-index: 1;
-
-  &:focus-visible {
-    outline: none;
-  }
-
-  &[data-disabled] {
-    opacity: 0.5;
-    cursor: not-allowed;
-  }
-`;
-
-const TabPanel = styled(BaseTabs.Panel)(
-  ({ theme }) => css`
-    width: 100%;
-    font-family: 'IBM Plex Sans', sans-serif;
-    font-size: 0.875rem;
-    padding: 20px 12px;
-    background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
-    border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
-    border-radius: 12px;
-    opacity: 0.6;
-    box-sizing: border-box;
-  `,
-);
-
-*/
