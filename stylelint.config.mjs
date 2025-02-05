479676f188247b4ee5090b3a5a1823717e417617
Michał Dudak <michal@mui.com>
2025-02-05 17:41:06 +0100

    Update rules

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -3,4 +3,21 @@ import baseline from '@mui/monorepo/stylelint.config.mjs';
 export default {
   ...baseline,
   defaultSeverity: 'warning', // TODO, remove
+  rules: {
+    ...baseline.rules,
+    // empty lines help with readability
+    'declaration-empty-line-before': null,
+    'custom-property-empty-line-before': null,
+    'comment-empty-line-before': null,
+    'at-rule-empty-line-before': null,
+
+    // our bundler understands the simple notation we use
+    'import-notation': null,
+
+    // makes it harder to change
+    'color-hex-length': null,
+
+    // Tailwind
+    'at-rule-no-unknown': [true, { ignoreAtRules: ['theme'] }],
+  },
 };
