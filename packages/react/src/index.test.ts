8de23b990955891ec2e2015ece3e27c51911677f
Michał Dudak <michal@mui.com>
2025-02-07 11:20:49 +0100

    [test] Verify if modules are exported from the root

---
+++ packages/react/src/index.test.ts
@@ -0,0 +1,43 @@
+/**
+ * Important: This test also serves as a point to
+ * import the entire lib for coverage reporting
+ */
+import { expect } from 'chai';
+import { describe, it } from 'vitest';
+import * as BaseUI from './index';
+import packageJson from '../package.json';
+
+describe('@base-ui-components/react', () => {
+  it('should have exports', () => {
+    expect(typeof BaseUI).to.equal('object');
+  });
+
+  it('should not have undefined exports', () => {
+    (Object.keys(BaseUI) as (keyof typeof BaseUI)[]).forEach((exportKey) => {
+      expect(Boolean(BaseUI[exportKey])).to.equal(true);
+    });
+  });
+
+  it('should have the correct root exports', async () => {
+    const subpathExports = packageJson.exports;
+
+    await Promise.all(
+      Object.keys(subpathExports)
+        .filter((key) => !['.', './utils'].includes(key) && !key.startsWith('./unstable-'))
+        .map(async (subpath) => {
+          const importSpecifier = `@base-ui-components/react/${subpath.replace('./', '')}`;
+          const module = await import(importSpecifier);
+
+          Object.keys(module).forEach((exportKey) => {
+            expect((BaseUI as Record<string, unknown>)[exportKey]).not.to.equal(
+              undefined,
+              `${exportKey} (from ${importSpecifier}) was not found in root exports`,
+            );
+          });
+        }),
+    );
+
+    // Ensure the utils are also exported
+    expect(BaseUI.utils).not.to.equal(undefined);
+  });
+});
