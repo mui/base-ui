166b66f706fcdbd18f98d940d4e382cae2b9540f
Albert Yu <albert@albertyu.co>
2025-02-13 18:40:11 +0800

    Update demos

--- docs/src/app/(private)/experiments/toolbar/_icons.tsx
+++ docs/src/app/(private)/experiments/toolbar/_icons.tsx
@@ -250,3 +250,54 @@ export function MinusIcon(props: React.ComponentProps<'svg'>) {
     </svg>
   );
 }
+
+export function BellIcon(props: React.ComponentProps<'svg'>) {
+  return (
+    <svg fill="currentcolor" width="20" height="20" viewBox="0 0 16 16" {...props}>
+      <path d="M 8 1 C 7.453125 1 7 1.453125 7 2 L 7 3.140625 C 5.28125 3.589844 4 5.144531 4 7 L 4 10.984375 C 4 10.984375 3.984375 11.261719 3.851563 11.519531 C 3.71875 11.78125 3.558594 12 3 12 L 3 13 L 13 13 L 13 12 C 12.40625 12 12.253906 11.78125 12.128906 11.53125 C 12.003906 11.277344 12 11.003906 12 11.003906 L 12 7 C 12 5.144531 10.71875 3.589844 9 3.140625 L 9 2 C 9 1.453125 8.546875 1 8 1 Z M 8 13 C 7.449219 13 7 13.449219 7 14 C 7 14.550781 7.449219 15 8 15 C 8.550781 15 9 14.550781 9 14 C 9 13.449219 8.550781 13 8 13 Z M 8 4 C 9.664063 4 11 5.335938 11 7 L 11 10.996094 C 11 10.996094 10.988281 11.472656 11.234375 11.96875 C 11.238281 11.980469 11.246094 11.988281 11.25 12 L 4.726563 12 C 4.730469 11.992188 4.738281 11.984375 4.742188 11.980469 C 4.992188 11.488281 5 11.015625 5 11.015625 L 5 7 C 5 5.335938 6.335938 4 8 4 Z" />
+    </svg>
+  );
+}
+
+export function TrashIcon(props: React.ComponentProps<'svg'>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      {...props}
+    >
+      <polyline points="3 6 5 6 21 6" />
+      <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+      <line x1="10" y1="11" x2="10" y2="17" />
+      <line x1="14" y1="11" x2="14" y2="17" />
+    </svg>
+  );
+}
+
+export function SlidersIcon(props: React.ComponentProps<'svg'>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      {...props}
+    >
+      <line x1="4" y1="21" x2="4" y2="14" />
+      <line x1="4" y1="10" x2="4" y2="3" />
+      <line x1="12" y1="21" x2="12" y2="12" />
+      <line x1="12" y1="8" x2="12" y2="3" />
+      <line x1="20" y1="21" x2="20" y2="16" />
+      <line x1="20" y1="12" x2="20" y2="3" />
+      <line x1="1" y1="14" x2="7" y2="14" />
+      <line x1="9" y1="8" x2="15" y2="8" />
+      <line x1="17" y1="16" x2="23" y2="16" />
+    </svg>
+  );
+}
--- docs/src/app/(private)/experiments/toolbar/triggers.tsx
+++ docs/src/app/(private)/experiments/toolbar/triggers.tsx
@@ -1,15 +1,27 @@
 'use client';
 import * as React from 'react';
 import { Toolbar } from '@base-ui-components/react/toolbar';
-// import { Toggle } from '@base-ui-components/react/toggle';
 import { Switch } from '@base-ui-components/react/switch';
+import { NumberField } from '@base-ui-components/react/number-field';
+import { Slider } from '@base-ui-components/react/slider';
+import { Tooltip } from '@base-ui-components/react/tooltip';
+import { Popover } from '@base-ui-components/react/popover';
 import { Dialog } from '@base-ui-components/react/dialog';
+import { AlertDialog } from '@base-ui-components/react/alert-dialog';
 import toolbarClasses from './toolbar.module.css';
 import triggerToolbarClasses from './triggers.module.css';
+import tooltipClasses from '../../../(public)/(content)/react/components/tooltip/demos/hero/css-modules/index.module.css';
 import switchClasses from '../../../(public)/(content)/react/components/switch/demos/hero/css-modules/index.module.css';
 import dialogClasses from '../../../(public)/(content)/react/components/alert-dialog/demos/hero/css-modules/index.module.css';
-// import popoverClasses from '../../../(public)/(content)/react/components/popover/demos/hero/css-modules/index.module.css';
-import { MessageCircleIcon } from './_icons';
+import popoverClasses from '../../../(public)/(content)/react/components/popover/demos/hero/css-modules/index.module.css';
+import comboSliderClasses from './slider.module.css';
+import {
+  SlidersIcon,
+  TrashIcon,
+  MessageCircleIcon,
+  ArrowSvg,
+  BellIcon,
+} from './_icons';
 import {
   SettingsMetadata,
   useExperimentSettings,
@@ -37,18 +49,59 @@ const styles = {
   toolbar: toolbarClasses,
   switch: switchClasses,
   dialog: dialogClasses,
+  popover: popoverClasses,
+  slider: comboSliderClasses,
+  tooltip: tooltipClasses,
 };
 
 const TEXT = `Shows toolbar buttons as various triggers:
 - Menu.Trigger
-- Dialog.Trigger
+- Dialog.Trigger / AlertDialog.Trigger
 - Popover.Trigger
+- Tooltip.Trigger
+- and a Switch
 `;
-
-const DISABLED = false;
 
 function classNames(...c: Array<string | undefined | null | false>) {
   return c.filter(Boolean).join(' ');
+}
+
+function renderTriggerWithTooltip(args: {
+  render: any;
+  key: string;
+  label: string;
+  // icon: (props: React.ComponentProps<'svg'>) => React.JSX.Element;
+  disabled?: boolean;
+}) {
+  const { render, key, label, disabled = false } = args;
+  return (
+    <Tooltip.Root key={key}>
+      <Tooltip.Trigger
+        render={
+          <Toolbar.Button
+            className={classNames(styles.toolbar.Toggle, styles.demo.Toggle)}
+            disabled={disabled}
+            render={render}
+          />
+        }
+      />
+      <Tooltip.Portal>
+        <Tooltip.Positioner sideOffset={10}>
+          <Tooltip.Popup className={styles.tooltip.Popup}>
+            <Tooltip.Arrow
+              className={classNames(
+                styles.tooltip.Arrow,
+                styles.toolbar.TooltipArrow,
+              )}
+            >
+              <ArrowSvg className={styles.toolbar.ArrowSvg} />
+            </Tooltip.Arrow>
+            {label}
+          </Tooltip.Popup>
+        </Tooltip.Positioner>
+      </Tooltip.Portal>
+    </Tooltip.Root>
+  );
 }
 
 export default function App() {
@@ -66,15 +119,15 @@ export default function App() {
       <div className={styles.toolbar.Wrapper}>
         <Toolbar.Root className={classNames(styles.toolbar.Root, styles.demo.Root)}>
           <Dialog.Root>
-            <Toolbar.Button
-              disabled={DISABLED}
-              className={classNames(styles.toolbar.Toggle, styles.demo.Toggle)}
-              render={
+            {renderTriggerWithTooltip({
+              render: (
                 <Dialog.Trigger>
                   <MessageCircleIcon className={styles.toolbar.Icon} />
                 </Dialog.Trigger>
-              }
-            />
+              ),
+              key: 'comment-dialog',
+              label: 'Add a comment',
+            })}
 
             <Dialog.Portal keepMounted>
               <Dialog.Backdrop className={styles.dialog.Backdrop} />
@@ -97,11 +150,139 @@ export default function App() {
 
           <Toolbar.Separator className={styles.demo.Separator} />
 
+          <Popover.Root>
+            <Tooltip.Root>
+              {renderTriggerWithTooltip({
+                render: (
+                  <Popover.Trigger>
+                    <SlidersIcon
+                      aria-label="RGB color picker"
+                      className={styles.popover.Icon}
+                    />
+                  </Popover.Trigger>
+                ),
+                key: 'rgb-color-picker',
+                label: 'RGB color picker',
+              })}
+
+              <Tooltip.Portal>
+                <Tooltip.Positioner sideOffset={10}>
+                  <Tooltip.Popup className={styles.tooltip.Popup}>
+                    <Tooltip.Arrow
+                      className={classNames(
+                        styles.tooltip.Arrow,
+                        styles.toolbar.TooltipArrow,
+                      )}
+                    >
+                      <ArrowSvg className={styles.toolbar.ArrowSvg} />
+                    </Tooltip.Arrow>
+                    RGB color picker
+                  </Tooltip.Popup>
+                </Tooltip.Positioner>
+              </Tooltip.Portal>
+            </Tooltip.Root>
+
+            <Popover.Portal>
+              <Popover.Positioner sideOffset={8}>
+                <Popover.Popup className={styles.popover.Popup}>
+                  <Popover.Arrow className={styles.popover.Arrow}>
+                    <ArrowSvg />
+                  </Popover.Arrow>
+                  <Popover.Title
+                    className={styles.popover.Title}
+                    style={{ marginBottom: '1rem' }}
+                  >
+                    RGB Color Picker
+                  </Popover.Title>
+                  <ComboSlider color="r" />
+                  <ComboSlider color="g" />
+                  <ComboSlider color="b" />
+                </Popover.Popup>
+              </Popover.Positioner>
+            </Popover.Portal>
+          </Popover.Root>
+
+          <Toolbar.Separator className={styles.demo.Separator} />
+
+          <Popover.Root>
+            {renderTriggerWithTooltip({
+              render: (
+                <Popover.Trigger>
+                  <BellIcon
+                    aria-label="Notifications"
+                    className={styles.popover.Icon}
+                  />
+                </Popover.Trigger>
+              ),
+              key: 'notifications',
+              label: 'Notifications',
+            })}
+
+            <Popover.Portal>
+              <Popover.Positioner sideOffset={8}>
+                <Popover.Popup className={styles.popover.Popup}>
+                  <Popover.Arrow className={styles.popover.Arrow}>
+                    <ArrowSvg />
+                  </Popover.Arrow>
+                  <Popover.Title className={styles.popover.Title}>
+                    Notifications
+                  </Popover.Title>
+                  <Popover.Description className={styles.popover.Description}>
+                    You are all caught up. Good job!
+                  </Popover.Description>
+                </Popover.Popup>
+              </Popover.Positioner>
+            </Popover.Portal>
+          </Popover.Root>
+
+          <Toolbar.Separator className={styles.demo.Separator} />
+
+          <AlertDialog.Root>
+            {renderTriggerWithTooltip({
+              render: (
+                <AlertDialog.Trigger>
+                  <TrashIcon className={styles.toolbar.Icon} />
+                </AlertDialog.Trigger>
+              ),
+              key: 'delete',
+              label: 'Delete',
+            })}
+
+            <AlertDialog.Portal keepMounted>
+              <AlertDialog.Backdrop className={styles.dialog.Backdrop} />
+              <AlertDialog.Popup className={styles.dialog.Popup}>
+                <AlertDialog.Title className={styles.dialog.Title}>
+                  Delete everything
+                </AlertDialog.Title>
+                <AlertDialog.Description className={styles.dialog.Description}>
+                  Are you sure? You cannot undo this.
+                </AlertDialog.Description>
+                <div className={styles.dialog.Actions}>
+                  <AlertDialog.Close className={styles.dialog.Button}>
+                    No, Cancel
+                  </AlertDialog.Close>
+                  <AlertDialog.Close
+                    data-color="red"
+                    className={styles.dialog.Button}
+                  >
+                    Yes, Delete
+                  </AlertDialog.Close>
+                </div>
+              </AlertDialog.Popup>
+            </AlertDialog.Portal>
+          </AlertDialog.Root>
+
+          <Toolbar.Separator className={styles.demo.Separator} />
+
           <Toolbar.Button
             disabled={settings.switchDisabled}
             className={classNames(styles.toolbar.Toggle, styles.demo.Toggle)}
             render={
-              <Switch.Root defaultChecked className={styles.switch.Switch}>
+              <Switch.Root
+                defaultChecked
+                className={styles.switch.Switch}
+                style={{ insetInline: '.5rem' }}
+              >
                 <Switch.Thumb
                   className={styles.switch.Thumb}
                   style={{ marginRight: 'auto' }}
@@ -120,5 +301,50 @@ export default function App() {
         />
       </div>
     </React.Fragment>
+  );
+}
+
+function ComboSlider({ color }: { color: 'r' | 'g' | 'b' }) {
+  const id = React.useId();
+  const [val, setVal] = React.useState(20);
+  return (
+    <div className={styles.slider.Row}>
+      <Slider.Root
+        value={val}
+        onValueChange={(newValue, event) => {
+          if (color === 'r') {
+            event.preventDefault();
+          }
+          setVal(newValue as number);
+        }}
+        className={styles.slider.Slider}
+        min={0}
+        max={255}
+      >
+        <Slider.Value className={styles.slider.Value} />
+        <Slider.Control className={styles.slider.Control}>
+          <Slider.Track className={styles.slider.Track}>
+            <Slider.Indicator
+              className={classNames(styles.slider.Indicator, styles.slider[color])}
+            />
+            <Slider.Thumb className={styles.slider.Thumb} />
+          </Slider.Track>
+        </Slider.Control>
+      </Slider.Root>
+      <NumberField.Root
+        id={id}
+        value={val}
+        onValueChange={(newValue) => {
+          if (newValue != null) {
+            setVal(newValue);
+          }
+        }}
+        className={styles.slider.Field}
+      >
+        <NumberField.Group className={styles.slider.Group}>
+          <NumberField.Input className={styles.slider.Input} />
+        </NumberField.Group>
+      </NumberField.Root>
+    </div>
   );
 }
