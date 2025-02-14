77ff4a44ad556bb5f8c71d9e1f61dd06c0abc9d3
mnajdova <mnajdova@gmail.com>
2025-02-14 13:14:57 +0100

    improve docs page

---
+++ docs/src/app/(public)/(content)/react/utils/use-renderer/demos/render/css-modules/index.tsx
@@ -0,0 +1,32 @@
+'use client';
+import * as React from 'react';
+import { useRenderer, RenderProp } from '@base-ui-components/react/use-renderer';
+import styles from './index.module.css';
+
+type TextProps = {
+  className?: string;
+  render?: RenderProp<Record<string, any>>;
+  children: React.ReactNode;
+};
+
+function Text(props: TextProps) {
+  const { render, ...otherProps } = props;
+
+  const { renderElement } = useRenderer({
+    render: render ?? <p />,
+    props: otherProps,
+  });
+
+  return renderElement();
+}
+
+export default function ExampleText() {
+  return (
+    <div>
+      <Text className={styles.Text}>Text component rendered as a paragraph tag</Text>
+      <Text className={styles.Text} render={<strong />}>
+        Text component rendered as a strong tag
+      </Text>
+    </div>
+  );
+}
