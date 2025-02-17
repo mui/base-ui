8c6d4e647a4ee6214a9d03eb1a3f9ec457f79066
Giorgos Kalogeropoulos <georgekalohome@gmail.com>
2025-02-17 09:19:13 +0200

    [Slider] Narrow the type of `value` in callbacks (#1241)
    Co-authored-by: Albert Yu <albert@albertyu.co>
    Co-authored-by: Michał Dudak <michal@mui.com>

---
+++ packages/react/src/slider/root/SliderRoot.spec.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { expectType } from '#test-utils';
+import { Slider } from '@base-ui-components/react/slider';
+
+const value: number = 25;
+const array = [25];
+
+const singleValueWithOnValueChange = (
+  <Slider.Root value={value} onValueChange={(v) => expectType<number, typeof v>(v)} />
+);
+const singleDefaultValueWithOnValueChange = (
+  <Slider.Root defaultValue={25} onValueChange={(v) => expectType<number, typeof v>(v)} />
+);
+
+const arrayValueWithOnValueChange = (
+  <Slider.Root value={array} onValueChange={(v) => expectType<number[], typeof v>(v)} />
+);
+const arrayDefaultValueWithOnValueChange = (
+  <Slider.Root defaultValue={[25]} onValueChange={(v) => expectType<number[], typeof v>(v)} />
+);
+
+const singleValueWithOnValueCommitted = (
+  <Slider.Root value={value} onValueCommitted={(v) => expectType<number, typeof v>(v)} />
+);
+const singleDefaultValueWithOnValueCommitted = (
+  <Slider.Root defaultValue={25} onValueCommitted={(v) => expectType<number, typeof v>(v)} />
+);
+
+const arrayValueWithOnValueCommitted = (
+  <Slider.Root value={array} onValueCommitted={(v) => expectType<number[], typeof v>(v)} />
+);
+const arrayDefaultValueWithOnValueCommitted = (
+  <Slider.Root defaultValue={[25]} onValueCommitted={(v) => expectType<number[], typeof v>(v)} />
+);
+
+const singleValueExplicitTypeAnnotation = (
+  <Slider.Root<number> onValueChange={(v) => expectType<number, typeof v>(v)} />
+);
+const arrayValueExplicitTypeAnnotation = (
+  <Slider.Root<number[]> onValueChange={(v) => expectType<number[], typeof v>(v)} />
+);
