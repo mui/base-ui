fb1597ea0adb5468fd0a0915edeb7395c9ac623c
Michał Dudak <michal@mui.com>
2025-02-21 11:17:30 +0100

    [test] Extract common popup tests (#1358)

--- packages/react/src/alert-dialog/root/AlertDialogRoot.test.tsx
+++ packages/react/src/alert-dialog/root/AlertDialogRoot.test.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { expect } from 'chai';
 import { act, screen, waitFor } from '@mui/internal-test-utils';
 import { AlertDialog } from '@base-ui-components/react/alert-dialog';
-import { createRenderer, isJSDOM } from '#test-utils';
+import { createRenderer, isJSDOM, popupConformanceTests } from '#test-utils';
 import { spy } from 'sinon';
 
 describe('<AlertDialog.Root />', () => {
@@ -10,6 +10,20 @@ describe('<AlertDialog.Root />', () => {
 
   beforeEach(() => {
     globalThis.BASE_UI_ANIMATIONS_DISABLED = true;
+  });
+
+  popupConformanceTests({
+    createComponent: (props) => (
+      <AlertDialog.Root {...props.root}>
+        <AlertDialog.Trigger {...props.trigger}>Open dialog</AlertDialog.Trigger>
+        <AlertDialog.Portal {...props.portal}>
+          <AlertDialog.Popup {...props.popup}>Dialog</AlertDialog.Popup>
+        </AlertDialog.Portal>
+      </AlertDialog.Root>
+    ),
+    render,
+    triggerMouseAction: 'click',
+    expectedPopupRole: 'alertdialog',
   });
 
   it('ARIA attributes', async () => {
