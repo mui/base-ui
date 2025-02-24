dd5ea350a516a428e93441b609d517c136a7c6c8
Albert Yu <albert@albertyu.co>
2025-02-24 18:13:57 +0800

    Flatten useToggle mergeReactProps

--- packages/react/src/toggle/useToggle.ts
+++ packages/react/src/toggle/useToggle.ts
@@ -38,17 +38,15 @@ export function useToggle(parameters: useToggle.Parameters): useToggle.ReturnVal
 
   const getRootProps = React.useCallback(
     (externalProps?: GenericHTMLProps): GenericHTMLProps => {
-      return getButtonProps(
-        mergeReactProps(externalProps, {
-          'aria-pressed': pressed,
-          onClick(event: React.MouseEvent) {
-            const nextPressed = !pressed;
-            setPressedState(nextPressed);
-            onPressedChange(nextPressed, event.nativeEvent);
-          },
-          ref: buttonRef,
-        }),
-      );
+      return mergeReactProps(getButtonProps, externalProps, {
+        'aria-pressed': pressed,
+        onClick(event: React.MouseEvent) {
+          const nextPressed = !pressed;
+          setPressedState(nextPressed);
+          onPressedChange(nextPressed, event.nativeEvent);
+        },
+        ref: buttonRef,
+      });
     },
     [getButtonProps, buttonRef, onPressedChange, pressed, setPressedState],
   );
