2658b106d7727332c2c8c3e35de22428e0162cda
Aarón García Hervás <aaron@mui.com>
2025-02-25 10:20:22 +0100

    Use unknown instead of any

--- docs/src/app/(public)/(content)/react/utils/use-render/demos/render/css-modules/index.tsx
+++ docs/src/app/(public)/(content)/react/utils/use-render/demos/render/css-modules/index.tsx
@@ -4,7 +4,7 @@ import { useRender } from '@base-ui-components/react/use-render';
 import styles from './index.module.css';
 
 type TextProps = {
-  render?: useRender.RenderProp<Record<string, any>>;
+  render?: useRender.RenderProp<Record<string, unknown>>;
   children: React.ReactNode;
 };
 
