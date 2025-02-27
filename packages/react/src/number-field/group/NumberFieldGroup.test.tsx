c60097a8d7761a574f8ae7717b2cddea0dcf6de3
Albert Yu <albert@albertyu.co>
2025-02-27 20:21:13 +0800

    Extract NumberFieldGroup propGetter

--- packages/react/src/number-field/group/NumberFieldGroup.test.tsx
+++ packages/react/src/number-field/group/NumberFieldGroup.test.tsx
@@ -4,16 +4,56 @@ import { screen } from '@mui/internal-test-utils';
 import { NumberField } from '@base-ui-components/react/number-field';
 import { createRenderer, describeConformance } from '#test-utils';
 import { NumberFieldRootContext } from '../root/NumberFieldRootContext';
+import { NOOP } from '../../utils/noop';
 
 const testContext = {
-  getGroupProps: (externalProps) => ({ role: 'group', ...externalProps }),
+  allowInputSyncRef: { current: false },
+  disabled: false,
+  formatOptionsRef: { current: undefined },
+  getInputProps: (externalProps) => externalProps,
+  getScrubAreaProps: (externalProps) => externalProps,
+  getScrubAreaCursorProps: (externalProps) => externalProps,
+  getStepAmount: NOOP,
+  id: 'id',
+  incrementValue: NOOP,
+  inputRef: { current: null },
+  inputValue: '',
+  intentionalTouchCheckTimeoutRef: { current: -1 },
+  isPressedRef: { current: false },
+  isScrubbing: false,
+  maxWithDefault: 100,
+  mergedRef: (_node) => {},
+  minWithDefault: 0,
+  movesAfterTouchRef: { current: 0 },
+  readOnly: false,
+  scrubAreaRef: { current: null },
+  scrubAreaCursorRef: { current: null },
+  scrubHandleRef: {
+    current: {
+      direction: 'horizontal',
+      pixelSensitivity: 0,
+      teleportDistance: 0,
+    },
+  },
+  setValue: NOOP,
   state: {
     value: null,
     required: false,
     disabled: false,
     invalid: false,
     readOnly: false,
+    scrubbing: false,
+    touched: false,
+    dirty: false,
+    inputValue: '',
+    valid: true,
+    filled: false,
+    focused: false,
   },
+  startAutoChange: NOOP,
+  stopAutoChange: NOOP,
+  value: null,
+  valueRef: { current: null },
 } as NumberFieldRootContext;
 
 describe('<NumberField.Group />', () => {
