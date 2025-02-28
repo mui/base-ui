b6b1c5842387f0e0b8669bf6d2f67804e3735726
mnajdova <mnajdova@gmail.com>
2025-02-28 11:51:58 +0100

    move render value to default props destruction value

--- docs/src/app/(public)/(content)/react/utils/use-render/demos/render-callback/css-modules/index.tsx
+++ docs/src/app/(public)/(content)/react/utils/use-render/demos/render-callback/css-modules/index.tsx
@@ -13,13 +13,13 @@ type CounterProps = {
 };
 
 function Counter(props: CounterProps) {
-  const { render, className, ...otherProps } = props;
+  const { render = <button type="button" />, className, ...otherProps } = props;
   const [count, setCount] = React.useState(0);
   const odd = count % 2 === 1;
   const state = React.useMemo(() => ({ odd }), [odd]);
 
   const { renderElement } = useRender({
-    render: render ?? <button type="button" />,
+    render: render,
     state,
     props: {
       ...otherProps,
