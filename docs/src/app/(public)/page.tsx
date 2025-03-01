b64194de3f9ad1a6304fa55cc0a216313384b320
Olivier Tassinari <olivier.tassinari@gmail.com>
2025-03-01 18:17:43 +0100

    [docs] Fix SEO site name description

--- docs/src/app/(public)/page.tsx
+++ docs/src/app/(public)/page.tsx
@@ -7,23 +7,39 @@ import './page.css';
 
 export default function Homepage() {
   return (
-    <div className="HomepageRoot">
-      <div className="HomepageContent">
-        <Logo className="mb-8 ml-px" aria-label="Base UI" />
-        <h1 className="HomepageHeading">
-          Unstyled UI components for building accessible web apps and design systems.
-        </h1>
-        <p className="HomepageCaption">
-          From the creators of Radix, Floating&nbsp;UI, and Material&nbsp;UI.
-        </p>
-        <Link
-          className="-m-1 inline-flex items-center gap-1 p-1"
-          href="/react/overview/quick-start"
-        >
-          Documentation <ArrowRightIcon />
-        </Link>
+    <React.Fragment>
+      {/* Set the Site name for Google results. https://developers.google.com/search/docs/appearance/site-names */}
+      <script
+        type="application/ld+json"
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            '@context': 'https://schema.org',
+            '@type': 'WebSite',
+            name: 'Base UI',
+            url: 'https://base-ui.com',
+          }),
+        }}
+      />
+      <div className="HomepageRoot">
+        <div className="HomepageContent">
+          <Logo className="mb-8 ml-px" aria-label="Base UI" />
+          <h1 className="HomepageHeading">
+            Unstyled UI components for building accessible web apps and design
+            systems.
+          </h1>
+          <p className="HomepageCaption">
+            From the creators of Radix, Floating&nbsp;UI, and Material&nbsp;UI.
+          </p>
+          <Link
+            className="-m-1 inline-flex items-center gap-1 p-1"
+            href="/react/overview/quick-start"
+          >
+            Documentation <ArrowRightIcon />
+          </Link>
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 }
 
