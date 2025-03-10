d69830f62bcb2806fcae52e94f35d6fcbb793365
Albert Yu <albert@albertyu.co>
2025-03-10 19:57:10 +0800

    Support keepMounted false

--- docs/src/app/(private)/experiments/collapsible/plain.tsx
+++ docs/src/app/(private)/experiments/collapsible/plain.tsx
@@ -1,21 +1,37 @@
 'use client';
 import * as React from 'react';
+import { useEnhancedEffect } from '@base-ui-components/react/utils';
 import classes from './plain.module.css';
 
-export default function DomCollapsible() {
+import { useAnimationsFinished } from '../../../../../../packages/react/src/utils/useAnimationsFinished';
+import { useEventCallback } from '../../../../../../packages/react/src/utils/useEventCallback';
+
+function PlainCollapsible(props: { keepMounted?: boolean }) {
+  const { keepMounted = true } = props;
+
   const [open, setOpen] = React.useState(false);
+
+  const [mounted, setMounted] = React.useState(open);
 
   const panelRef: React.RefObject<HTMLElement | null> = React.useRef(null);
 
   const [height, setHeight] = React.useState(0);
 
-  const handleTrigger = () => {
+  const handleTrigger = useEventCallback(() => {
+    const nextOpen = !open;
+
+    if (!keepMounted) {
+      // mount only
+      if (!mounted) {
+        setMounted(true);
+      }
+    }
+    setOpen(nextOpen);
+
     const panel = panelRef.current;
     if (!panel) {
       return;
     }
-
-    const nextOpen = !open;
 
     panel.style.display = 'block';
     // const targetHeight = panel.clientHeight;
@@ -33,34 +49,70 @@ export default function DomCollapsible() {
     } else {
       /* closing */
       requestAnimationFrame(() => {
-        console.log('closing, scrollHeight', panel.scrollHeight);
+        // console.log('closing, scrollHeight', panel.scrollHeight);
         panel.style.opacity = '0';
         setHeight(0);
       });
     }
+  });
 
-    setOpen(!open);
-  };
+  const runOnceAnimationsFinish = useAnimationsFinished(panelRef);
+
+  useEnhancedEffect(() => {
+    // This only matters when `keepMounted={false}`
+    if (keepMounted) {
+      return;
+    }
+    // console.log('useEnhancedEffect open', open, 'mounted', mounted);
+
+    const panel = panelRef.current;
+    if (!panel) {
+      return;
+    }
+
+    if (open) {
+      /* opening */
+      panel.style.opacity = '0';
+      panel.style.height = '0px';
+
+      requestAnimationFrame(() => {
+        panel.style.opacity = '1';
+        panel.style.height = '';
+        setHeight(panel.scrollHeight);
+      });
+    } else {
+      /* closing */
+      requestAnimationFrame(() => {
+        console.log('closing, scrollHeight', panel.scrollHeight);
+        panel.style.opacity = '0';
+        setHeight(0);
+      });
+
+      runOnceAnimationsFinish(() => {
+        setMounted(false);
+      });
+    }
+  }, [keepMounted, open, mounted, setMounted, runOnceAnimationsFinish]);
 
   return (
-    <div className={classes.wrapper}>
-      <div
-        className={classes.Root}
-        style={{
-          // @ts-ignore
-          '--collapsible-panel-height': `${height}px`,
-        }}
+    <div
+      className={classes.Root}
+      style={{
+        // @ts-ignore
+        '--collapsible-panel-height': `${height}px`,
+      }}
+    >
+      <button
+        type="button"
+        className={classes.Trigger}
+        data-panel-open={open || undefined}
+        onClick={handleTrigger}
       >
-        <button
-          type="button"
-          className={classes.Trigger}
-          data-panel-open={open || undefined}
-          onClick={handleTrigger}
-        >
-          <ExpandMoreIcon className={classes.Icon} />
-          Trigger
-        </button>
+        <ExpandMoreIcon className={classes.Icon} />
+        Trigger (keepMounted {String(keepMounted)})
+      </button>
 
+      {(keepMounted || (!keepMounted && mounted)) && (
         <div
           // @ts-ignore
           ref={panelRef}
@@ -78,7 +130,17 @@ export default function DomCollapsible() {
             </p>
           </div>
         </div>
-      </div>
+      )}
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <div className={classes.wrapper}>
+      <PlainCollapsible />
+
+      <PlainCollapsible keepMounted={false} />
     </div>
   );
 }
