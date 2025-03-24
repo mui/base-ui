d715f66059d753a25400e7acc3d34fcb41576735
atomiks <cc.glows@gmail.com>
2025-03-24 11:39:46 +1100

    jsdom

--- packages/react/src/scroll-area/utils/onVisible.ts
+++ packages/react/src/scroll-area/utils/onVisible.ts
@@ -2,6 +2,10 @@
  * Executes a callback when an element becomes visible.
  */
 export function onVisible(element: HTMLElement, callback: () => void) {
+  if (typeof IntersectionObserver === 'undefined') {
+    return () => {};
+  }
+
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if (entry.intersectionRatio > 0) {
