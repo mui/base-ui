1d76db0aaa6e131c6d8ebd28be5377e2b88aac4f
Albert Yu <albert@albertyu.co>
2025-03-26 21:00:01 +0800

    Update motion examples

--- docs/src/app/(private)/experiments/collapsible/animate-presence.tsx
+++ docs/src/app/(private)/experiments/collapsible/animate-presence.tsx
@@ -7,6 +7,7 @@ import { ChevronIcon } from './_icons';
 
 export default function CollapsibleAnimatePresence() {
   const [open, setOpen] = React.useState(false);
+
   return (
     <div className={styles.wrapper}>
       <Collapsible.Root open={open} onOpenChange={setOpen} className={styles.Root}>
@@ -19,20 +20,33 @@ export default function CollapsibleAnimatePresence() {
             <Collapsible.Panel
               key="hey"
               className={styles.Panel}
+              // when using motion we can't defer the hidden attribute
+              // this ensures the panel is visible during the exit animation
+              // Tailwind v4 sets `display: none !important` on [hidden] and there
+              // is no way for motion to directly override !important
+              // https://github.com/motiondivision/motion/issues/1285#issuecomment-934332108
+              hidden={undefined}
               keepMounted
               render={
                 <motion.div
+                  // https://github.com/framer/motion/issues/368#issuecomment-898055607
+                  // it's possible to animate padding on Collapsible.Panel with framer-motion
+                  // it looks much less janky than using CSS animations or transitions, but
+                  // it's still noticeably un-smooth
                   initial={{ height: 0, opacity: 0 }}
                   animate={{
                     height: '100%',
                     opacity: 1,
-                    transition: { duration: 2 },
+                    transition: { duration: 1 },
                   }}
                   exit={{
                     height: 0,
                     opacity: 0,
-                    display: 'block',
-                    transition: { duration: 20 },
+                    // motion needs to ensure the `display` property is set here
+                    // to override `display: none` that's usually default on the
+                    // [hidden] attribute
+                    // display: 'block',
+                    transition: { duration: 1 },
                   }}
                 />
               }
