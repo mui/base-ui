2fc67470adbe49e79925ca2e6aa804ad12deacc1
Seb Insua <me@sebinsua.com>
2025-03-31 10:41:23 +0100

    Fix types

--- packages/react/src/utils/inertValue.ts
+++ packages/react/src/utils/inertValue.ts
@@ -1,9 +1,9 @@
 import { isReactVersionAtLeast } from './reactVersion';
 
-export function inertValue(value?: boolean): string | boolean | undefined {
+export function inertValue(value?: boolean): boolean | undefined {
   if (isReactVersionAtLeast(19)) {
     return value;
   }
   // compatibility with React < 19
-  return value ? 'true' : undefined;
+  return (value ? 'true' : undefined) as boolean | undefined;
 }
