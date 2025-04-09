6c11db7219b3b4458e191b836b6a3ced7f52240f
atomiks <cc.glows@gmail.com>
2025-04-09 16:54:39 +1000

    [Menu][Select] Missing item data attributes docs (#1691)

--- packages/react/src/menu/checkbox-item/MenuCheckboxItemDataAttributes.ts
+++ packages/react/src/menu/checkbox-item/MenuCheckboxItemDataAttributes.ts
@@ -7,4 +7,12 @@ export enum MenuCheckboxItemDataAttributes {
    * Present when the menu checkbox item is not checked.
    */
   unchecked = 'data-unchecked',
+  /**
+   * Present when the menu checkbox item is disabled.
+   */
+  disabled = 'data-disabled',
+  /**
+   * Present when the menu checkbox item is highlighted.
+   */
+  highlighted = 'data-highlighted',
 }
