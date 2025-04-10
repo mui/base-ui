f275b1db87aebd1ce7c6078a7de01f0fa89dea11
atomiks <cc.glows@gmail.com>
2025-04-10 11:54:11 +1000

    [Menu] Fix stuck highlight on submenu trigger when submenu opens with keyboard

--- packages/react/src/menu/root/useMenuRoot.ts
+++ packages/react/src/menu/root/useMenuRoot.ts
@@ -175,7 +175,6 @@ export function useMenuRoot(parameters: useMenuRoot.Parameters): useMenuRoot.Ret
     enabled: hoverEnabled && openOnHover && !disabled && openReason !== 'click',
     handleClose: safePolygon({ blockPointerEvents: true }),
     mouseOnly: true,
-    move: false,
     delay: {
       open: delay,
     },
@@ -266,6 +265,7 @@ export function useMenuRoot(parameters: useMenuRoot.Parameters): useMenuRoot.Ret
   return React.useMemo(
     () => ({
       activeIndex,
+      setActiveIndex,
       allowMouseUpTriggerRef,
       floatingRootContext,
       itemProps,
@@ -398,6 +398,7 @@ export namespace useMenuRoot {
     instantType: 'dismiss' | 'click' | undefined;
     onOpenChangeComplete: ((open: boolean) => void) | undefined;
     setHoverEnabled: React.Dispatch<React.SetStateAction<boolean>>;
+    setActiveIndex: React.Dispatch<React.SetStateAction<number | null>>;
   }
 
   export interface Actions {
