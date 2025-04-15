0cbcde9e5860fe928530b19312b8f7ecb288aad2
Michał Dudak <michal@mui.com>
2025-04-15 10:38:02 +0200

    Remove unnecessary internal tag

--- packages/react/src/utils/useBaseUiId.ts
+++ packages/react/src/utils/useBaseUiId.ts
@@ -5,7 +5,6 @@ import { useId } from './useId';
  * Wraps `useId` and prefixes generated `id`s with `base-ui-`
  * @param {string | undefined} idOverride overrides the generated id when provided
  * @returns {string | undefined}
- * @internal
  */
 export function useBaseUiId(idOverride?: string): string | undefined {
   return useId(idOverride, 'base-ui');
