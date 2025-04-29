ad0b2ffa469aad18b0bbfd7b886870881c26518c
Michał Dudak <michal@mui.com>
2025-04-29 15:09:12 +0200

    Remove unused env var

--- babel.config.js
+++ babel.config.js
@@ -12,7 +12,7 @@ module.exports = function getBabelConfig(api) {
       '@babel/preset-env',
       {
         bugfixes: true,
-        browserslistEnv: process.env.BABEL_ENV || process.env.NODE_ENV,
+        browserslistEnv: process.env.NODE_ENV,
         debug: process.env.MUI_BUILD_VERBOSE === 'true',
         modules: useESModules ? false : 'commonjs',
       },
