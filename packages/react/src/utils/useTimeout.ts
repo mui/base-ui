3a6ccf48e079c9ff1f3f0e049090791599c39cd3
Rom Grk <romgrk.cc@gmail.com>
2025-05-06 15:05:52 -0400

    feat: import useTimeout

---
+++ packages/react/src/utils/useTimeout.ts
@@ -0,0 +1,45 @@
+'use client';
+import { useLazyRef } from './useLazyRef';
+import { useOnMount } from './useOnMount';
+
+type TimeoutId = ReturnType<typeof setTimeout>;
+
+const EMPTY = 0 as unknown as TimeoutId;
+
+export class Timeout {
+  static create() {
+    return new Timeout();
+  }
+
+  currentId: TimeoutId = EMPTY;
+
+  /**
+   * Executes `fn` after `delay`, clearing any previously scheduled call.
+   */
+  start(delay: number, fn: Function) {
+    this.clear();
+    this.currentId = setTimeout(() => {
+      this.currentId = EMPTY;
+      fn();
+    }, delay);
+  }
+
+  clear = () => {
+    if (this.currentId !== EMPTY) {
+      clearTimeout(this.currentId as TimeoutId);
+      this.currentId = EMPTY;
+    }
+  };
+
+  disposeEffect = () => {
+    return this.clear;
+  };
+}
+
+export default function useTimeout() {
+  const timeout = useLazyRef(Timeout.create).current;
+
+  useOnMount(timeout.disposeEffect);
+
+  return timeout;
+}
