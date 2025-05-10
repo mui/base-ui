43a4e821805cca76406904221ed1c25edfdc7e06
Rom Grk <romgrk.cc@gmail.com>
2025-05-10 13:03:34 -0400

    perf: create useLatestRef

--- packages/react/src/utils/useLatestRef.ts
+++ packages/react/src/utils/useLatestRef.ts
@@ -1,2 +1,26 @@
 'use client';
-export { useLatestRef } from '@floating-ui/react/utils';
+import * as React from 'react';
+import { useLazyRef } from './useLazyRef';
+
+const useEffect = typeof document !== 'undefined' ? React.useLayoutEffect : React.useEffect;
+
+export function useLatestRef<T>(value: T) {
+  const latest = useLazyRef(createLatestRef, value).current;
+
+  latest.next = value;
+
+  useEffect(latest.effect);
+
+  return latest;
+}
+
+function createLatestRef<T>(value: T) {
+  const latest = {
+    current: value,
+    next: value,
+    effect: () => {
+      latest.current = latest.next;
+    },
+  };
+  return latest;
+}
