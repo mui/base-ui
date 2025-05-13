b2ec0c00a0f779323a179303a80ee98ed9706be7
Albert Yu <albert@albertyu.co>
2025-05-13 13:20:31 +0800

    fix

--- docs/src/components/SkipNav.tsx
+++ docs/src/components/SkipNav.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import clsx from 'clsx';
 
-export function SkipNav({ className, ...props }: React.ComponentProps<'a'>) {
-  return <a className={clsx('SkipNav', className)} href={MAIN_CONTENT_ID} {...props} />;
-}
+export const MAIN_CONTENT_ID = 'main-content';
+const HREF = `#${MAIN_CONTENT_ID}`;
 
-export const MAIN_CONTENT_ID = '#main-content';
+export function SkipNav({ className, ...props }: React.ComponentProps<'a'>) {
+  return <a className={clsx('SkipNav', className)} href={HREF} {...props} />;
+}
