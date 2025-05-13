74d4fe77bb741718ae5c61105520b0b1e1a26e1f
Rom Grk <romgrk.cc@gmail.com>
2025-05-12 20:48:25 -0400

    lint

--- packages/react/src/utils/useEventCallback.ts
+++ packages/react/src/utils/useEventCallback.ts
@@ -4,7 +4,7 @@ import { useLazyRef } from './useLazyRef';
 
 // https://github.com/mui/material-ui/issues/41190#issuecomment-2040873379
 const useInsertionEffect =
-  (React as any)[`useInsertionEffect${Math.random()}`.slice(0, 18)] || ((fn: any) => fn());
+  (React as any)[`useInsertionEffect${Math.random().toFixed(1)}`.slice(0, -3)] || ((fn: any) => fn());
 
 type Callback = (...args: any[]) => any;
 
