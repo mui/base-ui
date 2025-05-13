dc3ea5f3a6a6992f11f4cc158c5421e4451dee01
Rom Grk <romgrk.cc@gmail.com>
2025-05-12 20:09:29 -0400

    lint

--- packages/react/src/utils/mergeObjects.ts
+++ packages/react/src/utils/mergeObjects.ts
@@ -8,7 +8,7 @@ export function mergeObjects<A extends object | undefined, B extends object | un
   if (!a && b) {
     return b;
   }
-  if (b || a) {
+  if (a || b) {
     return { ...a, ...b };
   }
   return undefined;
