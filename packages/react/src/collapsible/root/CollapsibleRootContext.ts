60985839bb92146d0ff163219f9c1b63befec579
Rom Grk <romgrk.cc@gmail.com>
2025-05-14 02:13:27 -0400

    lint

--- packages/react/src/collapsible/root/CollapsibleRootContext.ts
+++ packages/react/src/collapsible/root/CollapsibleRootContext.ts
@@ -12,9 +12,13 @@ export const CollapsibleRootContext = React.createContext<CollapsibleRootContext
   undefined,
 );
 
-if (process.env.NODE_ENV !== 'production') {
-  CollapsibleRootContext.displayName = 'CollapsibleRootContext';
-}
+export function useCollapsibleRootContext() {
+  const context = React.useContext(CollapsibleRootContext);
+  if (context === undefined) {
+    throw new Error(
+      'Base UI: CollapsibleRootContext is missing. Collapsible parts must be placed within <Collapsible.Root>.',
+    );
+  }
 
   return context;
 }
