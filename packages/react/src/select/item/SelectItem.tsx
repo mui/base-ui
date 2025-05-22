15698861d8d2a3269cf15f78c0f106203c50607e
Rom Grk <romgrk.cc@gmail.com>
2025-05-22 15:44:09 -0400

    refactor: remove useSelectItem()

--- packages/react/src/select/item/SelectItem.tsx
+++ packages/react/src/select/item/SelectItem.tsx
@@ -6,11 +6,17 @@ import { SelectIndexContext, useSelectIndexContext } from '../root/SelectIndexCo
 import { useCompositeListItem } from '../../composite/list/useCompositeListItem';
 import { useForkRef } from '../../utils/useForkRef';
 import type { BaseUIComponentProps } from '../../utils/types';
-import { useSelectItem } from './useSelectItem';
 import { useModernLayoutEffect } from '../../utils/useModernLayoutEffect';
 import { useLatestRef } from '../../utils/useLatestRef';
-import { SelectItemContext } from './SelectItemContext';
+import { useTimeout } from '../../utils/useTimeout';
+import { useEventCallback } from '../../utils/useEventCallback';
+import { addHighlight, hasHighlight, removeHighlight } from '../../utils/highlighted';
+import { isMouseWithinBounds } from '../../utils/isMouseWithinBounds';
+import { AnimationFrame } from '../../utils/useAnimationFrame';
 import { useRenderElement } from '../../utils/useRenderElement';
+import { useButton } from '../../use-button';
+import { mergeProps } from '../../merge-props';
+import { SelectItemContext } from './SelectItemContext';
 
 interface InnerSelectItemProps extends Omit<SelectItem.Props, 'value'> {
   highlighted: boolean;
@@ -75,25 +81,213 @@ const InnerSelectItem = React.memo(
     delete rootProps.onFocus;
     delete rootProps.id;
 
-    const { props, rootRef } = useSelectItem({
-      open,
-      setOpen,
+    const handleSelect = (event: any) => setValue(value, event);
+
+    const ref = React.useRef<HTMLDivElement | null>(null);
+    const lastKeyRef = React.useRef<string | null>(null);
+    const pointerTypeRef = React.useRef<'mouse' | 'touch' | 'pen'>('mouse');
+    const didPointerDownRef = React.useRef(false);
+    const prevPopupHeightRef = React.useRef(0);
+    const allowFocusSyncRef = React.useRef(true);
+    const cursorMovementTimeout = useTimeout();
+
+    const mergedRef = useForkRef(forwardedRef, ref);
+
+    const { getButtonProps, buttonRef } = useButton({
       disabled,
-      highlighted,
-      selected,
-      ref: forwardedRef,
-      typingRef,
-      handleSelect: (event) => setValue(value, event),
-      selectionRef,
-      selectedIndexRef,
-      indexRef,
-      setActiveIndex,
-      popupRef,
-      keyboardActiveRef,
-      events,
-      rootProps,
-      elementProps,
+      focusableWhenDisabled: true,
+      buttonRef: mergedRef,
     });
+
+    const commitSelection = useEventCallback((event: MouseEvent) => {
+      handleSelect(event);
+      setOpen(false, event, 'item-press');
+    });
+
+    const handlePopupLeave = useEventCallback(() => {
+      if (cursorMovementTimeout.isStarted()) {
+        cursorMovementTimeout.clear();
+        removeHighlight(ref);
+      }
+    });
+
+    React.useEffect(() => {
+      return handlePopupLeave;
+    }, [handlePopupLeave]);
+
+    useModernLayoutEffect(() => {
+      if (!open) {
+        return;
+      }
+
+      if (highlighted) {
+        addHighlight(ref);
+      } else {
+        removeHighlight(ref);
+      }
+    }, [open, highlighted]);
+
+    React.useEffect(() => {
+      function handleItemHover(item: HTMLDivElement) {
+        if (ref.current && item !== ref.current) {
+          removeHighlight(ref);
+        }
+      }
+
+      events.on('itemhover', handleItemHover);
+      return () => {
+        events.off('itemhover', handleItemHover);
+      };
+    }, [events, setActiveIndex, indexRef]);
+
+    const props = mergeProps<'div'>(
+      rootProps,
+      {
+        'aria-disabled': disabled || undefined,
+        tabIndex: highlighted ? 0 : -1,
+        onFocus() {
+          if (
+            allowFocusSyncRef.current &&
+            keyboardActiveRef.current &&
+            cursorMovementTimeout.isStarted() === false
+          ) {
+            setActiveIndex(indexRef.current);
+          }
+        },
+        onMouseEnter() {
+          if (!keyboardActiveRef.current && selectedIndexRef.current === null) {
+            addHighlight(ref);
+            events.emit('itemhover', ref.current);
+          }
+        },
+        onMouseMove() {
+          if (keyboardActiveRef.current) {
+            setActiveIndex(indexRef.current);
+          } else {
+            addHighlight(ref);
+            events.emit('itemhover', ref.current);
+          }
+
+          if (popupRef.current) {
+            prevPopupHeightRef.current = popupRef.current.offsetHeight;
+          }
+
+          events.off('popupleave', handlePopupLeave);
+          events.on('popupleave', handlePopupLeave);
+          // When this fires, the cursor has stopped moving.
+          cursorMovementTimeout.start(50, () => {
+            setActiveIndex(indexRef.current);
+          });
+        },
+        onMouseLeave(event) {
+          const popup = popupRef.current;
+          if (!popup || !open || keyboardActiveRef.current) {
+            return;
+          }
+
+          if (isMouseWithinBounds(event)) {
+            return;
+          }
+
+          removeHighlight(ref);
+          events.off('popupleave', handlePopupLeave);
+
+          const wasCursorStationary = cursorMovementTimeout.isStarted() === false;
+          if (!wasCursorStationary) {
+            cursorMovementTimeout.clear();
+          }
+
+          // With `alignItemWithTrigger=true`, avoid re-rendering the root due to `onMouseLeave`
+          // firing and causing a performance issue when expanding the popup.
+          if (popup.offsetHeight === prevPopupHeightRef.current) {
+            // Prevent `onFocus` from causing the highlight to be stuck when quickly moving
+            // the mouse out of the popup.
+            allowFocusSyncRef.current = false;
+
+            if (keyboardActiveRef.current || wasCursorStationary) {
+              setActiveIndex(null);
+            }
+
+            AnimationFrame.request(() => {
+              cursorMovementTimeout.clear();
+              allowFocusSyncRef.current = true;
+            });
+          }
+        },
+        onTouchStart() {
+          selectionRef.current = {
+            allowSelectedMouseUp: false,
+            allowUnselectedMouseUp: false,
+            allowSelect: true,
+          };
+        },
+        onKeyDown(event) {
+          selectionRef.current.allowSelect = true;
+          lastKeyRef.current = event.key;
+          setActiveIndex(indexRef.current);
+        },
+        onClick(event) {
+          didPointerDownRef.current = false;
+
+          // Prevent double commit on {Enter}
+          if (event.type === 'keydown' && lastKeyRef.current === null) {
+            return;
+          }
+
+          if (
+            disabled ||
+            (lastKeyRef.current === ' ' && typingRef.current) ||
+            (pointerTypeRef.current !== 'touch' && !hasHighlight(ref))
+          ) {
+            return;
+          }
+
+          if (selectionRef.current.allowSelect) {
+            lastKeyRef.current = null;
+            commitSelection(event.nativeEvent);
+          }
+        },
+        onPointerEnter(event) {
+          pointerTypeRef.current = event.pointerType;
+        },
+        onPointerDown(event) {
+          pointerTypeRef.current = event.pointerType;
+          didPointerDownRef.current = true;
+        },
+        onMouseUp(event) {
+          if (disabled) {
+            return;
+          }
+
+          if (didPointerDownRef.current) {
+            didPointerDownRef.current = false;
+            return;
+          }
+
+          const disallowSelectedMouseUp = !selectionRef.current.allowSelectedMouseUp && selected;
+          const disallowUnselectedMouseUp =
+            !selectionRef.current.allowUnselectedMouseUp && !selected;
+
+          if (
+            disallowSelectedMouseUp ||
+            disallowUnselectedMouseUp ||
+            (pointerTypeRef.current !== 'touch' && !hasHighlight(ref))
+          ) {
+            return;
+          }
+
+          if (selectionRef.current.allowSelect || !selected) {
+            commitSelection(event.nativeEvent);
+          }
+
+          selectionRef.current.allowSelect = true;
+        },
+      },
+      elementProps,
+      getButtonProps,
+    );
+
+    const rootRef = buttonRef;
 
     const element = useRenderElement('div', componentProps, {
       ref: [rootRef, forwardedRef],
