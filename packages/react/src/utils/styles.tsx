d3f28f2ca1094cafc4557232fe14e677f8ede9bb
Rom Grk <romgrk.cc@gmail.com>
2025-05-22 13:32:02 -0400

    lint

--- packages/react/src/utils/styles.tsx
+++ packages/react/src/utils/styles.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 export const STYLE_DISABLE_SCROLLBAR = {
   className: 'base-ui-disable-scrollbar',
   element: (
-    <style href="/base-ui-disable-scrollbar">
+    <style href="base-ui-disable-scrollbar" precedence="low">
       {`.base-ui-disable-scrollbar{scrollbar-width:none}.base-ui-disable-scrollbar::-webkit-scrollbar{display:none}`}
     </style>
   ),
