3d7a934cf5478932cb43599b012c9d5542943c4b
Michał Dudak <michal@mui.com>
2025-05-26 15:05:26 +0200

    Update TS API Extractor to handle conditional types

--- packages/react/src/slider/root/SliderRoot.tsx
+++ packages/react/src/slider/root/SliderRoot.tsx
@@ -460,6 +460,8 @@ export namespace SliderRoot {
      * @param {Event} event The corresponding event that initiated the change.
      * You can pull out the new value by accessing `event.target.value` (any).
      * @param {number} activeThumbIndex Index of the currently moved thumb.
+     *
+     * @type {((value: (number | number[]), event: Event, activeThumbIndex: number) => void)}
      */
     onValueChange?: (
       value: Value extends number ? number : Value,
@@ -472,6 +474,8 @@ export namespace SliderRoot {
      * @param {number | number[]} value The new value.
      * @param {Event} event The corresponding event that initiated the change.
      * **Warning**: This is a generic event not a change event.
+     *
+     * @type {((value: (number | number[]), event: Event) => void)}
      */
     onValueCommitted?: (value: Value extends number ? number : Value, event: Event) => void;
   }
