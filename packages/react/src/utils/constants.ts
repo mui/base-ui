226b427a819d2951ea501d7d4fcc2f06e1ee2b67
atomiks <cc.glows@gmail.com>
2025-05-28 13:30:15 +1000

    [popups] Adjust collisionAvoidance defaults

--- packages/react/src/utils/constants.ts
+++ packages/react/src/utils/constants.ts
@@ -1,6 +1,21 @@
 export const TYPEAHEAD_RESET_MS = 500;
 export const PATIENT_CLICK_THRESHOLD = 500;
-export const DEFAULT_COLLISION_AVOIDANCE = {
+
+/**
+ * Used for dropdowns that usually strictly prefer top/bottom placements and
+ * use `var(--available-height)` to limit their height.
+ */
+export const DROPDOWN_COLLISION_AVOIDANCE = {
+  side: 'flip',
+  align: 'flip',
+  fallbackAxisSide: 'none',
+} as const;
+
+/**
+ * Used by regular popups that usually aren't scrollable and are allowed to
+ * freely flip to any axis of placement.
+ */
+export const POPUP_COLLISION_AVOIDANCE = {
   side: 'flip',
   align: 'flip',
   fallbackAxisSide: 'end',
