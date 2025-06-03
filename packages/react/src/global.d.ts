81c9dcd4511398e0fb4a97238dce852373a50ff0
Brijesh Bittu <brijesh42@gmail.com>
2025-06-03 15:29:59 +0530

    [code-infra] Migrate to eslint v9 and flat config

--- packages/react/src/global.d.ts
+++ packages/react/src/global.d.ts
@@ -2,7 +2,7 @@ declare global {
   /**
    * When `true`, disables animation-related code, even if supported by the runtime enviroment.
    */
-  // eslint-disable-next-line no-var, vars-on-top
+  // eslint-disable-next-line vars-on-top
   var BASE_UI_ANIMATIONS_DISABLED: boolean;
 }
 
