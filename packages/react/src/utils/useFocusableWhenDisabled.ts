d0ea44d9e751c86b8dc769caddffed12312857a8
Albert Yu <albert@albertyu.co>
2025-06-18 01:53:46 +0800

    Update jsdocs

--- packages/react/src/utils/useFocusableWhenDisabled.ts
+++ packages/react/src/utils/useFocusableWhenDisabled.ts
@@ -2,7 +2,14 @@
 import * as React from 'react';
 
 export interface UseFocusableWhenDisabledParameters {
+  /**
+   * Whether the component should be focusable when disabled.
+   * When `undefined`, composite items are focusable when disabled by default.
+   */
   focusableWhenDisabled?: boolean | undefined;
+  /**
+   * The disabled state of the component.
+   */
   disabled: boolean;
   /**
    * Whether this is a composite item or not
