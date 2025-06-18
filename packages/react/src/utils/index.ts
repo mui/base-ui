345f5ebee576ff07be5d164836c4b24bd52da1f7
flavien <flaviendelangle@gmail.com>
2025-06-18 08:10:40 +0200

    [utils] Make Romain's utils public

--- packages/react/src/utils/index.ts
+++ packages/react/src/utils/index.ts
@@ -1,10 +1,14 @@
 // Public utils
 
 export * from './getReactElementRef';
+export * from './store';
 export * from './useControlled';
 export * from './useModernLayoutEffect';
 export * from './useForkRef';
 export * from './useId';
+export * from './useLazyRef';
+export * from './useOnMount';
 export * from './useScrollLock';
+export * from './useTimeout';
 export * from './useTransitionStatus';
 export * from './visuallyHidden';
