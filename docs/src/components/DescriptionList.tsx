5c16060c744fc2150551e5ec66e0ab95134d6de4
Albert Yu <albert@albertyu.co>
2025-06-19 18:35:16 +0800

    tweaks

--- docs/src/components/DescriptionList.tsx
+++ docs/src/components/DescriptionList.tsx
@@ -2,14 +2,14 @@
 import * as React from 'react';
 import clsx from 'clsx';
 
-export function Root({ className, ...props }: React.ComponentProps<'dl'>) {
-  return <dl className={clsx('DescriptionList', className)} {...props} />;
+export function Root(props: React.ComponentProps<'dl'>) {
+  return <dl {...props} className={clsx('DescriptionList', props.className)} />;
 }
 
-export function Term({ className, ...props }: React.ComponentProps<'dt'>) {
-  return <dt className={clsx('DescriptionTerm', className)} {...props} />;
+export function Term(props: React.ComponentProps<'dt'>) {
+  return <dt {...props} className={clsx('DescriptionTerm', props.className)} />;
 }
 
-export function Details({ className, ...props }: React.ComponentProps<'dd'>) {
-  return <dd className={clsx('DescriptionDetails', className)} {...props} />;
+export function Details(props: React.ComponentProps<'dd'>) {
+  return <dd {...props} className={clsx('DescriptionDetails', props.className)} />;
 }
