fa138929f3c4411bcae24417924f280a372b0e7c
Albert Yu <albert@albertyu.co>
2025-06-23 20:08:40 +0800

    remove extraneous line

--- docs/src/components/QuickNav/rehypeSlug.mjs
+++ docs/src/components/QuickNav/rehypeSlug.mjs
@@ -71,7 +71,3 @@ export function stringToUrl(string) {
     .replace(/^-|-$/gi, '')
     .toLowerCase();
 }
-
-// https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
-const SEMVER_PATTERN =
-  /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/gm;
