9715c79396bb4da8b1ceb4fcf4ef633308d75f5e
atomiks <cc.glows@gmail.com>
2025-07-04 20:44:12 +1000

    lint

--- packages/react/src/composite/item/CompositeItem.tsx
+++ packages/react/src/composite/item/CompositeItem.tsx
@@ -36,7 +36,8 @@ export function CompositeItem<Metadata, State extends Record<string, any>>(
 
 export namespace CompositeItem {
   export interface Props<Metadata, State extends Record<string, any>>
-    extends Pick<BaseUIComponentProps<any, State>, 'render' | 'className' | 'children'> {
+    extends Pick<BaseUIComponentProps<any, State>, 'render' | 'className'> {
+    children?: React.ReactNode;
     metadata?: Metadata;
     refs?: React.Ref<HTMLElement | null>[];
     props?: Array<Record<string, any> | (() => Record<string, any>)>;
