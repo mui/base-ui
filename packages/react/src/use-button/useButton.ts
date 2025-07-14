6cfdabd3c0f52c142df9c847237e8983b02ffff6
atomiks <cc.glows@gmail.com>
2025-07-14 18:37:20 +1000

    [useButton] Prevent `Space` key default on keydown (#2295)

--- packages/react/src/use-button/useButton.ts
+++ packages/react/src/use-button/useButton.ts
@@ -90,16 +90,23 @@ export function useButton(parameters: useButton.Parameters = {}): useButton.Retu
               return;
             }
 
-            // Keyboard accessibility for non interactive elements
-            if (
+            const shouldClick =
               event.target === event.currentTarget &&
               !isNativeButton &&
               !isValidLink() &&
-              event.key === 'Enter' &&
-              !disabled
-            ) {
-              externalOnClick?.(event);
-              event.preventDefault();
+              !disabled;
+            const isEnterKey = event.key === 'Enter';
+            const isSpaceKey = event.key === ' ';
+
+            // Keyboard accessibility for non interactive elements
+            if (shouldClick) {
+              if (isSpaceKey || isEnterKey) {
+                event.preventDefault();
+              }
+
+              if (isEnterKey) {
+                externalOnClick?.(event);
+              }
             }
           },
           onKeyUp(event: BaseUIEvent<React.KeyboardEvent>) {
