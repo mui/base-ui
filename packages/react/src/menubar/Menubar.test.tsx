c2ef38b378a2bb7a27e638ec381bf6923e724832
atomiks <cc.glows@gmail.com>
2025-07-17 21:59:49 +1000

    amend test

--- packages/react/src/menubar/Menubar.test.tsx
+++ packages/react/src/menubar/Menubar.test.tsx
@@ -675,14 +675,13 @@ describe('<Menubar />', () => {
 
   describe('role', () => {
     it('sets role="menubar" on the root element', async () => {
-      const { container } = await render(<TestMenubar />);
-      const menubar = container.firstChild as HTMLElement;
-      expect(menubar).to.have.attribute('role', 'menubar');
+      await render(<TestMenubar />);
+      expect(screen.queryByRole('menubar')).not.to.equal(null);
     });
 
     it('sets role="menuitem" on menu triggers', async () => {
-      const { getAllByRole } = await render(<TestMenubar />);
-      const menuItems = getAllByRole('menuitem');
+      await render(<TestMenubar />);
+      const menuItems = screen.getAllByRole('menuitem');
       expect(menuItems).to.have.length(3);
     });
   });
