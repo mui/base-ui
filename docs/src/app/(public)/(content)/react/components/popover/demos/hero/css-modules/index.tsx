874c342bc6264c211b60ecd4d5c2c730fc35579e
atomiks <cc.glows@gmail.com>
2025-07-25 10:08:41 +1000

    unset docs

--- docs/src/app/(public)/(content)/react/components/popover/demos/hero/css-modules/index.tsx
+++ docs/src/app/(public)/(content)/react/components/popover/demos/hero/css-modules/index.tsx
@@ -5,7 +5,7 @@ import styles from './index.module.css';
 export default function ExamplePopover() {
   return (
     <Popover.Root>
-      <Popover.Trigger className={styles.IconButton} render={<div />}>
+      <Popover.Trigger className={styles.IconButton}>
         <BellIcon aria-label="Notifications" className={styles.Icon} />
       </Popover.Trigger>
       <Popover.Portal>
