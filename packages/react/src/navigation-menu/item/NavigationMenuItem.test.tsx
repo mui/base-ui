b4ecfa7292e984ca257b22694535d2328ea95a79
atomiks <cc.glows@gmail.com>
2025-08-19 14:00:00 +1000

    [navigation menu] Semantic element structure and `active` page prop (#2526)

--- packages/react/src/navigation-menu/item/NavigationMenuItem.test.tsx
+++ packages/react/src/navigation-menu/item/NavigationMenuItem.test.tsx
@@ -6,7 +6,7 @@ describe('<NavigationMenu.Item />', () => {
   const { render } = createRenderer();
 
   describeConformance(<NavigationMenu.Item />, () => ({
-    refInstanceof: window.HTMLDivElement,
+    refInstanceof: window.HTMLLIElement,
     render(node) {
       return render(<NavigationMenu.Root>{node}</NavigationMenu.Root>);
     },
