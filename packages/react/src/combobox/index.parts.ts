bfc29cca6497882c5f495074d0831f1d39b03b33
atomiks <cc.glows@gmail.com>
2025-08-28 21:04:48 +1000

    docs: improvements

--- packages/react/src/combobox/index.parts.ts
+++ packages/react/src/combobox/index.parts.ts
@@ -1,5 +1,5 @@
 export { ComboboxRoot as Root } from './root/ComboboxRoot';
-export { ComboboxValue as Value } from './selected-value/ComboboxValue';
+export { ComboboxValue as Value } from './value/ComboboxValue';
 export { ComboboxInput as Input } from './input/ComboboxInput';
 export { ComboboxTrigger as Trigger } from './trigger/ComboboxTrigger';
 export { ComboboxList as List } from './list/ComboboxList';
