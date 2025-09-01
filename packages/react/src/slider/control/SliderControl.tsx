ac055c32210acf251e8d4ed229a6d761126374a4
Albert Yu <albert@albertyu.co>
2025-09-01 17:41:30 +0800

    add inset positioning

--- packages/react/src/slider/control/SliderControl.tsx
+++ packages/react/src/slider/control/SliderControl.tsx
@@ -81,6 +81,7 @@ export const SliderControl = React.forwardRef(function SliderControl(
     disabled,
     dragging,
     fieldControlValidation,
+    inset,
     lastChangedValueRef,
     max,
     min,
@@ -118,6 +119,9 @@ export const SliderControl = React.forwardRef(function SliderControl(
   const touchIdRef = React.useRef<number>(null);
   // The number of touch/pointermove events that have fired.
   const moveCountRef = React.useRef(0);
+  // The offset amount to each side of the control for inset sliders.
+  // This value should be equal to the radius or half the width/height of the thumb.
+  const insetThumbOffsetRef = React.useRef(0);
 
   const getFingerState = useEventCallback((fingerCoords: Coords): FingerState | null => {
     const control = controlRef.current;
@@ -129,7 +133,9 @@ export const SliderControl = React.forwardRef(function SliderControl(
     const { width, height, bottom, left, right } = control.getBoundingClientRect();
 
     const controlOffset = getControlOffset(stylesRef.current, vertical);
-    const controlSize = (vertical ? height : width) - controlOffset.start - controlOffset.end;
+    const insetThumbOffset = insetThumbOffsetRef.current;
+    const controlSize =
+      (vertical ? height : width) - controlOffset.start - controlOffset.end - insetThumbOffset * 2;
     const thumbCenterOffset = pressedThumbCenterOffsetRef.current ?? 0;
     const fingerX = fingerCoords.x - thumbCenterOffset;
     const fingerY = fingerCoords.y - thumbCenterOffset;
@@ -138,7 +144,7 @@ export const SliderControl = React.forwardRef(function SliderControl(
       ? bottom - fingerY - controlOffset.end
       : (direction === 'rtl' ? right - fingerX : fingerX - left) - controlOffset.start;
     // the value at the finger origin scaled down to fit the range [0, 1]
-    const valueRescaled = clamp(valueSize / controlSize, 0, 1);
+    const valueRescaled = clamp((valueSize - insetThumbOffset) / controlSize, 0, 1);
 
     let newValue = (max - min) * valueRescaled + min;
     newValue = roundValueToStep(newValue, step, min);
@@ -205,7 +211,14 @@ export const SliderControl = React.forwardRef(function SliderControl(
       pressedThumbIndexRef.current = closestThumbIndex;
     }
 
-    return closestThumbIndex;
+    if (inset) {
+      const thumbEl = thumbRefs.current[closestThumbIndex];
+      if (isElement(thumbEl)) {
+        const thumbRect = thumbEl.getBoundingClientRect();
+        const side = !vertical ? 'width' : 'height';
+        insetThumbOffsetRef.current = thumbRect[side] / 2;
+      }
+    }
   });
 
   const focusThumb = useEventCallback((thumbIndex: number) => {
--- packages/react/src/slider/indicator/SliderIndicator.tsx
+++ packages/react/src/slider/indicator/SliderIndicator.tsx
@@ -1,30 +1,100 @@
 'use client';
 import * as React from 'react';
-import type { BaseUIComponentProps, Orientation } from '../../utils/types';
+import type { BaseUIComponentProps } from '../../utils/types';
+import { valueToPercent } from '../../utils/valueToPercent';
 import { useRenderElement } from '../../utils/useRenderElement';
 import { useSliderRootContext } from '../root/SliderRootContext';
 import { sliderStateAttributesMapping } from '../root/stateAttributesMapping';
 import type { SliderRoot } from '../root/SliderRoot';
-import { valueArrayToPercentages } from '../utils/valueArrayToPercentages';
 
-function getRangeStyles(
-  orientation: Orientation,
-  offset: number,
-  leap: number,
+function getInsetStyles(
+  vertical: boolean,
+  range: boolean,
+  start: number | undefined,
+  end: number | undefined,
 ): React.CSSProperties {
-  if (orientation === 'vertical') {
+  const visibility = start === undefined || (range && end === undefined) ? 'hidden' : undefined;
+
+  if (!range) {
+    if (vertical) {
+      return {
+        visibility,
+        position: 'absolute',
+        bottom: 0,
+        height: `${start ?? 0}%`,
+        width: 'inherit',
+      };
+    }
+
+    return {
+      visibility,
+      position: 'relative',
+      insetInlineStart: 0,
+      width: `${start}%`,
+      height: 'inherit',
+    };
+  }
+
+  const size = (end ?? 0) - (start ?? 0);
+
+  if (vertical) {
+    return {
+      visibility,
+      position: 'absolute',
+      bottom: `${start}%`,
+      height: `${size}%`,
+      width: 'inherit',
+    };
+  }
+
+  return {
+    visibility,
+    position: 'relative',
+    insetInlineStart: `${start}%`,
+    width: `${size}%`,
+    height: 'inherit',
+  };
+}
+
+function getCenteredStyles(
+  vertical: boolean,
+  range: boolean,
+  start: number,
+  end: number,
+): React.CSSProperties {
+  if (!range) {
+    if (vertical) {
+      return {
+        position: 'absolute',
+        bottom: 0,
+        height: `${start}%`,
+        width: 'inherit',
+      };
+    }
+
+    return {
+      position: 'relative',
+      insetInlineStart: 0,
+      width: `${start}%`,
+      height: 'inherit',
+    };
+  }
+
+  const size = end - start;
+
+  if (vertical) {
     return {
       position: 'absolute',
-      bottom: `${offset}%`,
-      height: `${leap}%`,
+      bottom: `${start}%`,
+      height: `${size}%`,
       width: 'inherit',
     };
   }
 
   return {
     position: 'relative',
-    insetInlineStart: `${offset}%`,
-    width: `${leap}%`,
+    insetInlineStart: `${start}%`,
+    width: `${size}%`,
     height: 'inherit',
   };
 }
@@ -41,32 +111,19 @@ export const SliderIndicator = React.forwardRef(function SliderIndicator(
 ) {
   const { render, className, ...elementProps } = componentProps;
 
-  const { max, min, orientation, state, values } = useSliderRootContext();
+  const { max, min, indicatorPosition, inset, orientation, state, values } = useSliderRootContext();
 
-  const percentageValues = valueArrayToPercentages(values.slice(), min, max);
+  const vertical = orientation === 'vertical';
+  const range = values.length > 1;
 
-  let style: React.CSSProperties;
-
-  if (percentageValues.length > 1) {
-    const trackOffset = percentageValues[0];
-    const trackLeap = percentageValues[percentageValues.length - 1] - trackOffset;
-
-    style = getRangeStyles(orientation, trackOffset, trackLeap);
-  } else if (orientation === 'vertical') {
-    style = {
-      position: 'absolute',
-      bottom: 0,
-      height: `${percentageValues[0]}%`,
-      width: 'inherit',
-    };
-  } else {
-    style = {
-      position: 'relative',
-      insetInlineStart: 0,
-      width: `${percentageValues[0]}%`,
-      height: 'inherit',
-    };
-  }
+  const style = inset
+    ? getInsetStyles(vertical, range, indicatorPosition[0], indicatorPosition[1])
+    : getCenteredStyles(
+        vertical,
+        range,
+        valueToPercent(values[0], min, max),
+        valueToPercent(values[values.length - 1], min, max),
+      );
 
   const element = useRenderElement('div', componentProps, {
     state,
