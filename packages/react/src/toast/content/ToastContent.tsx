7a9c36eaa6a4f07af42240fba41d622062db1c43
atomiks <cc.glows@gmail.com>
2025-09-12 15:27:36 +1000

    [toast] Support variable height stacking

---
+++ packages/react/src/toast/content/ToastContent.tsx
@@ -0,0 +1,77 @@
+'use client';
+import * as React from 'react';
+import { useIsoLayoutEffect } from '@base-ui-components/utils/useIsoLayoutEffect';
+import type { BaseUIComponentProps } from '../../utils/types';
+import { useToastRootContext } from '../root/ToastRootContext';
+import { useRenderElement } from '../../utils/useRenderElement';
+
+/**
+ * A container for the contents of a toast.
+ * Renders a `<div>` element.
+ *
+ * Documentation: [Base UI Toast](https://base-ui.com/react/components/toast)
+ */
+export const ToastContent = React.forwardRef(function ToastContent(
+  componentProps: ToastContent.Props,
+  forwardedRef: React.ForwardedRef<HTMLDivElement>,
+) {
+  const { render, className, ...elementProps } = componentProps;
+
+  const { index, expanded, recalculateHeight } = useToastRootContext();
+
+  const contentRef = React.useRef<HTMLDivElement | null>(null);
+
+  useIsoLayoutEffect(() => {
+    const node = contentRef.current;
+    if (!node) {
+      return undefined;
+    }
+
+    function updateHeight() {
+      recalculateHeight();
+    }
+
+    updateHeight();
+
+    if (typeof ResizeObserver !== 'function') {
+      return undefined;
+    }
+
+    const resizeObserver = new ResizeObserver(updateHeight);
+    resizeObserver.observe(node);
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, [recalculateHeight]);
+
+  const state: ToastContent.State = React.useMemo(
+    () => ({
+      expanded,
+      behind: index > 0,
+    }),
+    [expanded, index],
+  );
+
+  const element = useRenderElement('div', componentProps, {
+    ref: [forwardedRef, contentRef],
+    state,
+    props: elementProps,
+  });
+
+  return element;
+});
+
+export namespace ToastContent {
+  export interface State {
+    /**
+     * Whether the toast viewport is expanded.
+     */
+    expanded: boolean;
+    /**
+     * Whether the toast is behind the frontmost toast in the stack.
+     */
+    behind: boolean;
+  }
+
+  export interface Props extends BaseUIComponentProps<'div', State> {}
+}
---
+++ packages/react/src/toast/viewport/ToastViewportCssVars.ts
@@ -0,0 +1,7 @@
+export enum ToastViewportCssVars {
+  /**
+   * Indicates the height of the frontmost toast at index 0 in the list.
+   * @type {number}
+   */
+  index0Height = '--toast-index-0-height',
+}
