a4c1607ef8e17454ca1fc72e25c58d5553873d26
Michał Dudak <michal@mui.com>
2025-09-17 11:13:41 +0200

    Revert useStoreControlled

--- packages/utils/src/useControlled.ts
+++ packages/utils/src/useControlled.ts
@@ -2,7 +2,6 @@
 // TODO: uncomment once we enable eslint-plugin-react-compiler // eslint-disable-next-line react-compiler/react-compiler -- process.env never changes, dependency arrays are intentionally ignored
 /* eslint-disable react-hooks/rules-of-hooks, react-hooks/exhaustive-deps */
 import * as React from 'react';
-import { Store, useStore } from './store';
 
 export interface UseControlledProps<T = unknown> {
   /**
@@ -74,67 +73,4 @@ export function useControlled<T = unknown>({
   }, []);
 
   return [value as T, setValueIfUncontrolled];
-}
-
-interface UseStoreControlledParameters<Value, State> {
-  controlled?: Value;
-  default?: Value;
-  store: Store<State>;
-  selector: (state: State) => Value;
-  name: keyof State;
-}
-
-export function useStoreControlled<Value, State>({
-  controlled,
-  default: defaultProp,
-  store,
-  selector,
-  name,
-}: UseStoreControlledParameters<Value, State>): [Value, (newValue: Value) => void] {
-  // isControlled is ignored in the hook dependency lists as it should never change.
-  const { current: isControlled } = React.useRef(controlled !== undefined);
-  const valueState = useStore(store, selector);
-  const value = isControlled ? controlled : valueState;
-
-  if (process.env.NODE_ENV !== 'production') {
-    React.useEffect(() => {
-      if (isControlled !== (controlled !== undefined)) {
-        console.error(
-          [
-            `Base UI: A component is changing the ${
-              isControlled ? '' : 'un'
-            }controlled state of ${name as string} to be ${isControlled ? 'un' : ''}controlled.`,
-            'Elements should not switch from uncontrolled to controlled (or vice versa).',
-            `Decide between using a controlled or uncontrolled ${name as string} ` +
-              'element for the lifetime of the component.',
-            "The nature of the state is determined during the first render. It's considered controlled if the value is not `undefined`.",
-            'More info: https://fb.me/react-controlled-components',
-          ].join('\n'),
-        );
-      }
-    }, [name, controlled]);
-
-    const { current: defaultValue } = React.useRef(defaultProp);
-
-    React.useEffect(() => {
-      // Object.is() is not equivalent to the === operator.
-      // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/is for more details.
-      if (!isControlled && !Object.is(defaultValue, defaultProp)) {
-        console.error(
-          [
-            `Base UI: A component is changing the default state of an uncontrolled ${name as string} after being initialized. ` +
-              `To suppress this warning opt to use a controlled ${name as string}.`,
-          ].join('\n'),
-        );
-      }
-    }, [JSON.stringify(defaultProp)]);
-  }
-
-  const setValueIfUncontrolled = React.useCallback((newValue: Value) => {
-    if (!isControlled) {
-      store.set(name, newValue as Value);
-    }
-  }, []);
-
-  return [value as Value, setValueIfUncontrolled];
 }
