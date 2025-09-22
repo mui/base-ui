b79ee8ab1fb9a052218ed519bbffc66803b0d406
atomiks <cc.glows@gmail.com>
2025-09-22 23:30:13 +1000

    frontmost

--- packages/react/src/toast/viewport/ToastViewportCssVars.ts
+++ packages/react/src/toast/viewport/ToastViewportCssVars.ts
@@ -3,5 +3,5 @@ export enum ToastViewportCssVars {
    * Indicates the height of the frontmost toast at index 0 in the list.
    * @type {number}
    */
-  index0Height = '--toast-index-0-height',
+  index0Height = '--toast-frontmost-height',
 }
