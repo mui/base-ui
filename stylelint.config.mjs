34bf2c79b845933e2fa485ce7ab1aa019ce65451
Janpot <2109932+Janpot@users.noreply.github.com>
2025-09-22 15:39:28 +0200

    More fixes

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -13,7 +13,26 @@ export default {
     'custom-property-empty-line-before': null,
     'comment-empty-line-before': null,
     'at-rule-empty-line-before': null,
+    'function-no-unknown': true,
     'number-max-precision': 5,
   },
-  maxWarnings: 10,
+  overrides: [
+    {
+      files: ['docs/**/*'],
+      extends: ['stylelint-config-tailwindcss'],
+      rules: {
+        // https://github.com/zhilidali/stylelint-config-tailwindcss/issues/12
+        'declaration-property-value-no-unknown': null,
+        'keyframes-name-pattern': null,
+      },
+    },
+    {
+      files: ['docs/src/app/[(]private[)]/experiments/**/*'],
+      rules: {
+        'block-no-redundant-nested-style-rules': null,
+        'no-descending-specificity': null,
+        'no-duplicate-selectors': null,
+      },
+    },
+  ],
 };
