42bf2810def29d78230957d7a1bf7f6eba4bb7dc
Lukas <llukas.tyla@gmail.com>
2025-09-24 18:20:42 +0300

    Further cleanup

--- docs/src/app/(private)/experiments/calendar/calendar-two-months.tsx
+++ docs/src/app/(private)/experiments/calendar/calendar-two-months.tsx
@@ -1,6 +1,5 @@
 'use client';
 import * as React from 'react';
-import clsx from 'clsx';
 import { format } from 'date-fns/format';
 import { addMonths } from 'date-fns/addMonths';
 import { Calendar } from '@base-ui-components/react/calendar';
@@ -13,7 +12,7 @@ function Header() {
   return (
     <header className={styles.Header}>
       <div className={styles.HeaderPanel}>
-        <Calendar.SetPreviousMonth className={clsx(styles.SetPreviousMonth)}>
+        <Calendar.SetPreviousMonth className={styles.SetPreviousMonth}>
           ◀
         </Calendar.SetPreviousMonth>
         <span className={styles.HeaderLabel}>{format(visibleDate, 'MMMM yyyy')}</span>
@@ -22,7 +21,7 @@ function Header() {
       <div className={styles.HeaderPanel}>
         <span />
         <span className={styles.HeaderLabel}>{format(addMonths(visibleDate, 1), 'MMMM yyyy')}</span>
-        <Calendar.SetNextMonth className={clsx(styles.SetNextMonth)}>▶</Calendar.SetNextMonth>
+        <Calendar.SetNextMonth className={styles.SetNextMonth}>▶</Calendar.SetNextMonth>
       </div>
     </header>
   );
@@ -61,7 +60,7 @@ function DayGrid(props: { offset: 0 | 1 }) {
 
 export default function CalendarTwoMonths() {
   return (
-    <Calendar.Root monthPageSize={2} className={clsx(styles.Root, styles.RootWithTwoPanels)}>
+    <Calendar.Root monthPageSize={2} className={styles.Root}>
       <Header />
       <div className={styles.RootWithTwoPanelsContent}>
         <DayGrid offset={0} />
