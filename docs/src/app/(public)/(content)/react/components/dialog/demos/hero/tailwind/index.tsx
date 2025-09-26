10e4a7bddeb776470403a69bcafdacc73a2324a9
atomiks <cc.glows@gmail.com>
2025-09-26 21:33:54 +1000

    min-height

--- docs/src/app/(public)/(content)/react/components/dialog/demos/hero/tailwind/index.tsx
+++ docs/src/app/(public)/(content)/react/components/dialog/demos/hero/tailwind/index.tsx
@@ -8,7 +8,7 @@ export default function ExampleDialog() {
         View notifications
       </Dialog.Trigger>
       <Dialog.Portal>
-        <Dialog.Backdrop className="absolute inset-0 h-dvh bg-black opacity-20 transition-all duration-150 data-[ending-style]:opacity-0 data-[starting-style]:opacity-0 dark:opacity-70" />
+        <Dialog.Backdrop className="absolute inset-0 min-h-dvh bg-black opacity-20 transition-all duration-150 data-[ending-style]:opacity-0 data-[starting-style]:opacity-0 dark:opacity-70" />
         <Dialog.Popup className="fixed top-1/2 left-1/2 -mt-8 w-96 max-w-[calc(100vw-3rem)] -translate-x-1/2 -translate-y-1/2 rounded-lg bg-gray-50 p-6 text-gray-900 outline outline-1 outline-gray-200 transition-all duration-150 data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[starting-style]:scale-90 data-[starting-style]:opacity-0 dark:outline-gray-300">
           <Dialog.Title className="-mt-1.5 mb-1 text-lg font-medium">Notifications</Dialog.Title>
           <Dialog.Description className="mb-6 text-base text-gray-600">
