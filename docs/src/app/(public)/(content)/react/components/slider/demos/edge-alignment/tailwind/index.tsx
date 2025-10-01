b0e716ba672d55681e6160824afffc0b52f3fd6a
Albert Yu <albert@albertyu.co>
2025-10-01 17:19:15 +0800

    Fix demo styles

--- docs/src/app/(public)/(content)/react/components/slider/demos/edge-alignment/tailwind/index.tsx
+++ docs/src/app/(public)/(content)/react/components/slider/demos/edge-alignment/tailwind/index.tsx
@@ -7,7 +7,7 @@ export default function EdgeAlignedThumb() {
       <Slider.Control className="flex w-56 touch-none items-center select-none">
         <Slider.Track className="h-4 w-full rounded-full bg-gray-200 shadow-[inset_0_0_0_1px] shadow-gray-200 select-none">
           <Slider.Indicator className="rounded-l-full bg-gray-600 select-none" />
-          <Slider.Thumb className="size-4 rounded-full bg-white outline outline-1 outline-gray-300 select-none has-[:focus-visible]:outline has-[:focus-visible]:outline-2 has-[:focus-visible]:outline-blue-800" />
+          <Slider.Thumb className="size-4 rounded-full bg-white outline outline-1 -outline-offset-1 outline-gray-300 select-none has-[:focus-visible]:outline has-[:focus-visible]:outline-2 has-[:focus-visible]:outline-blue-800" />
         </Slider.Track>
       </Slider.Control>
     </Slider.Root>
