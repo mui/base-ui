c2b25c6177308041468bbb4f48d1756bf2ced528
Jacksonmills <jackson.mills@steelseries.com>
2025-10-02 21:43:48 -0500

    prettier

--- docs/src/app/(public)/(content)/react/components/textarea/demos/form/tailwind/index.tsx
+++ docs/src/app/(public)/(content)/react/components/textarea/demos/form/tailwind/index.tsx
@@ -31,7 +31,7 @@ export default function ExampleForm() {
         <Field.Label className="text-sm font-medium text-gray-900">Feedback</Field.Label>
         <Textarea
           placeholder="Enter your feedback"
-          className="w-full max-w-64 min-h-16 rounded-md border border-gray-200 text-base text-gray-900 focus:outline-2 focus:-outline-offset-1 focus:outline-blue-800 px-3 py-2 field-sizing-content resize-y"
+          className="field-sizing-content min-h-16 w-full max-w-64 resize-y rounded-md border border-gray-200 px-3 py-2 text-base text-gray-900 focus:outline-2 focus:-outline-offset-1 focus:outline-blue-800"
         />
         <Field.Error className="text-sm text-red-800" />
       </Field.Root>
--- docs/src/app/(public)/(content)/react/components/textarea/demos/max-rows/tailwind/index.tsx
+++ docs/src/app/(public)/(content)/react/components/textarea/demos/max-rows/tailwind/index.tsx
@@ -7,7 +7,7 @@ export default function ExampleTextarea() {
       placeholder="Type something…"
       minRows={4}
       maxRows={8}
-      className="w-full max-w-64 min-h-16 rounded-md border border-gray-200 text-base text-gray-900 focus:outline-2 focus:-outline-offset-1 focus:outline-blue-800 px-3 py-2 field-sizing-content resize-y"
+      className="field-sizing-content min-h-16 w-full max-w-64 resize-y rounded-md border border-gray-200 px-3 py-2 text-base text-gray-900 focus:outline-2 focus:-outline-offset-1 focus:outline-blue-800"
     />
   );
 }
