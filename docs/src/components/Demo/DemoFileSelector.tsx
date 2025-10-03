caa0b672fec75f838c7f2a045820b86571f7e39f
dav-is <mail@connordav.is>
2025-10-03 11:54:33 -0400

    Use <a> tags for file tabs

--- docs/src/components/Demo/DemoFileSelector.tsx
+++ docs/src/components/Demo/DemoFileSelector.tsx
@@ -23,23 +23,61 @@ export function DemoFileSelector({
     [files],
   );
 
+  const modifierKeysRef = React.useRef(false);
+
   const onValueChange = React.useCallback(
     (value: string) => {
+      // Don't change tab if modifier keys were pressed (user is opening in new tab)
+      if (modifierKeysRef.current) {
+        return;
+      }
+
       selectFileName(value);
       onTabChange?.();
     },
     [selectFileName, onTabChange],
   );
 
+  const onTabPointerDown = React.useCallback((event: React.PointerEvent) => {
+    // Track modifier keys before focus event fires
+    modifierKeysRef.current = event.ctrlKey || event.metaKey;
+  }, []);
+
+  const onTabClick = React.useCallback((event: React.MouseEvent<HTMLAnchorElement>) => {
+    // Allow Ctrl+Click (or Cmd+Click on Mac) to open in new tab
+    if (event.ctrlKey || event.metaKey) {
+      // Reset modifier keys flag after all event handlers complete
+      queueMicrotask(() => {
+        modifierKeysRef.current = false;
+      });
+    } else {
+      // Prevent scroll jump to anchor
+      event.preventDefault();
+      modifierKeysRef.current = false;
+    }
+  }, []);
+
   if (files.length === 1) {
-    return <div className="DemoFilename">{files[0].name}</div>;
+    return (
+      <a className="DemoFilename" href={files[0].slug ? `#${files[0].slug}` : undefined}>
+        {files[0].name}
+      </a>
+    );
   }
 
   return (
     <Tabs.Root value={selectedFileName} onValueChange={onValueChange}>
       <Tabs.List className="DemoTabsList" aria-label="Files">
         {tabs.map((tab: Tab) => (
-          <Tabs.Tab className="DemoTab" value={tab.id} key={tab.id}>
+          <Tabs.Tab
+            // eslint-disable-next-line jsx-a11y/control-has-associated-label
+            render={<a href={tab.slug ? `#${tab.slug}` : undefined} onClick={onTabClick} />}
+            className="DemoTab"
+            value={tab.id}
+            key={tab.id}
+            nativeButton={false}
+            onPointerDown={onTabPointerDown}
+          >
             {tab.name}
           </Tabs.Tab>
         ))}
