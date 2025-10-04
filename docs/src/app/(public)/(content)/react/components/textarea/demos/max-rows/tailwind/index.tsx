a6b97b8b3b65c5f0d184a7d3270b949400d81f56
Jacksonmills <jackson.mills@steelseries.com>
2025-10-04 11:26:47 -0500

    moved resize none to example CSS

--- docs/src/app/(public)/(content)/react/components/textarea/demos/max-rows/tailwind/index.tsx
+++ docs/src/app/(public)/(content)/react/components/textarea/demos/max-rows/tailwind/index.tsx
@@ -7,7 +7,7 @@ export default function ExampleTextarea() {
       placeholder="Type something…"
       minRows={4}
       maxRows={8}
-      className="w-full max-w-64 resize-y rounded-md border border-gray-200 px-3 py-2 text-base text-gray-900 focus:outline-2 focus:-outline-offset-1 focus:outline-blue-800"
+      className="w-full max-w-64 resize-none rounded-md border border-gray-200 px-3 py-2 text-base text-gray-900 focus:outline-2 focus:-outline-offset-1 focus:outline-blue-800"
     />
   );
 }
