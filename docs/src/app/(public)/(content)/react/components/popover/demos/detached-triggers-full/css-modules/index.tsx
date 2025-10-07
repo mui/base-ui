a78f84890113ed43df891b402d89d1810d2550b7
dav-is <mail@connordav.is>
2025-10-07 14:03:31 -0400

    Cleanup popover demos

--- docs/src/app/(public)/(content)/react/components/popover/demos/detached-triggers-full/css-modules/index.tsx
+++ docs/src/app/(public)/(content)/react/components/popover/demos/detached-triggers-full/css-modules/index.tsx
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import { Popover } from '@base-ui-components/react/popover';
 import { Avatar } from '@base-ui-components/react/avatar';
-import styles from './index.module.css';
+import styles from './styles.module.css';
 
 const demoPopover = Popover.createHandle<React.ComponentType>();
 
