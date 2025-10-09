9c0d87f37c2d8280cfa519749b02c66a7959fed0
Albert Yu <albert@albertyu.co>
2025-10-09 17:45:35 +0800

    Add combobox

--- docs/src/app/(private)/experiments/forms/_icons.tsx
+++ docs/src/app/(private)/experiments/forms/_icons.tsx
@@ -52,3 +52,38 @@ export function HorizontalRuleIcon(props: React.ComponentProps<'svg'>) {
     </svg>
   );
 }
+
+export function ClearIcon(props: React.ComponentProps<'svg'>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M18 6L6 18" />
+      <path d="M6 6l12 12" />
+    </svg>
+  );
+}
+
+export function ChevronDownIcon(props: React.ComponentProps<'svg'>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M6 9l6 6 6-6" />
+    </svg>
+  );
+}
--- docs/src/app/(private)/experiments/forms/tanstack.tsx
+++ docs/src/app/(private)/experiments/forms/tanstack.tsx
@@ -5,12 +5,15 @@ import clsx from 'clsx';
 import { useForm } from '@tanstack/react-form';
 import { Field as BField } from '@base-ui-components/react/field';
 import { Form as BForm } from '@base-ui-components/react/form';
+import { Combobox as BCombobox } from '@base-ui-components/react/combobox';
+import { ClearIcon, CheckIcon, ChevronDownIcon } from './_icons';
 
 export default function App() {
   const { Field: FormField, handleSubmit } = useForm({
     defaultValues: {
-      name: '',
+      serverName: '',
       staticIpAddress: '',
+      region: '', // this cannot be null or `FormField` will throw a type error
     },
     onSubmit: async ({ value }) => {
       console.log('submit', value);
@@ -25,7 +28,7 @@ export default function App() {
         }}
       >
         <FormField
-          name="name"
+          name="serverName"
           validators={{
             onBlur: ({ value, fieldApi }) => {
               if (!fieldApi.state.meta.isDirty) {
@@ -86,11 +89,176 @@ export default function App() {
             );
           }}
         />
+        <FormField
+          name="region"
+          validators={{
+            onBlur: ({ value, fieldApi }) => {
+              if (!fieldApi.state.meta.isDirty) {
+                return undefined;
+              }
+              if (!value) {
+                return 'Required';
+              }
+              return value.includes('east') ? `${value} is unavailable` : undefined;
+            },
+            onChange: ({ value, fieldApi }) => {
+              // revalidate on change
+              if (fieldApi.state.meta.isDirty && fieldApi.state.meta.isBlurred && value) {
+                const message = value.includes('east') ? `${value} is unavailable` : undefined;
+                fieldApi.setErrorMap({ onBlur: message });
+              }
+              return undefined;
+            },
+          }}
+          children={(field) => {
+            return (
+              <FieldRoot name={field.name} invalid={!field.state.meta.isValid}>
+                <ComboboxRoot
+                  items={REGIONS}
+                  // useForm doesn't work well with `defaultValue: null`, but an
+                  // empty string is considered filled by Combobox and show the Clear button
+                  value={field.state.value || null}
+                  onValueChange={field.handleChange}
+                >
+                  <div className="relative flex flex-col gap-1 text-sm leading-5 font-medium text-gray-900">
+                    <FieldLabel>Choose a region</FieldLabel>
+                    <ComboboxInput placeholder="e.g. eu-central-1" onBlur={field.handleBlur} />
+                    <div className="absolute right-2 bottom-0 flex h-10 items-center justify-center text-gray-600">
+                      <ComboboxClear />
+                      <ComboboxTrigger>
+                        <ChevronDownIcon className="size-4" />
+                      </ComboboxTrigger>
+                    </div>
+                  </div>
+                  <ComboboxPortal>
+                    <ComboboxPositioner>
+                      <ComboboxPopup>
+                        <ComboboxEmpty>No matches</ComboboxEmpty>
+                        <ComboboxList>
+                          {(region: string) => {
+                            return (
+                              <ComboboxItem key={region} value={region}>
+                                <ComboboxItemIndicator>
+                                  <CheckIcon className="size-3" />
+                                </ComboboxItemIndicator>
+                                <div className="col-start-2">{region}</div>
+                              </ComboboxItem>
+                            );
+                          }}
+                        </ComboboxList>
+                      </ComboboxPopup>
+                    </ComboboxPositioner>
+                  </ComboboxPortal>
+                </ComboboxRoot>
+
+                <FieldError match={!field.state.meta.isValid}>
+                  {field.state.meta.errors.join(',')}
+                </FieldError>
+              </FieldRoot>
+            );
+          }}
+        />
 
         <Button type="submit">Submit</Button>
       </Form>
     </div>
   );
+}
+
+function ComboboxRoot(props: BCombobox.Root.Props<any, any>) {
+  return <BCombobox.Root {...props} />;
+}
+
+function ComboboxInput({ className, ...props }: BCombobox.Input.Props) {
+  return (
+    <BCombobox.Input
+      className={clsx(
+        'h-10 w-64 rounded-md font-normal border border-gray-200 pl-3.5 text-base text-gray-900 bg-[canvas] focus:outline focus:-outline-offset-1 focus:outline-blue-800',
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+function ComboboxClear({ className, ...props }: BCombobox.Clear.Props) {
+  return (
+    <BCombobox.Clear
+      className={clsx(
+        'flex h-10 w-6 items-center justify-center rounded bg-transparent p-0',
+        className,
+      )}
+      {...props}
+    >
+      <ClearIcon className="size-4" />
+    </BCombobox.Clear>
+  );
+}
+
+function ComboboxTrigger({ className, ...props }: BCombobox.Trigger.Props) {
+  return (
+    <BCombobox.Trigger
+      className={clsx(
+        'flex h-10 w-6 items-center justify-center rounded bg-transparent p-0',
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+function ComboboxPortal(props: BCombobox.Portal.Props) {
+  return <BCombobox.Portal {...props} />;
+}
+
+function ComboboxPositioner({ className, ...props }: BCombobox.Positioner.Props) {
+  return (
+    <BCombobox.Positioner className={clsx('outline-none', className)} sideOffset={4} {...props} />
+  );
+}
+
+function ComboboxPopup({ className, ...props }: BCombobox.Popup.Props) {
+  return (
+    <BCombobox.Popup
+      className={clsx(
+        'w-[var(--anchor-width)] max-h-[min(var(--available-height),23rem)] max-w-[var(--available-width)] origin-[var(--transform-origin)] overflow-y-auto scroll-pt-2 scroll-pb-2 overscroll-contain rounded-md bg-[canvas] py-2 text-gray-900 shadow-lg shadow-gray-200 outline-1 outline-gray-200 transition-[transform,scale,opacity] data-[ending-style]:scale-95 data-[ending-style]:opacity-0 data-[starting-style]:scale-95 data-[starting-style]:opacity-0 dark:shadow-none dark:-outline-offset-1 dark:outline-gray-300',
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+function ComboboxEmpty({ className, ...props }: BCombobox.Empty.Props) {
+  return (
+    <BCombobox.Empty
+      className={clsx(
+        'px-4 py-2 text-[0.925rem] leading-4 text-gray-600 empty:m-0 empty:p-0',
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+function ComboboxList(props: BCombobox.List.Props) {
+  return <BCombobox.List /* className={clsx('', className)} */ {...props} />;
+}
+
+function ComboboxItem({ className, ...props }: BCombobox.Item.Props) {
+  return (
+    <BCombobox.Item
+      className={clsx(
+        'grid cursor-default grid-cols-[0.75rem_1fr] items-center gap-2 py-2 pr-8 pl-4 text-base leading-4 outline-none select-none data-[highlighted]:relative data-[highlighted]:z-0 data-[highlighted]:text-gray-50 data-[highlighted]:before:absolute data-[highlighted]:before:inset-x-2 data-[highlighted]:before:inset-y-0 data-[highlighted]:before:z-[-1] data-[highlighted]:before:rounded-sm data-[highlighted]:before:bg-gray-900',
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+function ComboboxItemIndicator({ className, ...props }: BCombobox.ItemIndicator.Props) {
+  return <BCombobox.ItemIndicator className={clsx('col-start-1', className)} {...props} />;
 }
 
 function Form({ className, ...props }: BForm.Props) {
@@ -144,3 +312,9 @@ function Button({ className, ...props }: React.ComponentPropsWithoutRef<'button'
 
 const IPV4_PATTERN =
   /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+
+const REGIONS = ['us', 'eu'].flatMap((val1) => {
+  return ['central', 'east', 'west'].flatMap((val2) => {
+    return ['1', '2', '3', '4'].map((val3) => `${val1}-${val2}-${val3}`);
+  });
+});
