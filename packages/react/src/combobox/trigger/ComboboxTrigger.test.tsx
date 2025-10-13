1900074999308bf56c884af070318aabd9839b5c
atomiks <cc.glows@gmail.com>
2025-10-13 16:51:33 +1100

    [combobox] Add role=combobox to trigger if input is inside popup

--- packages/react/src/combobox/trigger/ComboboxTrigger.test.tsx
+++ packages/react/src/combobox/trigger/ComboboxTrigger.test.tsx
@@ -267,7 +267,7 @@ describe('<Combobox.Trigger />', () => {
         </Combobox.Root>,
       );
 
-      const trigger = screen.getByRole('button', { name: 'Open' });
+      const trigger = screen.getByRole('combobox');
       await user.click(trigger);
       await user.keyboard('{ArrowDown}');
       expect(screen.queryByRole('listbox')).to.equal(null);
