27fe43bfd8d21e613f2bf4e888b4b852c1381791
Michał Dudak <michal@mui.com>
2025-10-22 18:46:35 +0200

    Move setOpen to the store

--- packages/react/src/tooltip/store/TooltipStore.ts
+++ packages/react/src/tooltip/store/TooltipStore.ts
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import { createSelector, ReactStore } from '@base-ui-components/utils/store';
 import { EMPTY_OBJECT } from '@base-ui-components/utils/empty';
 import { FloatingRootContext } from '../../floating-ui-react';
@@ -28,6 +29,7 @@ export type State = {
 
 export type Context = {
   popupRef: React.RefObject<HTMLElement | null>;
+  onOpenChange?: (open: boolean, eventDetails: TooltipRoot.ChangeEventDetails) => void;
   onOpenChangeComplete: ((open: boolean) => void) | undefined;
 };
 
@@ -54,11 +56,44 @@ export class TooltipStore extends ReactStore<State, Context, typeof selectors> {
       { ...createInitialState(), ...initialState },
       {
         popupRef: React.createRef<HTMLElement | null>(),
+        onOpenChange: undefined,
         onOpenChangeComplete: undefined,
       },
       selectors,
     );
   }
+
+  setOpen = (nextOpen: boolean, eventDetails: TooltipRoot.ChangeEventDetails) => {
+    const reason = eventDetails.reason;
+
+    const isHover = reason === 'trigger-hover';
+    const isFocusOpen = nextOpen && reason === 'trigger-focus';
+    const isDismissClose = !nextOpen && (reason === 'trigger-press' || reason === 'escape-key');
+
+    this.context.onOpenChange?.(nextOpen, eventDetails);
+
+    if (eventDetails.isCanceled) {
+      return;
+    }
+
+    const changeState = () => {
+      if (isFocusOpen || isDismissClose) {
+        this.set('instantType', isFocusOpen ? 'focus' : 'dismiss');
+      } else if (reason === 'trigger-hover') {
+        this.set('instantType', undefined);
+      }
+
+      this.update({ open: nextOpen, lastOpenChangeReason: reason });
+    };
+
+    if (isHover) {
+      // If a hover reason is provided, we need to flush the state synchronously. This ensures
+      // `node.getAnimations()` knows about the new state.
+      ReactDOM.flushSync(changeState);
+    } else {
+      changeState();
+    }
+  };
 }
 
 function createInitialState(): State {
