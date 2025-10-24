7df120303382ddccea27117e794d719b88eafbc4
atomiks <cc.glows@gmail.com>
2025-10-24 19:21:11 +1100

    [combobox][autocomplete] Add empty and side styling attributes on `Input` (#2926)

--- packages/react/src/combobox/root/AriaCombobox.tsx
+++ packages/react/src/combobox/root/AriaCombobox.tsx
@@ -347,6 +347,7 @@ export function AriaCombobox<Value = any, Mode extends SelectionMode = 'none'>(
         listElement: null,
         triggerElement: null,
         inputElement: null,
+        popupSide: null,
         openMethod: null,
         inputInsidePopup: true,
         onOpenChangeComplete: onOpenChangeCompleteProp || NOOP,
