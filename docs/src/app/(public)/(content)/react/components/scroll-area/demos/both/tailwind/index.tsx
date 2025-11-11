a0cfa91a131b0a016d188bc415c99a7305ba1cd6
atomiks <cc.glows@gmail.com>
2025-11-11 20:38:09 +1100

    lint

--- docs/src/app/(public)/(content)/react/components/scroll-area/demos/both/tailwind/index.tsx
+++ docs/src/app/(public)/(content)/react/components/scroll-area/demos/both/tailwind/index.tsx
@@ -17,14 +17,14 @@ export default function ExampleScrollAreaBoth() {
           </ul>
         </ScrollArea.Content>
       </ScrollArea.Viewport>
-      <ScrollArea.Scrollbar className="pointer-events-none m-1 flex rounded-full bg-gray-200 opacity-0 transition-opacity duration-150 data-[orientation=vertical]:ml-0 data-[orientation=vertical]:w-1.25 data-[orientation=horizontal]:mt-0 data-[orientation=horizontal]:h-1.5 data-[hovering]:pointer-events-auto data-[hovering=true]:pointer-events-auto data-[hovering]:opacity-100 data-[hovering=true]:opacity-100 data-[scrolling]:pointer-events-auto data-[scrolling=true]:pointer-events-auto data-[scrolling]:opacity-100 data-[scrolling=true]:opacity-100 data-[scrolling]:duration-0 data-[scrolling=true]:duration-0">
-        <ScrollArea.Thumb className="w-full rounded-full bg-gray-500" />
+      <ScrollArea.Scrollbar className="relative flex rounded bg-gray-200 opacity-0 transition-opacity pointer-events-none before:absolute before:content-[''] data-[orientation=vertical]:m-2 data-[orientation=vertical]:w-1 data-[orientation=vertical]:before:h-full data-[orientation=vertical]:before:w-5 data-[orientation=vertical]:before:left-1/2 data-[orientation=vertical]:before:-translate-x-1/2 data-[orientation=horizontal]:m-2 data-[orientation=horizontal]:h-1 data-[orientation=horizontal]:before:h-5 data-[orientation=horizontal]:before:w-full data-[orientation=horizontal]:before:left-0 data-[orientation=horizontal]:before:right-0 data-[orientation=horizontal]:before:-bottom-2 data-[hovering]:pointer-events-auto data-[hovering]:opacity-100 data-[hovering]:delay-0 data-[scrolling]:pointer-events-auto data-[scrolling]:opacity-100 data-[scrolling]:duration-0">
+        <ScrollArea.Thumb className="w-full rounded bg-gray-500" />
       </ScrollArea.Scrollbar>
       <ScrollArea.Scrollbar
-        className="pointer-events-none m-1 flex rounded-full bg-gray-200 opacity-0 transition-opacity duration-150 data-[orientation=vertical]:ml-0 data-[orientation=vertical]:w-1.25 data-[orientation=horizontal]:mt-0 data-[orientation=horizontal]:h-1.5 data-[hovering]:pointer-events-auto data-[hovering=true]:pointer-events-auto data-[hovering]:opacity-100 data-[hovering=true]:opacity-100 data-[scrolling]:pointer-events-auto data-[scrolling=true]:pointer-events-auto data-[scrolling]:opacity-100 data-[scrolling=true]:opacity-100 data-[scrolling]:duration-0 data-[scrolling=true]:duration-0"
+        className="relative flex rounded bg-gray-200 opacity-0 transition-opacity pointer-events-none before:absolute before:content-[''] data-[orientation=vertical]:m-2 data-[orientation=vertical]:w-1 data-[orientation=vertical]:before:h-full data-[orientation=vertical]:before:w-5 data-[orientation=vertical]:before:left-1/2 data-[orientation=vertical]:before:-translate-x-1/2 data-[orientation=horizontal]:m-2 data-[orientation=horizontal]:h-1 data-[orientation=horizontal]:before:h-5 data-[orientation=horizontal]:before:w-full data-[orientation=horizontal]:before:left-0 data-[orientation=horizontal]:before:right-0 data-[orientation=horizontal]:before:-bottom-2 data-[hovering]:pointer-events-auto data-[hovering]:opacity-100 data-[hovering]:delay-0 data-[scrolling]:pointer-events-auto data-[scrolling]:opacity-100 data-[scrolling]:duration-0"
         orientation="horizontal"
       >
-        <ScrollArea.Thumb className="w-full rounded-full bg-gray-500" />
+        <ScrollArea.Thumb className="w-full rounded bg-gray-500" />
       </ScrollArea.Scrollbar>
       <ScrollArea.Corner />
     </ScrollArea.Root>
