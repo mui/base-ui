8a30f2a0f7cd2c44ea836727fbb607b55179eff2
Olivier Tassinari <oliviertassinari@users.noreply.github.com>
2025-11-20 15:51:41 +0100

    [docs-infra] Fix a11y and HTML errors on baseline docs page (#2718)
    Co-authored-by: Albert Yu <albert@albertyu.co>

--- docs/src/components/Table.tsx
+++ docs/src/components/Table.tsx
@@ -3,33 +3,33 @@ import * as React from 'react';
 import clsx from 'clsx';
 import { observeScrollableInner } from '../utils/observeScrollableInner';
 
-export function Root({ children, ...props }: React.ComponentProps<'div'>) {
+export function Root({ className, children, ...other }: React.ComponentProps<'div'>) {
   return (
-    <div {...props} className={clsx('TableRoot', props.className)}>
+    <div className={clsx('TableRoot', className)} {...other}>
       <table className="TableRootTable">{children}</table>
     </div>
   );
 }
 
-export function Head({ className, ...props }: React.ComponentProps<'thead'>) {
-  return <thead className={clsx('TableHead', className)} {...props} />;
+export function Head(props: React.ComponentProps<'thead'>) {
+  return <thead {...props} className={clsx('TableHead', props.className)} />;
 }
 
-export function Body({ className, ...props }: React.ComponentProps<'tbody'>) {
-  return <tbody className={clsx('TableBody', className)} {...props} />;
+export function Body(props: React.ComponentProps<'tbody'>) {
+  return <tbody {...props} className={clsx('TableBody', props.className)} />;
 }
 
-export function Row({ className, ...props }: React.ComponentProps<'tr'>) {
-  return <tr className={clsx('TableRow', className)} {...props} />;
+export function Row(props: React.ComponentProps<'tr'>) {
+  return <tr {...props} className={clsx('TableRow', props.className)} />;
 }
 
 export function ColumnHeader({
   children,
   className,
-  ...props
+  ...other
 }: Omit<React.ComponentProps<'th'>, 'scope'>) {
   return (
-    <th scope="col" className={clsx('TableColumnHeader', className)} {...props}>
+    <th scope="col" className={clsx('TableColumnHeader', className)} {...other}>
       <span className="TableCellInner">{children}</span>
     </th>
   );
@@ -38,23 +38,23 @@ export function ColumnHeader({
 export function RowHeader({
   children,
   className,
-  ...props
+  ...other
 }: Omit<React.ComponentProps<'th'>, 'scope'>) {
   return (
     <th
       scope="row"
       ref={observeScrollableInner}
       className={clsx('TableCell', className)}
-      {...props}
+      {...other}
     >
       <span className="TableCellInner">{children}</span>
     </th>
   );
 }
 
-export function Cell({ children, className, ...props }: React.ComponentProps<'td'>) {
+export function Cell({ children, className, ...other }: React.ComponentProps<'td'>) {
   return (
-    <td ref={observeScrollableInner} className={clsx('TableCell', className)} {...props}>
+    <td ref={observeScrollableInner} className={clsx('TableCell', className)} {...other}>
       <span className="TableCellInner">{children}</span>
     </td>
   );
