53c259fb45c858e8548c9360caf8101b239d165f
Lukas <llukas.tyla@gmail.com>
2025-12-01 15:23:11 +0200

    fix

--- docs/src/app/(private)/experiments/performance/tooltip-perf.tsx
+++ docs/src/app/(private)/experiments/performance/tooltip-perf.tsx
@@ -116,8 +116,7 @@ function ExampleBaseUIDetachedTooltip() {
             <BaseTooltip.Positioner sideOffset={10}>
               <BaseTooltip.Popup className={styles.Popup}>
                 <BaseTooltip.Arrow className={styles.Arrow} />
-                <span>payload: {payload}</span>
-                <span>{payload !== undefined && `Bold Item ${payload + 1}`}</span>
+                <span>{payload !== undefined && `Bold Item ${payload}`}</span>
               </BaseTooltip.Popup>
             </BaseTooltip.Positioner>
           </BaseTooltip.Portal>
