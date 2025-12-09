cf4c3f0b02e5f06e03db05c42063474ef37acf7f
dav-is <mail@connordav.is>
2025-12-09 10:00:28 -0500

    Typo

--- docs/src/components/Search/SearchBar.tsx
+++ docs/src/components/Search/SearchBar.tsx
@@ -304,7 +304,7 @@ export function SearchBar({
                       >
                         <CornerDownLeft />
                       </kbd>
-                      <span>Go to pagSe</span>
+                      <span>Go to page</span>
                     </div>
                   </div>
                 </Autocomplete.Root>
