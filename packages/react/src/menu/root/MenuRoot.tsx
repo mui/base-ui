98959b3ba973b0f0d853188d8e0d18b902e41f1d
atomiks <cc.glows@gmail.com>
2025-12-10 23:05:31 +1100

    [menu] Fix null open method

--- packages/react/src/menu/root/MenuRoot.tsx
+++ packages/react/src/menu/root/MenuRoot.tsx
@@ -476,7 +476,7 @@ export function MenuRoot<Payload>(props: MenuRoot.Props<Payload>) {
   ]);
 
   const activeTriggerProps = React.useMemo(() => {
-    const referenceProps = mergeProps(
+    const mergedProps = mergeProps(
       getReferenceProps(),
       {
         onMouseEnter() {
@@ -488,8 +488,9 @@ export function MenuRoot<Payload>(props: MenuRoot.Props<Payload>) {
       },
       interactionTypeProps,
     );
-    delete referenceProps.role;
-    return referenceProps;
+
+    delete mergedProps.role;
+    return mergedProps;
   }, [getReferenceProps, store, interactionTypeProps]);
 
   const inactiveTriggerProps = React.useMemo(() => {
@@ -498,9 +499,11 @@ export function MenuRoot<Payload>(props: MenuRoot.Props<Payload>) {
       return triggerProps;
     }
 
-    const { role: roleDiscarded, ['aria-controls']: ariaControlsDiscarded, ...rest } = triggerProps;
-    return rest;
-  }, [getTriggerProps]);
+    const mergedProps = mergeProps(triggerProps, interactionTypeProps);
+    delete mergedProps.role;
+    delete mergedProps['aria-controls'];
+    return mergedProps;
+  }, [getTriggerProps, interactionTypeProps]);
 
   const disableHoverTimeout = useAnimationFrame();
   const popupProps = React.useMemo(
