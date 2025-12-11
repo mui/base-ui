13897eb4400bfb5c514532d917a5a5ad7a1ed665
Michał Dudak <michal@mui.com>
2025-12-11 09:35:14 +0100

    Fix CSS imports

--- docs/src/app/(website)/layout.tsx
+++ docs/src/app/(website)/layout.tsx
@@ -3,37 +3,6 @@ import type { Metadata, Viewport } from 'next/types';
 import { GoogleAnalytics } from 'docs/src/components/GoogleAnalytics';
 import './fonts/index.css';
 import './css/index.css';
-import './css/components/Accordion.css';
-import './css/components/Body.css';
-import './css/components/Button.css';
-import './css/components/Figure.css';
-import './css/components/Icon.css';
-import './css/components/Key.css';
-import './css/components/Link.css';
-import './css/components/List.css';
-import './css/components/Menu.css';
-import './css/components/Popup.css';
-import './css/components/Separator.css';
-import './css/components/Text.css';
-import './css/utilities/align-items.css';
-import './css/utilities/box-sizing.css';
-import './css/utilities/display.css';
-import './css/utilities/flex-direction.css';
-import './css/utilities/flex-wrap.css';
-import './css/utilities/font-weight.css';
-import './css/utilities/gap.css';
-import './css/utilities/grid-auto-rows.css';
-import './css/utilities/grid-column-end.css';
-import './css/utilities/grid-column-start.css';
-import './css/utilities/grid-template-columns.css';
-import './css/utilities/height.css';
-import './css/utilities/justify-content.css';
-import './css/utilities/left.css';
-import './css/utilities/padding.css';
-import './css/utilities/position.css';
-import './css/utilities/text-align.css';
-import './css/utilities/top.css';
-import './css/utilities/transform.css';
 
 export default function Layout({ children }: React.PropsWithChildren) {
   return (
