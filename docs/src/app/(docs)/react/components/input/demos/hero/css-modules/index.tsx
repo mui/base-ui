b4953720396b1c16a30a244b9209e3c955d1a0e5
atomiks <cc.glows@gmail.com>
2025-12-30 21:55:57 +1100

    [docs] Label all demos and document labeling (#3635)

--- docs/src/app/(docs)/react/components/input/demos/hero/css-modules/index.tsx
+++ docs/src/app/(docs)/react/components/input/demos/hero/css-modules/index.tsx
@@ -2,5 +2,10 @@ import { Input } from '@base-ui/react/input';
 import styles from './index.module.css';
 
 export default function ExampleInput() {
-  return <Input placeholder="Name" className={styles.Input} />;
+  return (
+    <label className={styles.Label}>
+      Name
+      <Input placeholder="Enter your name" className={styles.Input} />
+    </label>
+  );
 }
