02657263b404cf61e9a2da86d5f0c4d6513f17eb
Seongmin Choi <berry9901@gmail.com>
2025-12-31 20:51:39 +0900

    [docs] Remove incorrectly documented `data-required` and `data-readonly` state attributes from `Slider` (#3651)

--- packages/react/src/slider/value/SliderValueDataAttributes.ts
+++ packages/react/src/slider/value/SliderValueDataAttributes.ts
@@ -13,14 +13,6 @@ export enum SliderValueDataAttributes {
    */
   disabled = 'data-disabled',
   /**
-   * Present when the slider is readonly.
-   */
-  readonly = 'data-readonly',
-  /**
-   * Present when the slider is required.
-   */
-  required = 'data-required',
-  /**
    * Present when the slider is in valid state (when wrapped in Field.Root).
    */
   valid = 'data-valid',
