769e98203087422568cdbe052b0c997faa11ddc0
Michał Dudak <michal@mui.com>
2026-01-05 16:21:18 +0100

    [docs] Fix custom 404 page

---
+++ docs/src/app/global-not-found.tsx
@@ -0,0 +1,20 @@
+import { Link } from 'docs/src/components/Link';
+import Layout from './(website)/layout';
+
+export default function NotFoundPage() {
+  return (
+    <Layout>
+      <section className="d-c">
+        <h1 className="Text size-3 bp2:size-4 gcs-1 gce-9 bp4:gce-5">Page not found</h1>
+        <div className="gcs-1 gce-9">
+          This page couldn't be found. Please return to the{' '}
+          <Link href="/react/overview/quick-start">docs</Link> or create a corresponding issue on{' '}
+          <Link href="https://github.com/mui/base-ui" skipExternalIcon>
+            GitHub
+          </Link>
+          .
+        </div>
+      </section>
+    </Layout>
+  );
+}
