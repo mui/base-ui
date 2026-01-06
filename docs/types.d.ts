16ca2ec00306e4ea3a2ab3fd9415f385286505bc
Michał Dudak <michal@mui.com>
2026-01-06 11:27:42 +0100

    noUncheckedSideEffectImports

--- docs/types.d.ts
+++ docs/types.d.ts
@@ -7,9 +7,4 @@ declare module '*.mdx' {
   export default MDXComponent;
 }
 
-declare module '*.module.css' {
-  const classes: { readonly [key: string]: string };
-  export default classes;
-}
-
 declare module '*.css';
