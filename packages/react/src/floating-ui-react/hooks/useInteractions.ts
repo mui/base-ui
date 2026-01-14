35940129cff8a57fce9ec6a5b86b4f5158a2cc13
sai6855 <fayazceg@gmail.com>
2026-01-14 13:34:34 +0530

    fix

--- packages/react/src/floating-ui-react/hooks/useInteractions.ts
+++ packages/react/src/floating-ui-react/hooks/useInteractions.ts
@@ -24,10 +24,20 @@ export interface UseInteractionsReturn {
  * @see https://floating-ui.com/docs/useInteractions
  */
 export function useInteractions(propsList: Array<ElementProps | void> = []): UseInteractionsReturn {
-  const referenceDeps = propsList.map((key) => key?.reference);
-  const floatingDeps = propsList.map((key) => key?.floating);
-  const itemDeps = propsList.map((key) => key?.item);
-  const triggerDeps = propsList.map((key) => key?.trigger);
+  const { referenceDeps, floatingDeps, itemDeps, triggerDeps } = propsList.reduce(
+    (acc, props) => ({
+      referenceDeps: [...acc.referenceDeps, props?.reference],
+      floatingDeps: [...acc.floatingDeps, props?.floating],
+      itemDeps: [...acc.itemDeps, props?.item],
+      triggerDeps: [...acc.triggerDeps, props?.trigger],
+    }),
+    {
+      referenceDeps: [] as Array<void | ElementProps['reference']>,
+      floatingDeps: [] as Array<void | ElementProps['floating']>,
+      itemDeps: [] as Array<void | ElementProps['item']>,
+      triggerDeps: [] as Array<void | ElementProps['trigger']>,
+    },
+  );
 
   const getReferenceProps = React.useCallback(
     (userProps?: React.HTMLProps<Element>) => mergeProps(userProps, propsList, 'reference'),
