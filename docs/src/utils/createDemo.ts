653cc13067b2b0215f040e8caa6392f879713130
dav-is <mail@connordav.is>
2026-01-22 23:11:59 -0500

    Fix typedocs

--- docs/src/utils/createDemo.ts
+++ docs/src/utils/createDemo.ts
@@ -14,7 +14,7 @@ const demoGlobalData = [DemoDataTheme];
  * Creates a demo component for displaying code examples with syntax highlighting.
  * @param url Depends on `import.meta.url` to determine the source file location.
  * @param component The component to be rendered in the demo.
- * @param meta Additional meta for the demo.
+ * @param [meta] Additional meta for the demo.
  */
 export const createDemo = createDemoFactory({
   DemoContent,
@@ -26,7 +26,7 @@ export const createDemo = createDemoFactory({
  * A variant is a different implementation style of the same component.
  * @param url Depends on `import.meta.url` to determine the source file location.
  * @param variants The variants of the component to be rendered in the demo.
- * @param meta Additional meta for the demo.
+ * @param [meta] Additional meta for the demo.
  */
 export const createDemoWithVariants = createDemoWithVariantsFactory({
   DemoContent,
--- docs/src/utils/createTypes.tsx
+++ docs/src/utils/createTypes.tsx
@@ -32,7 +32,7 @@ const inlineComponents: MDXComponents = {
  * Creates a type doc component that renders a reference table for the given component.
  * @param url Depends on `import.meta.url` to determine the source file location.
  * @param component The component to render a reference table for.
- * @param meta Additional meta for the typedocs.
+ * @param [meta] Additional meta for the typedocs.
  */
 export const createTypes = createTypesFactory({
   TypesContent: ReferenceTable,
@@ -45,7 +45,7 @@ export const createTypes = createTypesFactory({
  * A variant is a different implementation style of the same component.
  * @param url Depends on `import.meta.url` to determine the source file location.
  * @param components The components to render reference tables for.
- * @param meta Additional meta for the typedocs.
+ * @param [meta] Additional meta for the typedocs.
  */
 export const createMultipleTypes = createMultipleTypesFactory({
   TypesContent: ReferenceTable,
