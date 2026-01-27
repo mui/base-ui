b2043f877e33272eff388f46d44673d850f49fc2
Michał Dudak <michal@mui.com>
2026-01-27 12:26:42 +0100

    Enable sourcemaps

--- playground/vite-app/vite.config.ts
+++ playground/vite-app/vite.config.ts
@@ -6,6 +6,9 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  build: {
+    sourcemap: true,
+  },
   resolve: {
     alias: {
       '@base-ui/react': path.resolve(__dirname, '..', '..', 'packages', 'react', 'src'),
