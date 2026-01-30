ec0ffc2b5b1f6671eb9ffc6e376ea669a128df80
dav-is <mail@connordav.is>
2026-01-29 21:39:01 -0500

    Replace icon in additional types

--- docs/src/components/ReferenceTable/AdditionalTypesAccordion.tsx
+++ docs/src/components/ReferenceTable/AdditionalTypesAccordion.tsx
@@ -29,7 +29,14 @@ export function AdditionalTypesAccordion({
               className={`group relative flex w-full items-baseline justify-between gap-4 bg-gray-50 py-2 pr-1 pl-3 text-left font-medium hover:bg-gray-100 focus-visible:z-1 focus-visible:outline-2 focus-visible:outline-blue-800 scroll-mt-12 md:scroll-mt-0 ${isFirst ? 'rounded-t-[5px]' : ''} ${isLast ? 'rounded-b-[5px]' : ''}`}
             >
               {additionalType.name}
-              <PlusIcon className="mr-2 size-3 shrink-0 transition-all ease-out group-open:scale-110 group-open:rotate-45" />
+              <svg
+                className="AccordionIcon mr-2 size-2.5 shrink-0"
+                viewBox="0 0 10 10"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M1 3.5L5 7.5L9 3.5" stroke="currentcolor" />
+              </svg>
             </Accordion.Trigger>
             <Accordion.Panel className="text-base text-gray-600">
               <Accordion.Content>
@@ -57,13 +64,5 @@ export function AdditionalTypesAccordion({
         );
       })}
     </Accordion.Root>
-  );
-}
-
-function PlusIcon(props: React.ComponentProps<'svg'>) {
-  return (
-    <svg viewBox="0 0 12 12" fill="currentcolor" {...props}>
-      <path d="M6.75 0H5.25V5.25H0V6.75L5.25 6.75V12H6.75V6.75L12 6.75V5.25H6.75V0Z" />
-    </svg>
   );
 }
