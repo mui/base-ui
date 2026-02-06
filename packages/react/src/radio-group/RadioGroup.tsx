4c59249e410faf9fce7bb255d87e84dd4b6909b1
Albert Yu <albert@albertyu.co>
2026-02-06 17:31:32 +0800

    fix RadioGroup

--- packages/react/src/radio-group/RadioGroup.tsx
+++ packages/react/src/radio-group/RadioGroup.tsx
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import { useControlled } from '@base-ui/utils/useControlled';
 import { useStableCallback } from '@base-ui/utils/useStableCallback';
+import { useIsoLayoutEffect } from '@base-ui/utils/useIsoLayoutEffect';
 import type { BaseUIComponentProps, HTMLProps } from '../utils/types';
 import { useBaseUiId } from '../utils/useBaseUiId';
 import { contains } from '../floating-ui-react/utils';
@@ -171,6 +172,12 @@ export const RadioGroup = React.forwardRef(function RadioGroup<Value>(
       setInputRef(fallbackInput);
     }
   });
+
+  useIsoLayoutEffect(() => {
+    if (disabled) {
+      setFocused(false);
+    }
+  }, [disabled, setFocused]);
 
   const [touched, setTouched] = React.useState(false);
 
