33e51afc946e2ed503e26c9751331e868b4bdba6
sai6855 <fayazceg@gmail.com>
2026-02-09 17:23:06 +0530

    fix

--- packages/react/src/utils/createContext.ts
+++ packages/react/src/utils/createContext.ts
@@ -4,7 +4,7 @@ import * as React from 'react';
 export function useContext<T>(
   Context: React.Context<T | undefined>,
   errorMessage: string,
-  optional: boolean = true,
+  optional: boolean = false,
 ) {
   const context = React.useContext(Context);
   if (context === undefined && !optional) {
