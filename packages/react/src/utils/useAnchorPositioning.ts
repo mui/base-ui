a991e7a1c4d4b0feb16d72000873a6ee6cc96b74
sai6855 <fayazceg@gmail.com>
2026-02-09 22:05:27 +0530

    fix

--- packages/react/src/utils/useAnchorPositioning.ts
+++ packages/react/src/utils/useAnchorPositioning.ts
@@ -304,14 +304,11 @@ export function useAnchorPositioning(
     size({
       ...commonCollisionProps,
       apply({ elements: { floating }, rects: { reference }, availableWidth, availableHeight }) {
-        Object.entries({
-          '--available-width': `${availableWidth}px`,
-          '--available-height': `${availableHeight}px`,
-          '--anchor-width': `${reference.width}px`,
-          '--anchor-height': `${reference.height}px`,
-        }).forEach(([key, value]) => {
-          floating.style.setProperty(key, value);
-        });
+        const s = floating.style;
+        s.setProperty('--available-width', `${availableWidth}px`);
+        s.setProperty('--available-height', `${availableHeight}px`);
+        s.setProperty('--anchor-width', `${reference.width}px`);
+        s.setProperty('--anchor-height', `${reference.height}px`);
       },
     }),
     arrow(
