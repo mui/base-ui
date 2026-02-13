a2d8efb1784397b38a259240d091c52679675efe
Lukas <llukas.tyla@gmail.com>
2026-02-13 16:50:09 +0200

    Reuse context

--- packages/react/src/calendar/use-context/CalendarContext.ts
+++ packages/react/src/calendar/use-context/CalendarContext.ts
@@ -1,24 +1,10 @@
 'use client';
-import * as React from 'react';
 import { useStore } from '@base-ui/utils/store';
-import { TemporalSupportedObject } from '../../types/temporal';
-import { SharedCalendarRootContext } from '../root/SharedCalendarRootContext';
+import { useSharedCalendarRootContext } from '../root/SharedCalendarRootContext';
 import { selectors } from '../store';
 
-export interface CalendarContext {
-  /**
-   * The currently visible date.
-   */
-  visibleDate: TemporalSupportedObject;
-}
-
 export function useCalendarContext() {
-  const store = React.useContext(SharedCalendarRootContext);
-  if (store === undefined) {
-    throw new Error(
-      'Base UI: SharedCalendarRootContext is missing. Calendar parts must be placed within <Calendar.Root />.',
-    );
-  }
+  const store = useSharedCalendarRootContext();
 
   return useStore(store, selectors.publicContext);
 }
