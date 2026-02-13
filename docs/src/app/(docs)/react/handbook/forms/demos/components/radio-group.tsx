9f70d5e4ac72410ca2bb53b7c23681d95c422e6a
Albert Yu <albert@albertyu.co>
2026-02-13 17:09:19 +0800

    update form radio group examples

--- docs/src/app/(docs)/react/handbook/forms/demos/components/radio-group.tsx
+++ docs/src/app/(docs)/react/handbook/forms/demos/components/radio-group.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import clsx from 'clsx';
 import { RadioGroup as BaseRadioGroup } from '@base-ui/react/radio-group';
 
-export function RadioGroup({ className, ...props }: BaseRadioGroup.Props) {
+export function RadioGroup<Value>({ className, ...props }: BaseRadioGroup.Props<Value>) {
   return (
     <BaseRadioGroup
       className={clsx('w-full flex flex-row items-start gap-1 text-gray-900', className)}
