96c9daaa9a11ea542a1a411e286a0aed5079c5fe
Lukas <llukas.tyla@gmail.com>
2026-02-16 17:41:59 +0200

    Add a demo showcasing timezone updates

---
+++ docs/src/app/(docs)/react/components/unstable-calendar/demos/timezone-display/index.ts
@@ -0,0 +1,4 @@
+import { createDemoWithVariants } from 'docs/src/utils/createDemo';
+import CssModules from './css-modules';
+
+export const DemoCalendarTimezoneDisplay = createDemoWithVariants(import.meta.url, { CssModules });
