cb5918880daa22020fe165be3e6813411afe2a8e
Aarón García Hervás <aaron@mui.com>
2026-03-02 11:17:29 +0100

    Revert "import mdx.css in mdx-components.tsx"
    This reverts commit 97c87b328dd0744d7a5827cd2fe7d3841667c397.

--- docs/src/mdx-components.tsx
+++ docs/src/mdx-components.tsx
@@ -12,7 +12,6 @@ import { Link } from './components/Link';
 import { HeadingLink } from './components/HeadingLink';
 import { Subtitle } from './components/Subtitle/Subtitle';
 import { Kbd } from './components/Kbd/Kbd';
-import './mdx-components.css';
 
 interface MDXComponents {
   [key: string]: React.FC<any> | MDXComponents;
