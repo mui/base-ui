f5390d683b1855acc7c82ab19d90c31100698dc3
atomiks <cc.glows@gmail.com>
2026-03-11 22:12:23 +1100

    [combobox] Add InputGroup fallback regression tests

--- packages/react/src/combobox/positioner/ComboboxPositioner.test.tsx
+++ packages/react/src/combobox/positioner/ComboboxPositioner.test.tsx
@@ -19,6 +19,39 @@ describe('<Combobox.Positioner />', () => {
   }));
 
   describe.skipIf(isJSDOM)('default anchor', () => {
+    it('uses the input when input group is absent', async () => {
+      const inputWidth = 120;
+      const triggerWidth = 240;
+      let anchorWidth = 0;
+      const inputRef = React.createRef<HTMLInputElement>();
+
+      await render(
+        <Combobox.Root open>
+          <Combobox.Input ref={inputRef} style={{ width: inputWidth }} />
+          <Combobox.Trigger style={{ width: triggerWidth }}>Open</Combobox.Trigger>
+          <Combobox.Portal>
+            <Combobox.Positioner
+              sideOffset={(data) => {
+                anchorWidth = data.anchor.width;
+                return 0;
+              }}
+            >
+              <Combobox.Popup>
+                <Combobox.List>
+                  <Combobox.Item value="One">One</Combobox.Item>
+                </Combobox.List>
+              </Combobox.Popup>
+            </Combobox.Positioner>
+          </Combobox.Portal>
+        </Combobox.Root>,
+      );
+
+      await waitFor(() => {
+        expect(anchorWidth).toBeCloseTo(inputRef.current!.getBoundingClientRect().width, 0);
+        expect(anchorWidth).not.toBeCloseTo(triggerWidth, 0);
+      });
+    });
+
     it('uses the input group when present', async () => {
       const inputGroupWidth = 240;
       const inputWidth = 120;
