1c24e6db789fca00fb3cd1580c1fb7bef3c4ae90
atomiks <cc.glows@gmail.com>
2026-03-13 16:32:43 +1100

    [all components] Tighten render prop warning heuristic

--- packages/react/src/utils/useRenderElement.tsx
+++ packages/react/src/utils/useRenderElement.tsx
@@ -110,6 +110,7 @@ function useRenderElementProps<
 //
 // TODO delete once https://github.com/facebook/react/issues/32392 is fixed
 const REACT_LAZY_TYPE = Symbol.for('react.lazy');
+const COMPONENT_IDENTIFIER_PATTERN = /^[A-Z][A-Za-z0-9_$]*$/;
 
 function evaluateRenderProp<T extends React.ElementType, S>(
   element: IntrinsicTagName | undefined,
@@ -173,8 +174,7 @@ function warnIfRenderPropLooksLikeComponent(renderFn: { name: string }) {
     return;
   }
 
-  const firstCharacterCode = functionName.charCodeAt(0);
-  if (firstCharacterCode < 65 || firstCharacterCode > 90) {
+  if (!COMPONENT_IDENTIFIER_PATTERN.test(functionName)) {
     return;
   }
 
