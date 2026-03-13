938bec12045e288f7bd97a85aeff2c9e13308e08
atomiks <cc.glows@gmail.com>
2026-03-13 17:20:10 +1100

    [select] Preserve touch exit animations

--- packages/react/src/select/root/SelectRoot.tsx
+++ packages/react/src/select/root/SelectRoot.tsx
@@ -4,6 +4,7 @@ import { visuallyHidden, visuallyHiddenInput } from '@base-ui/utils/visuallyHidd
 import { useMergedRefs } from '@base-ui/utils/useMergedRefs';
 import { useRefWithInit } from '@base-ui/utils/useRefWithInit';
 import { useOnFirstRender } from '@base-ui/utils/useOnFirstRender';
+import { usePreviousValue } from '@base-ui/utils/usePreviousValue';
 import { useControlled } from '@base-ui/utils/useControlled';
 import { useIsoLayoutEffect } from '@base-ui/utils/useIsoLayoutEffect';
 import { useStableCallback } from '@base-ui/utils/useStableCallback';
@@ -159,6 +160,9 @@ export function SelectRoot<Value, Multiple extends boolean | undefined = false>(
   const triggerElement = useStore(store, selectors.triggerElement);
   const positionerElement = useStore(store, selectors.positionerElement);
 
+  const previousOpenMethod = usePreviousValue(openMethod);
+  const renderedOpenMethod = openMethod ?? previousOpenMethod;
+
   const serializedValue = React.useMemo(() => {
     if (multiple && Array.isArray(value) && value.length === 0) {
       return '';
@@ -274,7 +278,7 @@ export function SelectRoot<Value, Multiple extends boolean | undefined = false>(
 
   const handleUnmount = useStableCallback(() => {
     setMounted(false);
-    store.set('activeIndex', null);
+    store.update({ activeIndex: null, openMethod: null });
     onOpenChangeComplete?.(false);
   });
 
@@ -413,7 +417,7 @@ export function SelectRoot<Value, Multiple extends boolean | undefined = false>(
       itemToStringLabel,
       itemToStringValue,
       isItemEqualToValue,
-      openMethod,
+      openMethod: renderedOpenMethod,
     });
   }, [
     store,
@@ -430,7 +434,7 @@ export function SelectRoot<Value, Multiple extends boolean | undefined = false>(
     itemToStringLabel,
     itemToStringValue,
     isItemEqualToValue,
-    openMethod,
+    renderedOpenMethod,
   ]);
 
   const contextValue: SelectRootContext = React.useMemo(
