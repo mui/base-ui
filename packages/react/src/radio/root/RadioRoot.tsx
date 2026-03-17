c8ca80eeaa1aa1e5d13493541f5dc97e6c4f5525
atomiks <cc.glows@gmail.com>
2026-03-17 22:50:16 +1100

    [radio] Remove Radio.Root form prop

--- packages/react/src/radio/root/RadioRoot.tsx
+++ packages/react/src/radio/root/RadioRoot.tsx
@@ -43,7 +43,6 @@ export const RadioRoot = React.forwardRef(function RadioRoot<Value>(
     required: requiredProp = false,
     'aria-labelledby': ariaLabelledByProp,
     value,
-    form: formProp,
     inputRef: inputRefProp,
     nativeButton = false,
     id: idProp,
@@ -81,7 +80,7 @@ export const RadioRoot = React.forwardRef(function RadioRoot<Value>(
   const disabled = fieldDisabled || fieldItemContext.disabled || disabledGroup || disabledProp;
   const readOnly = readOnlyGroup || readOnlyProp;
   const required = requiredGroup || requiredProp;
-  const form = formGroup ?? formProp;
+  const form = formGroup;
 
   const checked = groupContext ? checkedValue === value : value === '';
   const serializedValue = React.useMemo(() => serializeValue(value), [value]);
@@ -314,11 +313,6 @@ export interface RadioRootProps<Value = any>
    * Whether the user should be unable to select the radio button.
    */
   readOnly?: boolean | undefined;
-  /**
-   * Identifies the form that owns the hidden input.
-   * Useful when the radio is rendered outside the form.
-   */
-  form?: string | undefined;
   /**
    * A ref to access the hidden input element.
    */
