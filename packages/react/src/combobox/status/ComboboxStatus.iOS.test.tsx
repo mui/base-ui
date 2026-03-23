a54cf66ef72a64753552db83cca44e9d7e17900d
atomiks <cc.glows@gmail.com>
2026-03-24 10:02:57 +1100

    [combobox] Fix iOS test import order

--- packages/react/src/combobox/status/ComboboxStatus.iOS.test.tsx
+++ packages/react/src/combobox/status/ComboboxStatus.iOS.test.tsx
@@ -1,4 +1,5 @@
 import { expect, vi } from 'vitest';
+import { Combobox } from '@base-ui/react/combobox';
 import { screen } from '@mui/internal-test-utils';
 import { createRenderer } from '#test-utils';
 import { INITIAL_LIVE_REGION_TEXT_MUTATION_RESET_DELAY } from '../utils/constants';
@@ -13,8 +14,6 @@ vi.mock('@base-ui/utils/detectBrowser', async () => {
     isIOS: true,
   };
 });
-
-import { Combobox } from '@base-ui/react/combobox';
 
 describe('<Combobox.Status /> iOS', () => {
   const { render, clock } = createRenderer();
