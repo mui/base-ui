cdac973a3e9c9d3565d239e1a7255a12ea310aac
Michał Dudak <michal@mui.com>
2026-03-23 09:36:23 +0100

    Deduplicate

--- packages/react/src/listbox/group/ListboxGroupContext.ts
+++ packages/react/src/listbox/group/ListboxGroupContext.ts
@@ -1,19 +1,5 @@
 'use client';
-import * as React from 'react';
-
-export interface ListboxGroupContext {
-  labelId: string | undefined;
-  setLabelId: React.Dispatch<React.SetStateAction<string | undefined>>;
-}
-
-export const ListboxGroupContext = React.createContext<ListboxGroupContext | undefined>(undefined);
-
-export function useListboxGroupContext() {
-  const context = React.useContext(ListboxGroupContext);
-  if (context === undefined) {
-    throw new Error(
-      'Base UI: ListboxGroupContext is missing. ListboxGroup parts must be placed within <Listbox.Group>.',
-    );
-  }
-  return context;
-}
+export {
+  SelectGroupContext as ListboxGroupContext,
+  useSelectGroupContext as useListboxGroupContext,
+} from '../../select/group/SelectGroupContext';
