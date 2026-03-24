5d094ab64d35415675b71e40375c802700b572af
atomiks <cc.glows@gmail.com>
2026-03-25 09:36:32 +1100

    [otp-field] Fix tailwind focus outline demos

--- docs/src/app/(docs)/react/components/otp-field/demos/hero/tailwind/index.tsx
+++ docs/src/app/(docs)/react/components/otp-field/demos/hero/tailwind/index.tsx
@@ -20,7 +20,7 @@ export default function ExampleOTPField() {
         {Array.from({ length: OTP_LENGTH }, (_, index) => (
           <OTPField.Input
             key={index}
-            className="box-border m-0 h-11 w-10 rounded-lg border border-gray-300 bg-transparent text-center font-inherit text-lg font-medium text-gray-900 outline-none focus:outline-2 focus:-outline-offset-1 focus:outline-blue-800"
+            className="box-border m-0 h-11 w-10 rounded-lg border border-gray-300 bg-transparent text-center font-inherit text-lg font-medium text-gray-900 outline-none focus:outline-solid focus:outline-2 focus:-outline-offset-1 focus:outline-blue-800"
             aria-label={`Character ${index + 1} of ${OTP_LENGTH}`}
           />
         ))}
