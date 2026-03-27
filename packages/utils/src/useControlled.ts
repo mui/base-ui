27b4c7e1edc1dd21b0c4b3d5e1c984c26b700dd7
Oleg Baltag <online7890@gmail.com>
2026-03-27 12:42:49 +0200

    inline deepEqual

--- packages/utils/src/useControlled.ts
+++ packages/utils/src/useControlled.ts
@@ -2,7 +2,8 @@
 // TODO: uncomment once we enable eslint-plugin-react-compiler // eslint-disable-next-line react-compiler/react-compiler -- process.env never changes, dependency arrays are intentionally ignored
 /* eslint-disable react-hooks/rules-of-hooks, react-hooks/exhaustive-deps */
 import * as React from 'react';
-import { deepEqual } from './deepEqual';
+
+let deepEqual: (a: any, b: any) => boolean;
 
 export interface UseControlledProps<T = unknown> {
   /**
@@ -55,7 +56,6 @@ export function useControlled<T = unknown>({
     const { current: defaultValue } = React.useRef(defaultProp);
 
     React.useEffect(() => {
-      // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/is for more details.
       if (!isControlled && !deepEqual(defaultValue, defaultProp)) {
         console.error(
           [
@@ -74,4 +74,48 @@ export function useControlled<T = unknown>({
   }, []);
 
   return [value as T, setValueIfUncontrolled];
+}
+if (process.env.NODE_ENV !== 'production') {
+  deepEqual = (a: any, b: any) => {
+    if (a === b) {
+      return true;
+    }
+
+    // Handle NaN and null/primitive mismatches
+    if (!(a instanceof Object) || !(b instanceof Object)) {
+      return Number.isNaN(a) && Number.isNaN(b);
+    }
+
+    if (a.constructor !== b.constructor) {
+      return false;
+    }
+
+    // Convert Maps/Sets to Arrays to "cheat" and use the same logic
+    const arrA = a instanceof Set || a instanceof Map ? Array.from(a) : null;
+    const arrB = b instanceof Set || b instanceof Map ? Array.from(b) : null;
+
+    // If one is a Map/Set and the other isn't, they aren't equal
+    if (!!arrA !== !!arrB) {
+      return false;
+    }
+
+    if (arrA && arrB) {
+      if (arrA.length !== arrB.length) {
+        return false;
+      }
+      // Sets are unordered, so this only works for "Deep Equal" if
+      // the order happens to match or if you sort them (sorting is expensive).
+      return arrA.every((val, i) => deepEqual(val, arrB[i]));
+    }
+
+    // Standard Array/Object logic
+    const keys = Object.keys(a);
+    if (keys.length !== Object.keys(b).length) {
+      return false;
+    }
+
+    return keys.every(
+      (key) => Object.prototype.hasOwnProperty.call(b, key) && deepEqual(a[key], b[key]),
+    );
+  };
 }
