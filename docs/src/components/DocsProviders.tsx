41251c84768c4a1d3e46c3b8ea09298e771e28ce
dav-is <mail@connordav.is>
2026-03-31 18:23:39 -0400

    Improve html weight

--- docs/src/components/DocsProviders.tsx
+++ docs/src/components/DocsProviders.tsx
@@ -3,17 +3,20 @@ import { Tooltip } from '@base-ui/react/tooltip';
 import { TypesDataProvider } from '@mui/internal-docs-infra/useType';
 import { DemoVariantSelectorProvider } from 'docs/src/components/Demo/DemoVariantSelectorProvider';
 import { PackageManagerSnippetProvider } from 'docs/src/blocks/PackageManagerSnippet/PackageManagerSnippetProvider';
+import { CodeComponentsProvider } from 'docs/src/code-components';
 
 export function DocsProviders({ children }: { children: React.ReactNode }) {
   return (
     <Tooltip.Provider delay={350}>
-      <TypesDataProvider>
-        <DemoVariantSelectorProvider defaultVariant="css-modules" defaultLanguage="ts">
-          <PackageManagerSnippetProvider defaultValue="npm">
-            {children}
-          </PackageManagerSnippetProvider>
-        </DemoVariantSelectorProvider>
-      </TypesDataProvider>
+      <CodeComponentsProvider>
+        <TypesDataProvider>
+          <DemoVariantSelectorProvider defaultVariant="css-modules" defaultLanguage="ts">
+            <PackageManagerSnippetProvider defaultValue="npm">
+              {children}
+            </PackageManagerSnippetProvider>
+          </DemoVariantSelectorProvider>
+        </TypesDataProvider>
+      </CodeComponentsProvider>
     </Tooltip.Provider>
   );
 }
