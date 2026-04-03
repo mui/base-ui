4e7402b5871b48a0c07fe6692a3db6f069b4f74e
delangle <flaviendelangle@gmail.com>
2026-04-03 09:25:32 +0200

    Fix

--- packages/react/src/composite/index.ts
+++
@@ -1,7 +0,0 @@
-export { CompositeList } from '../internals/composite/list/CompositeList';
-export type { CompositeMetadata } from '../internals/composite/list/CompositeList';
-export { useCompositeListItem } from '../internals/composite/list/useCompositeListItem';
-export type {
-  UseCompositeListItemParameters,
-  IndexGuessBehavior,
-} from '../internals/composite/list/useCompositeListItem';
---
+++ packages/react/src/internals/composite/index.ts
@@ -0,0 +1,7 @@
+export { CompositeList } from './list/CompositeList';
+export type { CompositeMetadata } from './list/CompositeList';
+export { useCompositeListItem } from './list/useCompositeListItem';
+export type {
+  UseCompositeListItemParameters,
+  IndexGuessBehavior,
+} from './list/useCompositeListItem';
