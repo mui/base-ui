b6b96836dd4a2f0db8afe65b12d01aa36281eb6c
sai6855 <fayazceg@gmail.com>
2026-04-05 15:37:41 +0530

    Refactor: Replace enum exports with direct exports from corresponding data attributes files

--- packages/react/src/accordion/header/AccordionHeaderDataAttributes.ts
+++ packages/react/src/accordion/header/AccordionHeaderDataAttributes.ts
@@ -1,15 +1,1 @@
-export enum AccordionHeaderDataAttributes {
-  /**
-   * Indicates the index of the accordion item.
-   * @type {number}
-   */
-  index = 'data-index',
-  /**
-   * Present when the accordion item is disabled.
-   */
-  disabled = 'data-disabled',
-  /**
-   * Present when the accordion item is open.
-   */
-  open = 'data-open',
-}
+export { AccordionItemDataAttributes as AccordionHeaderDataAttributes } from '../item/AccordionItemDataAttributes';
