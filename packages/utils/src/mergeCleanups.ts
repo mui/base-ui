6b9e5693c563669b666ceeaa3fb0128ee956b34f
atomiks <cc.glows@gmail.com>
2026-04-07 00:10:31 +1000

    [all components] Clarify mergeCleanups JSDoc

--- packages/utils/src/mergeCleanups.ts
+++ packages/utils/src/mergeCleanups.ts
@@ -1,5 +1,8 @@
 type Cleanup = false | null | undefined | (() => void);
 
+/**
+ * Combines multiple cleanup functions into a single cleanup function.
+ */
 export function mergeCleanups(...cleanups: Cleanup[]) {
   return () => {
     for (let i = 0; i < cleanups.length; i += 1) {
