441b9128d23986cd6be2ce944140f7b56744337b
Aarón García Hervás <aaron@mui.com>
2026-04-17 16:23:44 +0200

    Button demos: remove active state

--- docs/src/app/(docs)/react/components/button/demos/loading/tailwind/index.tsx
+++ docs/src/app/(docs)/react/components/button/demos/loading/tailwind/index.tsx
@@ -7,7 +7,7 @@ export default function ExampleButton() {
 
   return (
     <Button
-      className="flex items-center justify-center h-8 px-3.5 border-none rounded-none bg-gray-200 dark:bg-gray-800 font-inherit text-sm font-normal leading-6 text-gray-900 dark:text-gray-50 select-none hover:not-data-disabled:bg-gray-300 dark:hover:not-data-disabled:bg-gray-700 active:not-data-disabled:bg-gray-400 dark:active:not-data-disabled:bg-gray-600 focus-visible:outline-2 focus-visible:outline-offset-2 data-disabled:text-gray-500 dark:data-disabled:text-gray-400"
+      className="flex items-center justify-center h-8 px-3.5 border-none rounded-none bg-gray-200 dark:bg-gray-800 font-inherit text-sm font-normal leading-6 text-gray-900 dark:text-gray-50 select-none hover:not-data-disabled:bg-gray-300 dark:hover:not-data-disabled:bg-gray-700 focus-visible:outline-2 focus-visible:outline-offset-2 data-disabled:text-gray-500 dark:data-disabled:text-gray-400"
       disabled={loading}
       focusableWhenDisabled
       onClick={() => {
