fcd31a265797bec7cb84a813ae7bacd3b6bcea06
Aarón García Hervás <aaron@mui.com>
2026-04-17 11:39:52 +0200

    Radio brutalist

--- docs/src/app/(docs)/react/components/radio/demos/hero/tailwind/index.tsx
+++ docs/src/app/(docs)/react/components/radio/demos/hero/tailwind/index.tsx
@@ -9,38 +9,38 @@ export default function ExampleRadioGroup() {
     <RadioGroup
       aria-labelledby={id}
       defaultValue="fuji-apple"
-      className="flex flex-col items-start gap-1 text-gray-900"
+      className="flex flex-col items-start gap-1 text-gray-900 dark:text-gray-50"
     >
-      <div className="font-bold" id={id}>
+      <div className="text-sm leading-5 font-bold" id={id}>
         Best apple
       </div>
 
-      <label className="flex items-center gap-2 font-normal">
+      <label className="flex items-center gap-2 text-sm leading-5 font-normal text-gray-900 dark:text-gray-50">
         <Radio.Root
           value="fuji-apple"
-          className="flex size-5 items-center justify-center rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-800 data-[checked]:bg-gray-900 data-[unchecked]:border data-[unchecked]:border-gray-300"
+          className="flex size-4 items-center justify-center border rounded-full p-0 border-gray-900 bg-gray-50 text-gray-50 dark:border-gray-50 dark:bg-gray-900 dark:text-gray-900 data-checked:bg-gray-900 data-checked:text-gray-50 dark:data-checked:bg-gray-50 dark:data-checked:text-gray-900 focus-visible:outline-2 focus-visible:outline-offset-2"
         >
-          <Radio.Indicator className="flex before:size-2 before:rounded-full before:bg-gray-50 data-[unchecked]:hidden" />
+          <Radio.Indicator className="flex items-center justify-center data-unchecked:hidden before:size-2 before:rounded-full before:bg-current" />
         </Radio.Root>
         Fuji
       </label>
 
-      <label className="flex items-center gap-2 font-normal">
+      <label className="flex items-center gap-2 text-sm leading-5 font-normal text-gray-900 dark:text-gray-50">
         <Radio.Root
           value="gala-apple"
-          className="flex size-5 items-center justify-center rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-800 data-[checked]:bg-gray-900 data-[unchecked]:border data-[unchecked]:border-gray-300"
+          className="flex size-4 items-center justify-center border rounded-full p-0 border-gray-900 bg-gray-50 text-gray-50 dark:border-gray-50 dark:bg-gray-900 dark:text-gray-900 data-checked:bg-gray-900 data-checked:text-gray-50 dark:data-checked:bg-gray-50 dark:data-checked:text-gray-900 focus-visible:outline-2 focus-visible:outline-offset-2"
         >
-          <Radio.Indicator className="flex before:size-2 before:rounded-full before:bg-gray-50 data-[unchecked]:hidden" />
+          <Radio.Indicator className="flex items-center justify-center data-unchecked:hidden before:size-2 before:rounded-full before:bg-current" />
         </Radio.Root>
         Gala
       </label>
 
-      <label className="flex items-center gap-2 font-normal">
+      <label className="flex items-center gap-2 text-sm leading-5 font-normal text-gray-900 dark:text-gray-50">
         <Radio.Root
           value="granny-smith-apple"
-          className="flex size-5 items-center justify-center rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-800 data-[checked]:bg-gray-900 data-[unchecked]:border data-[unchecked]:border-gray-300"
+          className="flex size-4 items-center justify-center border rounded-full p-0 border-gray-900 bg-gray-50 text-gray-50 dark:border-gray-50 dark:bg-gray-900 dark:text-gray-900 data-checked:bg-gray-900 data-checked:text-gray-50 dark:data-checked:bg-gray-50 dark:data-checked:text-gray-900 focus-visible:outline-2 focus-visible:outline-offset-2"
         >
-          <Radio.Indicator className="flex before:size-2 before:rounded-full before:bg-gray-50 data-[unchecked]:hidden" />
+          <Radio.Indicator className="flex items-center justify-center data-unchecked:hidden before:size-2 before:rounded-full before:bg-current" />
         </Radio.Root>
         Granny Smith
       </label>
