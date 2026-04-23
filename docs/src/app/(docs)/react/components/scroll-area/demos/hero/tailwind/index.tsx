84baa122c9c22c719cbd6c709a13e05d4d6b4cda
Aarón García Hervás <aaron@mui.com>
2026-04-23 15:54:24 +0200

    ScrollArea brutalist

--- docs/src/app/(docs)/react/components/scroll-area/demos/hero/tailwind/index.tsx
+++ docs/src/app/(docs)/react/components/scroll-area/demos/hero/tailwind/index.tsx
@@ -2,9 +2,9 @@ import { ScrollArea } from '@base-ui/react/scroll-area';
 
 export default function ExampleScrollArea() {
   return (
-    <ScrollArea.Root className="h-[8.5rem] w-96 max-w-[calc(100vw-8rem)]">
+    <ScrollArea.Root className="h-[8.5rem] w-96 max-w-[calc(100vw-8rem)] bg-white dark:bg-gray-950">
       <ScrollArea.Viewport className="h-full outline-1 -outline-offset-1 outline-gray-900 dark:outline-white focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-blue-800">
-        <div className="flex flex-col gap-4 py-3 pr-6 pl-4 text-sm leading-[1.375rem] text-gray-900 dark:text-white">
+        <ScrollArea.Content className="flex flex-col gap-4 py-3 pr-6 pl-4 text-sm leading-[1.375rem] text-gray-900 dark:text-white">
           <p>
             Vernacular architecture is building done outside any academic tradition, and without
             professional guidance. It is not a particular architectural movement or style, but
@@ -24,10 +24,10 @@ export default function ExampleScrollArea() {
             conscious with contemporary design and construction—part of a broader interest in
             sustainable design.
           </p>
-        </div>
+        </ScrollArea.Content>
       </ScrollArea.Viewport>
-      <ScrollArea.Scrollbar className="m-px flex w-4 justify-center bg-gray-900/25 dark:bg-gray-50/25 opacity-0 transition-opacity pointer-events-none before:absolute before:h-full before:w-5 before:content-[''] data-[hovering]:opacity-100 data-[hovering]:delay-0 data-[hovering]:pointer-events-auto data-[scrolling]:opacity-100 data-[scrolling]:duration-0 data-[scrolling]:pointer-events-auto">
-        <ScrollArea.Thumb className="w-full bg-gray-900/80 dark:bg-gray-50/80" />
+      <ScrollArea.Scrollbar className="m-px flex w-4 justify-center bg-black/12 dark:bg-white/12 opacity-0 transition-opacity pointer-events-none before:absolute before:h-full before:w-5 before:content-[''] data-[hovering]:opacity-100 data-[hovering]:pointer-events-auto data-[scrolling]:opacity-100 data-[scrolling]:duration-0 data-[scrolling]:pointer-events-auto">
+        <ScrollArea.Thumb className="w-full bg-gray-900 dark:bg-white" />
       </ScrollArea.Scrollbar>
     </ScrollArea.Root>
   );
