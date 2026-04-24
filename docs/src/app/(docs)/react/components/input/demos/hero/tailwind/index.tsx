a3f6a96ecb0aa628b76c4aed21518906df8b1cfb
Aarón García Hervás <aaron@mui.com>
2026-04-24 10:53:39 +0200

    Input: change outline offset

--- docs/src/app/(docs)/react/components/input/demos/hero/tailwind/index.tsx
+++ docs/src/app/(docs)/react/components/input/demos/hero/tailwind/index.tsx
@@ -6,7 +6,7 @@ export default function ExampleInput() {
       Name
       <Input
         placeholder="e.g. Colm Tuite"
-        className="h-8 w-40 border border-gray-950 dark:border-white bg-transparent px-2 text-sm font-normal text-gray-950 dark:text-white placeholder:text-gray-500 dark:placeholder:text-gray-400 focus:outline-2 focus:outline-offset-2 focus:outline-blue-800"
+        className="h-8 w-40 border border-gray-950 dark:border-white bg-transparent px-2 text-sm font-normal text-gray-950 dark:text-white placeholder:text-gray-500 dark:placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-800"
       />
     </label>
   );
