4cefcc04de0e2649cafc6dc08d0cd0ccc8c0c3f6
Aarón García Hervás <aaron@mui.com>
2026-04-24 11:24:47 +0200

    Collapsible: fix trigger's padding

--- docs/src/app/(docs)/react/components/collapsible/demos/hero/tailwind/index.tsx
+++ docs/src/app/(docs)/react/components/collapsible/demos/hero/tailwind/index.tsx
@@ -4,7 +4,7 @@ import { Collapsible } from '@base-ui/react/collapsible';
 export default function ExampleCollapsible() {
   return (
     <Collapsible.Root className="flex min-h-36 w-56 flex-col justify-center text-gray-950 dark:text-white">
-      <Collapsible.Trigger className="group flex h-8 items-center justify-between gap-2 rounded-none bg-gray-200 px-3 text-sm leading-5 font-normal text-gray-950 select-none hover:bg-gray-300 focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-blue-800 dark:bg-gray-800 dark:text-white dark:hover:bg-gray-700">
+      <Collapsible.Trigger className="group flex h-8 items-center justify-between gap-2 rounded-none bg-gray-200 pl-3 pr-2 text-sm leading-5 font-normal text-gray-950 select-none hover:bg-gray-300 focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-blue-800 dark:bg-gray-800 dark:text-white dark:hover:bg-gray-700">
         Recovery keys
         <ChevronIcon className="size-3 transition-transform duration-100 ease-out group-data-panel-open:rotate-90" />
       </Collapsible.Trigger>
