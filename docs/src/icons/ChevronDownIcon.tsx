4fac7e78a420ad8b6f76cf6e7cc6be35bf9cefda
Olivier Tassinari <oliviertassinari@users.noreply.github.com>
2026-04-27 02:01:36 +0200

    [internal] Fix currentColor attribute value
    Same as https://github.com/mui/mui-public/pull/1320

--- docs/src/icons/ChevronDownIcon.tsx
+++ docs/src/icons/ChevronDownIcon.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 export function ChevronDownIcon(props: React.ComponentProps<'svg'>) {
   return (
     <svg width="10" height="10" viewBox="0 0 10 10" fill="none" {...props}>
-      <path d="M1 3.5L5 7.5L9 3.5" stroke="currentcolor" />
+      <path d="M1 3.5L5 7.5L9 3.5" stroke="currentColor" />
     </svg>
   );
 }
