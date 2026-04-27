7f205bbf23a223e40610f209fa9c6f6974a13944
Aarón García Hervás <aaron@mui.com>
2026-04-27 23:07:49 +0200

    Fieldset brutalist

--- docs/src/app/(docs)/react/components/fieldset/demos/hero/tailwind/index.tsx
+++ docs/src/app/(docs)/react/components/fieldset/demos/hero/tailwind/index.tsx
@@ -4,23 +4,27 @@ import { Fieldset } from '@base-ui/react/fieldset';
 export default function ExampleField() {
   return (
     <Fieldset.Root className="flex w-full max-w-64 flex-col gap-4">
-      <Fieldset.Legend className="border-b border-gray-200 pb-3 text-lg font-bold text-gray-900">
+      <Fieldset.Legend className="border-b border-gray-950 text-base font-bold text-gray-950 dark:border-white dark:text-white">
         Billing details
       </Fieldset.Legend>
 
       <Field.Root className="flex flex-col items-start gap-1">
-        <Field.Label className="text-sm font-bold text-gray-900">Company</Field.Label>
+        <Field.Label className="text-sm font-bold text-gray-950 dark:text-white">
+          Company
+        </Field.Label>
         <Field.Control
           placeholder="Enter company name"
-          className="h-10 w-full rounded-md border border-gray-200 pl-3.5 text-base text-gray-900 focus:outline-2 focus:-outline-offset-1 focus:outline-blue-800 font-normal"
+          className="h-8 w-full border border-gray-950 bg-transparent px-2 text-sm font-normal text-gray-950 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-800 dark:border-white dark:text-white dark:placeholder:text-gray-400"
         />
       </Field.Root>
 
       <Field.Root className="flex flex-col items-start gap-1">
-        <Field.Label className="text-sm font-bold text-gray-900">Tax ID</Field.Label>
+        <Field.Label className="text-sm font-bold text-gray-950 dark:text-white">
+          Tax ID
+        </Field.Label>
         <Field.Control
           placeholder="Enter fiscal number"
-          className="h-10 w-full rounded-md border border-gray-200 pl-3.5 text-base text-gray-900 focus:outline-2 focus:-outline-offset-1 focus:outline-blue-800 font-normal"
+          className="h-8 w-full border border-gray-950 bg-transparent px-2 text-sm font-normal text-gray-950 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-800 dark:border-white dark:text-white dark:placeholder:text-gray-400"
         />
       </Field.Root>
     </Fieldset.Root>
