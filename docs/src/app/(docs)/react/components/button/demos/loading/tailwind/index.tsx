e0c7925441e4ceeb6c357b4fc8b012ab96a63634
Aarón García Hervás <aaron@mui.com>
2026-04-29 12:43:48 +0200

    Button: update hover, active, and disabled states

--- docs/src/app/(docs)/react/components/button/demos/loading/tailwind/index.tsx
+++ docs/src/app/(docs)/react/components/button/demos/loading/tailwind/index.tsx
@@ -7,7 +7,7 @@ export default function ExampleButton() {
 
   return (
     <Button
-      className="flex items-center justify-center h-8 px-3 border border-gray-950 dark:border-white rounded-none bg-white dark:bg-gray-950 text-sm font-normal leading-5 text-gray-950 dark:text-white select-none hover:not-data-disabled:bg-gray-50 dark:hover:not-data-disabled:bg-gray-900 active:not-data-disabled:bg-gray-100 dark:active:not-data-disabled:bg-gray-800 focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-blue-800 data-disabled:text-gray-500 dark:data-disabled:text-gray-400"
+      className="flex h-8 items-center justify-center rounded-none border border-gray-950 bg-white px-3 text-sm leading-5 font-normal text-gray-950 select-none hover:not-data-disabled:bg-gray-100 active:not-data-disabled:bg-gray-200 focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-blue-800 data-disabled:border-gray-500 data-disabled:text-gray-500 disabled:border-gray-500 disabled:text-gray-500 dark:border-white dark:bg-gray-950 dark:text-white dark:hover:not-data-disabled:bg-gray-800 dark:active:not-data-disabled:bg-gray-700 dark:data-disabled:border-gray-400 dark:data-disabled:text-gray-400"
       disabled={loading}
       focusableWhenDisabled
       onClick={() => {
