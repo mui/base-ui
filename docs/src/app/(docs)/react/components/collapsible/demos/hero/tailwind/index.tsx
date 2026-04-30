6439340ddfa41c9ab155164e04f4386f6da53621
Aarón García Hervás <aaron@mui.com>
2026-04-30 22:53:15 +0200

    Collapsible: improve chevron icon

--- docs/src/app/(docs)/react/components/collapsible/demos/hero/tailwind/index.tsx
+++ docs/src/app/(docs)/react/components/collapsible/demos/hero/tailwind/index.tsx
@@ -3,7 +3,7 @@ import { Collapsible } from '@base-ui/react/collapsible';
 
 export default function ExampleCollapsible() {
   return (
-    <Collapsible.Root className="flex min-h-36 w-56 flex-col justify-center text-neutral-950 dark:text-white">
+    <Collapsible.Root className="flex min-h-36 w-48 flex-col justify-center text-neutral-950 dark:text-white">
       <Collapsible.Trigger className="group flex h-8 items-center justify-between gap-2 rounded-none border border-neutral-950 dark:border-white bg-white dark:bg-neutral-950 pl-3 pr-2 text-sm font-normal text-neutral-950 dark:text-white select-none hover:not-data-disabled:bg-neutral-100 dark:hover:not-data-disabled:bg-neutral-800 active:not-data-disabled:bg-neutral-200 dark:active:not-data-disabled:bg-neutral-700 focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-blue-800 data-disabled:border-neutral-500 data-disabled:text-neutral-500 disabled:border-neutral-500 disabled:text-neutral-500 dark:data-disabled:border-neutral-400 dark:data-disabled:text-neutral-400">
         Recovery keys
         <ChevronIcon className="size-3 transition-transform duration-100 ease-out group-data-panel-open:rotate-90" />
@@ -22,7 +22,7 @@ export default function ExampleCollapsible() {
 export function ChevronIcon(props: React.ComponentProps<'svg'>) {
   return (
     <svg width="10" height="10" viewBox="0 0 10 10" fill="none" {...props}>
-      <path d="M3.5 9L7.5 5L3.5 1" stroke="currentColor" />
+      <path d="M3.5 1L7.5 5L3.5 9" stroke="currentColor" strokeWidth="1" />
     </svg>
   );
 }
