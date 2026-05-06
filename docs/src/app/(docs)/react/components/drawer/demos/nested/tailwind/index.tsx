b6f419f7515ce243debe7ebddb9d70dda1d9293a
Aarón García Hervás <aaron@mui.com>
2026-05-06 07:27:55 +0200

    Drawer: no soft buttons

--- docs/src/app/(docs)/react/components/drawer/demos/nested/tailwind/index.tsx
+++ docs/src/app/(docs)/react/components/drawer/demos/nested/tailwind/index.tsx
@@ -22,7 +22,7 @@ export default function ExampleDrawerNested() {
               <div className="flex items-center justify-end gap-4">
                 <div className="mr-auto">
                   <Drawer.Root>
-                    <Drawer.Trigger className="flex h-8 items-center justify-center bg-neutral-50 px-3 text-sm font-normal text-neutral-950 hover:bg-neutral-100 active:bg-neutral-200 dark:bg-neutral-900 dark:text-white dark:hover:bg-neutral-800 dark:active:bg-neutral-700 focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-blue-800">
+                    <Drawer.Trigger className="flex h-8 items-center justify-center border border-neutral-950 bg-white px-3 text-sm font-normal text-neutral-950 select-none hover:not-data-disabled:bg-neutral-100 active:not-data-disabled:bg-neutral-200 data-disabled:border-neutral-500 data-disabled:text-neutral-500 disabled:border-neutral-500 disabled:text-neutral-500 dark:border-white dark:bg-neutral-950 dark:text-white dark:hover:not-data-disabled:bg-neutral-800 dark:active:not-data-disabled:bg-neutral-700 dark:data-disabled:border-neutral-400 dark:data-disabled:text-neutral-400 focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-blue-800">
                       Security settings
                     </Drawer.Trigger>
                     <Drawer.Portal>
@@ -46,7 +46,7 @@ export default function ExampleDrawerNested() {
                             <div className="flex items-center justify-end gap-4">
                               <div className="mr-auto">
                                 <Drawer.Root>
-                                  <Drawer.Trigger className="flex h-8 items-center justify-center bg-neutral-50 px-3 text-sm font-normal text-neutral-950 hover:bg-neutral-100 active:bg-neutral-200 dark:bg-neutral-900 dark:text-white dark:hover:bg-neutral-800 dark:active:bg-neutral-700 focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-blue-800">
+                                  <Drawer.Trigger className="flex h-8 items-center justify-center border border-neutral-950 bg-white px-3 text-sm font-normal text-neutral-950 select-none hover:not-data-disabled:bg-neutral-100 active:not-data-disabled:bg-neutral-200 data-disabled:border-neutral-500 data-disabled:text-neutral-500 disabled:border-neutral-500 disabled:text-neutral-500 dark:border-white dark:bg-neutral-950 dark:text-white dark:hover:not-data-disabled:bg-neutral-800 dark:active:not-data-disabled:bg-neutral-700 dark:data-disabled:border-neutral-400 dark:data-disabled:text-neutral-400 focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-blue-800">
                                     Advanced options
                                   </Drawer.Trigger>
                                   <Drawer.Portal>
