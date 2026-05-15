35325e37f4553b511e59613de76857bef1712fda
Aarón García Hervás <aaron@mui.com>
2026-05-15 16:15:51 +0200

    New npm icon

--- docs/src/icons/NpmIcon.tsx
+++ docs/src/icons/NpmIcon.tsx
@@ -3,7 +3,9 @@ import * as React from 'react';
 export function NpmIcon(props: React.ComponentProps<'svg'>) {
   return (
     <svg fill="currentColor" width="16" height="16" viewBox="0 0 16 16" {...props}>
-      <path fillRule="evenodd" clipRule="evenodd" d="M1 1h14v14H1zM4 4h8v8H4zM7.5 6h3v6h-3z" />
+      <rect width="16" height="16" fill="black" />
+      <rect x="3" y="3" width="10" height="10" fill="white" />
+      <path d="M8 5H11V13H8V5Z" fill="black" />
     </svg>
   );
 }
