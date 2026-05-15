a57a85021a4133c978bda9cce9078c67f1bbe28e
Aarón García Hervás <aaron@mui.com>
2026-05-15 08:39:10 +0200

    Prettier

--- docs/src/icons/CaretDownIcon.tsx
+++ docs/src/icons/CaretDownIcon.tsx
@@ -2,13 +2,7 @@ import * as React from 'react';
 
 export function CaretDownIcon(props: React.ComponentProps<'svg'>) {
   return (
-    <svg
-      width="16"
-      height="16"
-      viewBox="0 0 16 16"
-      fill="currentColor"
-      {...props}
-    >
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor" {...props}>
       <path d="M12 6H4l4 4.5z" />
     </svg>
   );
--- docs/src/icons/NpmIcon.tsx
+++ docs/src/icons/NpmIcon.tsx
@@ -3,11 +3,7 @@ import * as React from 'react';
 export function NpmIcon(props: React.ComponentProps<'svg'>) {
   return (
     <svg fill="currentColor" width="16" height="16" viewBox="0 0 16 16" {...props}>
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M1 1h14v14H1zM4 4h8v8H4zM7.5 6h3v6h-3z"
-      />
+      <path fillRule="evenodd" clipRule="evenodd" d="M1 1h14v14H1zM4 4h8v8H4zM7.5 6h3v6h-3z" />
     </svg>
   );
 }
