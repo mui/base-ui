32ceb774ff5115454b6308f52f07ccc549e5e878
Albert Yu <albert@albertyu.co>
2026-06-02 06:47:41 +0800

    revert

--- packages/react/src/menu/trigger/MenuTrigger.tsx
+++ packages/react/src/menu/trigger/MenuTrigger.tsx
@@ -234,7 +234,6 @@ export const MenuTrigger = fastComponentRef(function MenuTrigger(
     rootTriggerProps,
     {
       'aria-haspopup': 'menu' as const,
-      'aria-expanded': isOpenedByThisTrigger,
       'aria-controls': popupId,
       id: thisTriggerId,
       onMouseDown: (event: React.MouseEvent) => {
