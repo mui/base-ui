1168a7128efdbe2db6020580a591e3caae9dcd56
Aarón García Hervás <aaron@mui.com>
2026-06-05 12:05:36 +0200

    Add background to inline code tags

--- docs/src/utils/createTypes.tsx
+++ docs/src/utils/createTypes.tsx
@@ -2,11 +2,13 @@ import {
   createTypesFactory,
   createMultipleTypesFactory,
 } from '@mui/internal-docs-infra/abstractCreateTypes';
+import enhanceCodeInline from '@mui/internal-docs-infra/pipeline/enhanceCodeInline';
 import { ReferenceTable } from '../components/ReferenceTable/ReferenceTable';
 import { mdxComponents } from '../mdx-components';
 import * as CodeBlock from '../components/CodeBlock';
 import { CodeBlockPreComputed } from '../components/CodeBlock/CodeBlockPreComputed';
 import { TableCode } from '../components/TableCode';
+import rehypeInlineCodeMeta from '../components/Code/rehypeInlineCodeMeta.mjs';
 
 interface MDXComponents {
   [key: string]: React.FC<any> | MDXComponents;
@@ -21,6 +23,8 @@ const components: MDXComponents = {
   ),
 };
 
+const enhancers = [enhanceCodeInline, rehypeInlineCodeMeta];
+
 /**
  * Creates a type doc component that renders a reference table for the given component.
  * @param url Depends on `import.meta.url` to determine the source file location.
@@ -33,6 +37,8 @@ export const createTypes = createTypesFactory({
   TypePre: CodeBlock.PreInline,
   ShortTypeCode: TableCode,
   DefaultCode: TableCode,
+  enhancers,
+  enhancersInline: enhancers,
   typeRefComponent: 'TypeRef',
   typePropRefComponent: 'TypePropRef',
 });
@@ -50,6 +56,8 @@ export const createMultipleTypes = createMultipleTypesFactory({
   TypePre: CodeBlock.PreInline,
   ShortTypeCode: TableCode,
   DefaultCode: TableCode,
+  enhancers,
+  enhancersInline: enhancers,
   typeRefComponent: 'TypeRef',
   typePropRefComponent: 'TypePropRef',
 });
