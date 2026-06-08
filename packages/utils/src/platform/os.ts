2fd7af6f6ae9444ba253dab4a2ca336033c6f1c1
Rom Grk <romgrk.cc@gmail.com>
2026-06-08 12:12:50 -0400

    docs: add iPad quirk comment

--- packages/utils/src/platform/os.ts
+++ packages/utils/src/platform/os.ts
@@ -2,6 +2,7 @@ import { lowerUserAgent, lowerPlatform, maxTouchPoints } from './shared';
 
 // iPadOS 13+ reports `MacIntel` for `navigator.platform`; disambiguated via
 // `maxTouchPoints` so iPad is classified as iOS, not macOS.
+// https://github.com/mui/base-ui/issues/1309
 /** iPhone, iPad (including iPadOS 13+ reporting as macOS), iPod. */
 export const ios =
   /^i(os$|p)/.test(lowerPlatform) || (lowerPlatform === 'macintel' && maxTouchPoints > 1);
