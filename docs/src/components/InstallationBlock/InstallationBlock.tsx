15b51c3e625169c2b368604329efbb688e04e739
Aarón García Hervás <aaron@mui.com>
2026-06-17 13:25:01 +0200

    Installation block new tabs

--- docs/src/components/InstallationBlock/InstallationBlock.tsx
+++ docs/src/components/InstallationBlock/InstallationBlock.tsx
@@ -40,7 +40,7 @@ export function InstallationBlock(props: InstallationBlockProps) {
       <CodeBlock.Root>
         <CodeBlock.Panel
           title="Installation command"
-          style={{ paddingLeft: 'calc(0.125rem + 1px)', borderBottom: 'none' }}
+          style={{ paddingLeft: 0, borderBottom: 'none' }}
         >
           <Tabs.List
             className="InstallationBlockTabsList"
