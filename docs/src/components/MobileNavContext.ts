b1484bf9925ca732ad827b779483cd019c6b509b
Aarón García Hervás <aaron@mui.com>
2026-06-29 13:05:15 +0200

    Simplify

--- docs/src/components/MobileNavContext.ts
+++ docs/src/components/MobileNavContext.ts
@@ -4,10 +4,6 @@ import type { Drawer } from '@base-ui/react/drawer';
 
 export const MobileNavContext = React.createContext<{
   handle: Drawer.Handle<unknown> | null;
-  searchValue: string;
-  setSearchValue: (value: string) => void;
 }>({
   handle: null,
-  searchValue: '',
-  setSearchValue: () => undefined,
 });
