9ed4b6c1d31bc959d0e1899649b1ad7c8b9b14ba
Janpot <2109932+Janpot@users.noreply.github.com>
2026-07-09 17:58:18 +0200

    Use the new sections functionality in docs infra

--- docs/src/app/sitemap/index.ts
+++ docs/src/app/sitemap/index.ts
@@ -1,12 +1,4 @@
 import { createSitemap } from '@mui/internal-docs-infra/createSitemap';
-import Overview from '../(docs)/react/(overview)/index.mdx';
-import Handbook from '../(docs)/react/(handbook)/index.mdx';
-import Components from '../(docs)/react/(components)/index.mdx';
-import Utils from '../(docs)/react/(utils)/index.mdx';
+import React from '../(docs)/react/page.mdx';
 
-export const sitemap = createSitemap(import.meta.url, {
-  Overview,
-  Handbook,
-  Components,
-  Utils,
-});
+export const sitemap = createSitemap(import.meta.url, { React });
