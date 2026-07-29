bf4f6e6ead5a97f58f7c607056c227c345647338
Takeru Chuganji <chu@ganzy.jp>
2026-07-29 10:36:38 +0900

    [internal] Complete reason constant migration

--- packages/react/src/number-field/utils/types.ts
+++ packages/react/src/number-field/utils/types.ts
@@ -1,11 +1,13 @@
+import { REASONS } from '../../internals/reasons';
+
 export type Direction = -1 | 1;
 
 export type DirectionalChangeReason =
-  | 'increment-press'
-  | 'decrement-press'
-  | 'wheel'
-  | 'scrub'
-  | 'keyboard';
+  | typeof REASONS.incrementPress
+  | typeof REASONS.decrementPress
+  | typeof REASONS.wheel
+  | typeof REASONS.scrub
+  | typeof REASONS.keyboard;
 
 export interface ChangeEventCustomProperties {
   direction?: Direction | undefined;
