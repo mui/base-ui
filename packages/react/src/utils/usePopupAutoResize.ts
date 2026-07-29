cd427fda443130a6dc8aa2fd5bf81887cf5593b0
atomiks <cc.glows@gmail.com>
2026-07-30 09:49:30 +1000

    [popups] Clarify auto-resize origin sides

--- packages/react/src/utils/usePopupAutoResize.ts
+++ packages/react/src/utils/usePopupAutoResize.ts
@@ -186,16 +186,19 @@ interface UsePopupAutoResizeParameters {
 
 export function getPopupAnchoringStyles(side: Side, direction: 'ltr' | 'rtl'): React.CSSProperties {
   // Ensure popup size transitions correctly when anchored to `bottom` (side=top) or `right` (side=left).
+  const isPhysicalTop = side === 'top';
   const isPhysicalLeft =
     side === 'left' || side === (direction === 'rtl' ? 'inline-end' : 'inline-start');
 
-  return side === 'top' || isPhysicalLeft
-    ? {
-        position: 'absolute',
-        [side === 'top' ? 'bottom' : 'top']: '0',
-        [isPhysicalLeft ? 'right' : 'left']: '0',
-      }
-    : EMPTY_OBJECT;
+  if (!isPhysicalTop && !isPhysicalLeft) {
+    return EMPTY_OBJECT;
+  }
+
+  return {
+    position: 'absolute',
+    [isPhysicalTop ? 'bottom' : 'top']: '0',
+    [isPhysicalLeft ? 'right' : 'left']: '0',
+  };
 }
 
 function overrideElementStyle(element: HTMLElement, property: string, value: string) {
