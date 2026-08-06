3527472c0e7a177121e506fbe96c08a8590ced9d
atomiks <cc.glows@gmail.com>
2026-08-07 08:56:39 +1000

    [docs] Remove redundant Select Motion demo

--- docs/src/app/(docs)/react/handbook/animation/demos/animated-select-motion/index.ts
+++
@@ -1,6 +0,0 @@
-import { createDemoWithVariants } from 'docs/src/utils/createDemo';
-import CssModules from './css-modules';
-
-export const DemoAnimatedSelectMotion = createDemoWithVariants(import.meta.url, {
-  CssModules,
-});
