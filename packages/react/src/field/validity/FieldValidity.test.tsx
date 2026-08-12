bb1d9905d45a2b154e363790d7b5ae7dda63821a
atomiks <cc.glows@gmail.com>
2026-08-12 21:51:07 +1000

    [field] Fix custom validity ownership and validation lifecycle (#5449)

--- packages/react/src/field/validity/FieldValidity.test.tsx
+++ packages/react/src/field/validity/FieldValidity.test.tsx
@@ -46,7 +46,7 @@ describe('<Field.Validity />', () => {
       fireEvent.change(input, { target: { value: '' } });
 
       expect(handleValidity.mock.lastCall?.[0].value).toBe('');
-      expect(handleValidity.mock.lastCall?.[0].validity.customError).toBe(true);
+      expect(handleValidity.mock.lastCall?.[0].validity.customError).toBe(false);
       expect(handleValidity.mock.lastCall?.[0].validity.valueMissing).toBe(true);
       expect(screen.getByText('Required')).toBeVisible();
       expect(validate).toHaveBeenCalledTimes(1);
@@ -58,7 +58,9 @@ describe('<Field.Validity />', () => {
       }
 
       expect(handleValidity.mock.lastCall?.[0].value).toBe('');
-      expect(handleValidity.mock.lastCall?.[0].validity.customError).toBe(true);
+      expect(handleValidity.mock.lastCall?.[0].validity.customError).toBe(
+        validationMode === 'onSubmit',
+      );
       expect(handleValidity.mock.lastCall?.[0].validity.valueMissing).toBe(true);
       expect(screen.getByText('Required')).toBeVisible();
     });
