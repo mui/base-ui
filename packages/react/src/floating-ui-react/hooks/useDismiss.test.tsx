2ba26578b1de2acb9fc294e3e29e6eb98ce9d4d3
wanxiankai <xiankai.wan@gmail.com>
2026-08-26 10:59:24 +0800

    [useDismiss] Register passive touch listeners

--- packages/react/src/floating-ui-react/hooks/useDismiss.test.tsx
+++ packages/react/src/floating-ui-react/hooks/useDismiss.test.tsx
@@ -70,6 +70,23 @@ function App(
 
 describe.skipIf(!isJSDOM)('useDismiss', () => {
   describe('default options', () => {
+    test('registers outside press touch listeners as passive', async () => {
+      const addEventListenerSpy = vi.spyOn(document, 'addEventListener');
+
+      render(<App />);
+
+      await Promise.all(
+        ['touchstart', 'touchmove', 'touchend'].map((eventName) =>
+          waitFor(() => {
+            expect(addEventListenerSpy).toHaveBeenCalledWith(eventName, expect.any(Function), {
+              capture: true,
+              passive: true,
+            });
+          }),
+        ),
+      );
+    });
+
     test('dismisses with escape key', async () => {
       render(<App />);
       fireEvent.keyDown(document.body, { key: 'Escape' });
