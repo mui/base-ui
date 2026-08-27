f1a986adefe215099c8328fbaa737ef43ab0549b
atomiks <cc.glows@gmail.com>
2026-08-27 14:08:14 +1000

    [filter menu] Require the item count for virtualized lists

--- packages/react/src/filter-menu/utils/FilterMenuProvider.tsx
+++ packages/react/src/filter-menu/utils/FilterMenuProvider.tsx
@@ -19,7 +19,7 @@ export interface FilterMenuProviderProps {
   locale: Intl.LocalesArgument | undefined;
   inline?: boolean | undefined;
   grid?: boolean | undefined;
-  virtualized?: boolean | number | undefined;
+  virtualized?: number | undefined;
   inputProps: HTMLProps;
   onValueChange: (value: string, details: FilterMenuRoot.InputValueChangeEventDetails) => void;
   onInputElementChange: (hasInput: boolean) => void;
