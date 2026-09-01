ad83c8ad177b90f0a5b1b4b9e5ee1004a52d9369
Brijesh Bittu <717550+brijeshb42@users.noreply.github.com>
2026-09-01 15:35:14 +0530

    [code-infra] Run TypeScript 7 alongside the TS6 JS API (#5603)

--- packages/react/src/internals/itemEquality.test.ts
+++ packages/react/src/internals/itemEquality.test.ts
@@ -1,4 +1,4 @@
-import { expect } from 'vitest';
+import { expect, describe, it } from 'vitest';
 import { defaultItemEquality, findSelectionIndex, resolveSelectedIndex } from './itemEquality';
 
 describe('findSelectionIndex', () => {
