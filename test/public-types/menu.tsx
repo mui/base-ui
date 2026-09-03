7ad1896dad81993fe20419e497028ade415ccbcd
atomiks <cc.glows@gmail.com>
2026-09-03 19:47:57 +1000

    [menu] Port the public type fixtures to Menu.FilterRoot

--- test/public-types/menu.tsx
+++ test/public-types/menu.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { Menu } from '@base-ui/react/menu';
 
 export type MenuRootProps = Menu.Root.Props;
@@ -11,6 +12,28 @@ export interface SimpleMenuProps extends Omit<MenuRootProps, 'children'> {
 }
 
 export function InternalSubmenuPropsStayHidden() {
-  // @ts-expect-error virtual focus is internal to FilterMenu
+  // @ts-expect-error virtual focus is internal to Menu.FilterRoot
   return <Menu.SubmenuRoot virtualFocus />;
 }
+
+const filterHandle = Menu.createHandle<{ id: number }>({ filterable: true });
+
+export function TypedFilterRootTrigger() {
+  return (
+    <React.Fragment>
+      <Menu.FilterRoot handle={filterHandle}>
+        {({ payload }) => <span>{payload?.id}</span>}
+      </Menu.FilterRoot>
+      <Menu.Trigger handle={filterHandle} payload={{ id: 1 }}>
+        Open
+      </Menu.Trigger>
+      {/* @ts-expect-error the payload must match the handle */}
+      <Menu.Trigger handle={filterHandle} payload="wrong">
+        Invalid
+      </Menu.Trigger>
+    </React.Fragment>
+  );
+}
+
+export type MenuFilterFunction = Menu.FilterRoot.Props['filter'];
+export type MenuFilterUtils = ReturnType<typeof Menu.useFilter>;
