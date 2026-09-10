8a2e19d7899e82ca32d67b0a5ffa7b58befb61eb
atomiks <cc.glows@gmail.com>
2026-09-10 19:43:37 +1000

    [docs] Label the filterable menu close button in every demo

--- test/public-types/menu.tsx
+++ test/public-types/menu.tsx
@@ -38,7 +38,7 @@ const filterHandle = Menu.createHandle<{ id: number }>({ filterable: true });
 export function TypedFilterableTrigger() {
   return (
     <React.Fragment>
-      <Menu.FilterProvider>
+      <Menu.FilterProvider closeLabel="Dismiss menu">
         <Menu.Root handle={filterHandle}>{({ payload }) => <span>{payload?.id}</span>}</Menu.Root>
       </Menu.FilterProvider>
       <Menu.Trigger handle={filterHandle} payload={{ id: 1 }}>
